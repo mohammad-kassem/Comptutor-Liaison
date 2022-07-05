cd894b02175463b2f5d5cea75c39558b741fd672
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-05 22:01:10 +0300

    feat: update the User model for JWT configuration backend

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -6,12 +6,11 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class User extends Authenticatable
-{
-    use HasApiTokens, HasFactory, Notifiable;
 
+class User extends Authenticatable implements JWTSubject{
+    use HasFactory, Notifiable;
 
     protected $fillable = [
         'fname',
@@ -55,4 +54,12 @@ class User extends Authenticatable
         ->as('user_subject')
         ->withTimestamps();
     }
+    public function getJWTIdentifier(){
+        return $this->getKey();
+    }
+
+    public function getJWTCustomClaims(){
+        return [];
+    }
+
 }
