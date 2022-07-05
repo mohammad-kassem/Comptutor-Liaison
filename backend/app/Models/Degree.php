ae04da69f4476e6d2fa71268de9c62d70f76c362
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-05 16:12:51 +0300

    feat: set up Degree model backend

---
+++ backend/app/Models/Degree.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Degree extends Model{
+    use HasFactory;
+
+    protected $fillable = [
+        'university',
+        'degree',
+        'tutor_id'
+    ];
+
+}
