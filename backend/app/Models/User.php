3b2da20df9fde46d897ceb3a3ffd03336eee3fac
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-07 02:48:08 +0300

    backend: fix: set the name of the foreign key between Appointment and User models to 'student_id'

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable implements JWTSubject{
     }
 
     public function appointments(){
-        return $this->hasMany(Appointment::class);
+        return $this->hasMany(Appointment::class, 'student_id');
     }
 
     
