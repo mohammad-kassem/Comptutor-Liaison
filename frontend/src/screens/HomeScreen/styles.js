4c5e1f0f180e8bde757985c92aadc711fc29ec50
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-10 22:52:44 +0300

    frontend: feat: implement the HomeScreen search bar stylesheet

--- frontend/src/screens/HomeScreen/styles.js
+++ frontend/src/screens/HomeScreen/styles.js
@@ -20,5 +20,27 @@ export default StyleSheet.create({
         fontSize: 20
     },
 
+    searchBar: {
+        backgroundColor: "#ffff",
+        flexDirection: "row",
+        height: 44,
+        marginHorizontal: 25,
+        marginTop: 25,
+        padding: 15,
+        borderRadius: 20,
+        elevation: 20,
+    },
+
+    searchIcon: {
+        color: "#1877F2",
+        fontSize: 24,
+        margin: -7.5,
+        marginRight: 10,
+    },
+
+    searchInput: {
+        flex: 1,
+        textAlignVertical: "center",
+    },
 
 })
