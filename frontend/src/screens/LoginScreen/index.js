b87e4a61a9a48287df831d431ae4cbbedf2918e6
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-10 12:43:02 +0300

    frontend: feat: linking the login api with the LoginScreen

--- frontend/src/screens/LoginScreen/index.js
+++ frontend/src/screens/LoginScreen/index.js
@@ -3,13 +3,32 @@ import styles from './styles';
 import { Text, View } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
+import axios from 'axios';
+
 
 
 export default function LoginScreen() {
   const navigation = useNavigation()
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-
+  
+  function login(cridentials){
+    axios({
+      method: "post",
+      url: "http://192.168.1.11:8000/api/v1/auth/login",
+      headers: {
+        "Content-type": "application/json",
+      },
+      data: JSON.stringify(cridentials),
+    })
+    .then(function(response){
+      console.log("success");
+    })
+    .catch(function(error){
+      let message = Object.values(error.response.data);
+      alert(message[0]);
+    })
+  };
   
   return (
     <>
@@ -22,7 +41,7 @@ export default function LoginScreen() {
       <TextInput style={styles.input} placeholder="Email" onChangeText={(enteredText)=>{setEmail(enteredText)}}/>
       
       <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} onChangeText={(enteredText)=>{setPassword(enteredText)}}/>
-      <TouchableOpacity containerStyle={styles.fullWidthButton}>
+      <TouchableOpacity containerStyle={styles.fullWidthButton} onPress={()=>login({email, password})}>
         <Text style={styles.fullWidthButtonText}>Log in</Text>
       </TouchableOpacity>
       <View style={styles.registerMessage}>
