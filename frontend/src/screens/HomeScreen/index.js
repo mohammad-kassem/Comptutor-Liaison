1acbb4691f91c5590c3effcaf60849b4e1b7b2c0
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-11 00:28:56 +0300

    frontend: feat: implement local state management for HomeScreen for linking

--- frontend/src/screens/HomeScreen/index.js
+++ frontend/src/screens/HomeScreen/index.js
@@ -1,44 +1,48 @@
-import { View, Text ,Image } from 'react-native'
-import React from 'react'
+import { View, Text ,Image, FlatList } from 'react-native'
+import React, { useEffect, useState } from 'react'
 import styles from './styles'
 import { TextInput, TouchableOpacity } from 'react-native-gesture-handler'
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function HomeScreen() {
-  return (
-    <>
-    <View style={styles.container}>
-        <View style={styles.title}>
-            <Text style={styles.titleText}>Home</Text>
-        </View>
-        <View style={styles.searchBar}>
-            <Icon name="search" style={styles.searchIcon}/>
-            <TextInput style={styles.searchInput} placeholder="Search"/>
-        </View>
-        <TouchableOpacity style={styles.tutorCard}>
-            <View style={styles.cardContent}>
-                <View style={styles.imageContainer}>
-                    <Image style={styles.tutorProfile} source={require('../../../assets/logo.png')}/>
-                </View>
-                <View style={styles.tutorInfo}>
-                    <Text style={styles.tutorName}>Jane Doe</Text>
-                    <View style={styles.tutorRates}>
-                        <Text style={styles.tutorDetail}>Chatting: 10</Text>
-                        <Text style={styles.tutorDetail}>Zoom: 20</Text>
-                    </View>
-                    <Text style={styles.tutorDetail}>Teaching since 2016</Text>
-                </View>
+    const [tutors, setTutors] = useState([])
+
+    return (
+        <>
+        <View style={styles.container}>
+            <View style={styles.title}>
+                <Text style={styles.titleText}>Home</Text>
             </View>
-            <View style={styles.tutorSubjects}>
-                <Text style={styles.tutorSubject}>PHP</Text>
-                <Text style={styles.tutorSubject}>Java</Text>
-                <Text style={styles.tutorSubject}>Javascript</Text>
-                <Text style={styles.tutorSubject}>Javascript</Text>
-                <Text style={styles.tutorSubject}>Javascript</Text>
-                <Text style={styles.tutorSubject}>Javascript</Text>
+            <View style={styles.searchBar}>
+                <Icon name="search" style={styles.searchIcon}/>
+                <TextInput style={styles.searchInput} placeholder="Search"/>
             </View>
-        </TouchableOpacity>
-    </View>
-    </>
-  )
+            <FlatList data={tutors} renderItem={(tutorData) =>{
+                return(
+                    <TouchableOpacity style={styles.tutorCard}>
+                        <View style={styles.cardContent}>
+                            <View style={styles.imageContainer}>
+                                <Image style={styles.tutorProfile} source={require('../../../assets/logo.png')}/>
+                            </View>
+                            <View style={styles.tutorInfo}>
+                                <Text style={styles.tutorName}>{tutorData.item.fname} {tutorData.item.lname}</Text>
+                                <View style={styles.tutorRates}>
+                                    <Text style={styles.tutorDetail}>Chatting: {tutorData.item.rate_chat}</Text>
+                                    <Text style={styles.tutorDetail}>Zoom: {tutorData.item.rate_zoom}</Text>
+                                </View>
+                                <Text style={styles.tutorDetail}>Teaching since {tutorData.item.experience}</Text>
+                            </View>
+                        </View>
+                        <View style={styles.tutorSubjects}>
+                            {tutorData.item.subjects.map((subject, index)=>{return(
+                            <Text style={styles.tutorSubject}>{subject.subject}</Text>
+                            )})}
+                        </View>
+                    </TouchableOpacity>
+                )
+            }}
+            />
+        </View>
+        </>
+    )
 }
