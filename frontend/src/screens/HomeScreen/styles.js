177a558b004dcf856e323c163e01297b681e19b8
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-10 21:36:55 +0300

    frontend: feat: implement the HomeScreen title stylesheet

---
+++ frontend/src/screens/HomeScreen/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        backgroundColor: "#f5f5f5",
+        paddingVertical: 15
+    },
+
+    title:{
+        justifyContent: "center",
+        alignItems: "center",
+        borderColor: "black",
+        borderWidth: 1,
+    },
+
+    titleText: {
+        fontSize: 20
+    },
+
+
+})
