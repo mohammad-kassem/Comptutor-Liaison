d2157293d9bb0151ee0a88a3a7c3594eb52d3916
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-11 22:22:42 +0300

    frontend: feat: implement the NavigatorSwitcher to render in App.js

--- frontend/App.js
+++ frontend/App.js
@@ -1,12 +1,15 @@
 import 'react-native-gesture-handler';
 import UserProvider from './src/Context/User';
+import BottomTabsStack from './src/navigation/BottomTabsStack';
 import OnBoardingStack from './src/navigation/OnBoardingStack';
+import NavigatorSwitch from './src/navigation/NavigatorSwitch';
+
 
 export default function App() {
   return (
     <>
     <UserProvider>
-      <OnBoardingStack/>
+      <NavigatorSwitch/>
     </UserProvider>
     </>
   );
