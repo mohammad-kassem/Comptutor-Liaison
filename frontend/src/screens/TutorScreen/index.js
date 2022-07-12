ed249432197e997da56487c75cfbd08e637c7285
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-12 04:08:39 +0300

    frontend: feat: implement the TutorScreen image

--- frontend/src/screens/TutorScreen/index.js
+++ frontend/src/screens/TutorScreen/index.js
@@ -5,5 +5,16 @@ import { ScrollView } from 'react-native-gesture-handler'
 
 export default function TutorScreen( {route} ) {
     const tutor = route.params.tutor
+    return (
+        <>
+        <View style={styles.container}>
+        <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View style={styles.imageContainer}>
+                <Image style={styles.tutorProfile} source={require('../../../assets/logo.png')}/>
+            </View>
 
+        </ScrollView>
+        </View>
+        </>
+    )
 }
