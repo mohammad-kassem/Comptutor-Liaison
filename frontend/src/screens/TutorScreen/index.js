e5182022e7fb0048a950f0acb32d180d0890d8d8
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-12 04:11:20 +0300

    frontend: feat: implement the TutorScreen title

--- frontend/src/screens/TutorScreen/index.js
+++ frontend/src/screens/TutorScreen/index.js
@@ -12,7 +12,13 @@ export default function TutorScreen( {route} ) {
             <View style={styles.imageContainer}>
                 <Image style={styles.tutorProfile} source={require('../../../assets/logo.png')}/>
             </View>
+            <View style={styles.infoContainer}>
+                <View style={styles.title}>
+                    <Text style={styles.tutorName}>{tutor.fname} {tutor.lname}</Text>
+                    <Text style={styles.tutorExperince}>Teaching since {}</Text>
+                </View>
 
+            </View>
         </ScrollView>
         </View>
         </>
