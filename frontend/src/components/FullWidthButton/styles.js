a6d3264a661d5e8b892bc9d670f2148dd5243b17
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-12 04:20:19 +0300

    frontend: feat: implement the FullWidthButton styling

---
+++ frontend/src/components/FullWidthButton/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    fullWidthButton:{
+        backgroundColor: "#1877F2",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 15,
+        borderRadius: 10,
+    },
+
+    fullWidthButtonText:{
+        fontSize: 16,
+        fontWeight: '500',
+        color: "white",
+    },
+})
