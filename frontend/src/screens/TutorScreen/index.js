013aff9a2cd42e6358ad210973ca398bba39d4b5
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-12 04:04:24 +0300

    frontend: feat: get the tutor info in TutorScreen form the route parameters

---
+++ frontend/src/screens/TutorScreen/index.js
@@ -0,0 +1,9 @@
+import { View, Text, Image } from 'react-native'
+import React from 'react'
+import styles from './styles'
+import { ScrollView } from 'react-native-gesture-handler'
+
+export default function TutorScreen( {route} ) {
+    const tutor = route.params.tutor
+
+}
