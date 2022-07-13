a658a9a9171a1717e8eb979057f6c1f4ba7ffd90
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 08:50:04 +0300

    frontend: feat: use the get available times function in the ScheduleScreen

--- frontend/src/screens/ScheduleScreen/index.js
+++ frontend/src/screens/ScheduleScreen/index.js
@@ -1,9 +1,15 @@
 import { View, Text } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import styles from './styles'
+import { getAvailableTimes } from './controller'
 
 export default function ScheduleScreen({ route }) {
     const tutor = route.params.tutor
+    const [schedules, setSchedules] = useState([])
+
+    useEffect(function(){
+        getAvailableTimes(tutor.id, setSchedules);
+      }, []);
 
   return (
     <View>
