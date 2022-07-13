2ca37826585400d7d7b8cd3b65e32f25ca5897b2
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 09:19:02 +0300

    frontend: feat: implement the AppointmentModal title styling

---
+++ frontend/src/components/AppointmentModal/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      
+      modal: {
+        alignItems: 'center',
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        borderRadius: 20,
+      },
+
+      title: {
+        marginTop: 30,
+        fontWeight: "bold",
+        fontSize: 20,
+        lineHeight: 24,
+      },
+
+
+});
