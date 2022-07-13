3687977dff09a108ddcdc3583c306c57b2a8b0e4
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 21:32:36 +0300

    frontend: feat: implement the appointments state flatlist

--- frontend/src/screens/AppointmentsScreen/index.js
+++ frontend/src/screens/AppointmentsScreen/index.js
@@ -2,22 +2,29 @@ import { View, Text } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import styles from './styles'
 import { getAppointments } from './controller';
+import { FlatList } from 'react-native-gesture-handler';
 
 export default function AppointmentsScreen() {
     const [appointments, setAppointments] = useState([])
+
     useEffect(function(){
         getAppointments(setAppointments);
       }, []);
 
-  return (
-        <View style={styles.container}>
-            <Text style={styles.title}>Appointments</Text>
-            <View style={styles.appointmentCard}>
-                <View style={styles.cardContent}>
-                    <Text style={styles.date}>6/15/22 3:00 PM - 4:00 PM</Text>
-                    <Text style={styles.details}>Appointment with Jane</Text>
+    return (
+            <View style={styles.container}>
+                <Text style={styles.title}>Appointments</Text>
+                <FlatList data={appointments} renderItem={(appointmentData) =>{
+                return(
+                <View style={styles.appointmentCard}>
+                    <View style={styles.cardContent}>
+                        <Text style={styles.date}>{appointmentData.item.date} {appointmentData.item.start_time} - {appointmentData.item.end_time}</Text>
+                        <Text style={styles.details}>Appointment with Jane</Text>
+                    </View>
                 </View>
-            </View>
-        </View>   
-  )
+                )
+                }}
+                />
+            </View>   
+    )
 }
