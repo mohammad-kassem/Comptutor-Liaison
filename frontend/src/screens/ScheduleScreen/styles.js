15424ee3e5dd843da9de65826777973f42f3d47d
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 09:05:58 +0300

    frontend: feat: implement the ScheduleScreen  time cards styling

--- frontend/src/screens/ScheduleScreen/styles.js
+++ frontend/src/screens/ScheduleScreen/styles.js
@@ -24,4 +24,41 @@ export default StyleSheet.create({
         paddingTop: 5,
     },
 
+    timesContainer: {
+        justifyContent: "flex-start",
+        alignItems: "center",
+        flexDirection: "row",
+        flexWrap: "wrap",
+    },
+
+    timeCardLeft: {
+        marginTop: 30,
+        width: "50%",
+        height: 50,
+        justifyContent: "center",
+        alignItems: "flex-start",
+    },
+
+    timeCardRight: {
+        marginTop: 30,
+        width: "50%",
+        height: 50,
+        justifyContent: "center",
+        alignItems: "flex-end",
+
+    },
+
+    timeContainer :{
+        backgroundColor: "white",
+        width: 100,
+        height: 50,
+        borderRadius: 10,
+        elevation: 5,
+        justifyContent: "center",
+    },
+
+    time: {
+        textAlign: "center",
+        textAlignVertical: "center"
+    },
 })
