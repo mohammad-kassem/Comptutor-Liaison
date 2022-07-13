f3c10f283c7fdc07c2850d8b2baa865f7c296451
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 20:38:31 +0300

    frontend: feat: implement the  AppointmentScreen title styling

---
+++ frontend/src/screens/AppointmentsScreen/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        backgroundColor: "#f5f5f5",
+        paddingVertical: 50,
+        paddingHorizontal: "10%"
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: "700",
+        lineHeight: 29
+    },
+
+
+})
