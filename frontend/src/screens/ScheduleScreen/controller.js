b1ac85b17caa4d9a645a826ee06634ff834e5acf
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 08:47:57 +0300

    frontend: feat: link the get available times api with the frontend

--- frontend/src/screens/ScheduleScreen/controller.js
+++ frontend/src/screens/ScheduleScreen/controller.js
@@ -1,5 +1,7 @@
 import { View, Text, ToastAndroid } from 'react-native'
 import React from 'react'
+import { getToken } from '../../components/utility/Token';
+import axios from 'axios';
 
 export function groupSchedules(schedules) {
   let result = []
@@ -12,3 +14,28 @@ export function groupSchedules(schedules) {
   }
     return groupedSchedule
 }
+
+export async function getAvailableTimes(tutorId, setSchedules) {
+    // data = new FormData()
+    // data.append("schedule_id", 28)
+    const token = await getToken()
+    console.log(token);
+    axios({
+        method: "get",
+        url: `http://192.168.1.105:8000/api/v1/tutor/schedule/available/${tutorId}`,
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+      })
+      .then(function(response){
+        console.log("hello")
+        setSchedules(response.data.available_times)
+        // alert(await response.data.message);
+      })
+      .catch(error=>console.log(error))
+        // console.log("error")
+        // let message = Object.values( await error.response.data);
+        // alert(message[0]);
+      
+};
