5670bfee4f41506573d416d392fd6ce423fa0db8
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 08:53:36 +0300

    frontend: feat: implement the ScheduleScreen title styling

---
+++ frontend/src/screens/ScheduleScreen/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        backgroundColor: "#f5f5f5",
+        paddingVertical: 25,
+        paddingHorizontal: "5%"
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: "700",
+        lineHeight: 29
+    },
+
+
+})
