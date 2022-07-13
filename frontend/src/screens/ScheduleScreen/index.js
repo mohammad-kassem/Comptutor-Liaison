8ac60dfdda7089b42d87e383cce892630d9fa5f6
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 08:51:16 +0300

    frontend: feat: implement grouping the available times by date in the ScheduleScreen

--- frontend/src/screens/ScheduleScreen/index.js
+++ frontend/src/screens/ScheduleScreen/index.js
@@ -1,11 +1,14 @@
 import { View, Text } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import styles from './styles'
-import { getAvailableTimes } from './controller'
+import { getAvailableTimes, groupSchedules } from './controller'
 
 export default function ScheduleScreen({ route }) {
     const tutor = route.params.tutor
     const [schedules, setSchedules] = useState([])
+    let groupedSchedules = groupSchedules(schedules);
+    console.log("hello2")
+
 
     useEffect(function(){
         getAvailableTimes(tutor.id, setSchedules);
