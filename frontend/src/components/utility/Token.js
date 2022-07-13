45fabc8e0c768f72433ad1f324d4e20b5a9ed121
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 08:46:29 +0300

    frontend: feat: implement the get token from async storage function

---
+++ frontend/src/components/utility/Token.js
@@ -0,0 +1,20 @@
+
+
+import { View, Text } from 'react-native'
+import React from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export async function getToken() {
+        let value;
+        try {
+          value = await AsyncStorage.getItem('storage_Key');
+          return value
+          if(value !== null) {
+          }
+        } catch(e) {
+            alert("Error getting token")
+            console.log(e);
+            return undefined
+        }
+
+}
