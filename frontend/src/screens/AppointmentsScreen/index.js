0342a9a9571787456009163dcdcb7711a67aa73a
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 20:39:27 +0300

    frontend: feat: implement the  AppointmentScreen appointment card with date

--- frontend/src/screens/AppointmentsScreen/index.js
+++ frontend/src/screens/AppointmentsScreen/index.js
@@ -6,6 +6,11 @@ export default function AppointmentsScreen() {
   return (
         <View style={styles.container}>
             <Text style={styles.title}>Appointments</Text>
+            <View style={styles.appointmentCard}>
+                <View style={styles.cardContent}>
+                    <Text style={styles.date}>6/15/22 3:00 PM - 4:00 PM</Text>
+                </View>
+            </View>
         </View>   
   )
 }
