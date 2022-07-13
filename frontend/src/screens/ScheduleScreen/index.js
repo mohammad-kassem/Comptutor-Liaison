98d3193af022a42975534d39071a8e3f34555634
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-13 09:04:13 +0300

    frontend: feat: implement the ScheduleScreen  time cards

--- frontend/src/screens/ScheduleScreen/index.js
+++ frontend/src/screens/ScheduleScreen/index.js
@@ -2,6 +2,7 @@ import { View, Text } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import styles from './styles'
 import { getAvailableTimes, groupSchedules } from './controller'
+import { FlatList, ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
 
 export default function ScheduleScreen({ route }) {
     const tutor = route.params.tutor
@@ -24,6 +25,24 @@ export default function ScheduleScreen({ route }) {
                 return (
                 <>
                 <Text style={styles.date}>{scheduleDate}</Text>
+                <View style={styles.timesContainer}>
+                {groupedSchedules[scheduleDate].map((time, index)=>{
+                     time.start_time = time.start_time.split(":")[0] + ":" + time.start_time.split(":")[1]
+                     time.end_time = time.end_time.split(":")[0] + ":" + time.end_time.split(":")[1]
+                     return(
+                     index % 2 === 0 ? <View style={styles.timeCardLeft}>
+                        <TouchableOpacity style={styles.timeContainer} onPress={()=>{setModalIsVisable(true); setModalData([time, scheduleDate])}}>
+                            <Text style={styles.time}>{time.start_time}</Text>
+                            <Text style={styles.time}>{time.end_time}</Text>
+                        </TouchableOpacity>
+                     </View>
+                     :
+                     <View style={styles.timeCardRight}>
+                        <TouchableOpacity style={styles.timeContainer} onPress={()=>{setModalIsVisable(true); setModalData([time, scheduleDate])}}>
+                            <Text style={styles.time}>{time.start_time}</Text>
+                            <Text style={styles.time}>{time.end_time}</Text>
+                        </TouchableOpacity>
+                     </View>)})}
                 </View>
                 </>
             )})}
