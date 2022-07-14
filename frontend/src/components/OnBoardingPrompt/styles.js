4ad09f69acf2261d6874044ba2cb1f3a30e59f41
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 01:59:11 +0300

    frontend: feat: impelement the OnBoardingPrompt component styling

---
+++ frontend/src/components/OnBoardingPrompt/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    title: {
+        color: "#656565",
+        fontSize: 20,
+        lineHeight: 23,
+        borderBottomWidth: 1,
+        paddingTop: 5,
+    },
+})
