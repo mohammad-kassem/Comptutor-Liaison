79e4914221199c3ae431e082ead03cc24c65835d
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 02:18:07 +0300

    frontend: feat: link the get subjects api to the AddSubjectsScreen

---
+++ frontend/src/screens/AddSubjectsScreen/controller.js
@@ -0,0 +1,23 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { getToken } from '../../components/utility/Token';
+import axios from 'axios';
+
+export async function getSubjects(setSubjects) {
+    const token = await getToken()
+    console.log(token);
+    axios({
+        method: "get",
+        url: "http://192.168.1.105:8000/api/v1/subject",
+        headers: {
+          "Content-type": "application/json",
+          "Authorization": `Bearer ${token}`
+        },
+      })
+      .then(function(response){
+        console.log("hello")
+        setSubjects(response.data.subjects)
+        // alert(await response.data.message);
+      })
+      .catch(error=>console.log(error))
+}
