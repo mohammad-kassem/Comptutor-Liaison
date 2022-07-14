7d7ce8e1f540924c92e0bc5313f1b671b038c34f
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-14 13:24:46 +0300

    frontend: feat: link the get user appointments api to the AppointmentsProvider

--- frontend/src/Context/Appointments.js
+++ frontend/src/Context/Appointments.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, {useEffect, useState} from 'react';
 import { getToken } from '../components/utility/Token';
 import { useUser } from './User';
@@ -7,6 +8,49 @@ export const AppointmentsContext = React.createContext()
 export default function AppointmentsProvider({children}) {
     const [appointments,setAppointments] = useState({})
 
+    useEffect(function(){
+        getAppointments(setAppointments)
+      }, []);
+
+      async function getAppointments() {
+        const token = await getToken();
+    //     // console.log(token)
+    //     //     try {
+    //     //       const res = await fetch("http://192.168.1.105:8000/api/v1/appointment/student",{
+    //     //         headers: {"Authorization" : `Bearer ${token}`,
+    //     //         "Content-type": "application/json",
+    //     //     "Accept": "application/json"}
+    //     //       });
+    //     //       const data = await res.json();
+    //     //       console.log("hello")
+    //     //       console.log(typeof data)
+    //     //       setAppointments(data.appointments)
+    //     //     } catch (err) {
+    //     //       console.log(err);
+    //     //     }
+    //     //   };
+        axios({
+            method: "get",
+            url: "http://192.168.1.105:8000/api/v1/appointment/student",
+            headers: {
+            "Authorization": `Bearer ${token}`,
+            "Accept": "application/json"}
+        })
+        .then(function(response){
+            // console.log(response)
+            console.log("jshjsh")
+            let json = response.data;
+            console.log(response.data)
+            if (typeof response.data === "string") json = JSON.parse(response.data)
+            console.log(typeof response.data)
+            setAppointments(json.appointments)
+        })
+        .catch(function(error){
+            console.log("error")
+            let message = Object.values(error.response.data);
+            ToastAndroid(message[0], ToastAndroid.SHORT) 
+        })
+    };
 
     return (
         <AppointmentsContext.Provider value={{appointments, setAppointments}}>
