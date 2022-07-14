f22744d06032d18dc6a3658d5d307b8c789bcf4f
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-14 22:55:09 +0300

    frontend: feat: link the register api with the RegisterScreen

--- frontend/src/screens/RegisterScreen/index.js
+++ frontend/src/screens/RegisterScreen/index.js
@@ -1,10 +1,31 @@
 import React from 'react'
 import { Text, TouchableOpacity, View } from 'react-native';
 import { useNavigation } from '@react-navigation/core';
+import axios from 'axios';
 
 
 export default function RegisterScreen() {
   const navigation = useNavigation()
+  function register(cridentials){
+    console.log(cridentials);
+    axios({
+      method: "post",
+      url: "http://192.168.1.105:8000/api/v1/auth/register",
+      headers: {
+        "Content-type": "application/json",
+      },
+      data: JSON.stringify(cridentials),
+    })
+    .then(function(response){
+      
+    })
+    .catch(function(error){
+      console.log(error)
+      let message = Object.values(error.response.data);
+      alert(message[0]);
+    })
+  };
+
 
   return (
     <>
