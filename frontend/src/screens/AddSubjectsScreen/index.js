53da8d177ece4976aca8384204447dabdc2bac39
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 02:17:20 +0300

    frontend: feat: implement the AddSubjectsScreen subjects local state

--- frontend/src/screens/AddSubjectsScreen/index.js
+++ frontend/src/screens/AddSubjectsScreen/index.js
@@ -1,10 +1,11 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './styles'
 import OnBoardingTitle from '../../components/OnBoardingTitle'
 import OnBoardingPrompt from '../../components/OnBoardingPrompt'
 
 export default function AddSubjectsScreen() {
+    const [subjects, setSubjects] = useState([])
   return (
     <View style={styles.container}>
       <OnBoardingTitle/>
