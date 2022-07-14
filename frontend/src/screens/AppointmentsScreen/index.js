9c0760ed1be85f4a17c47e0672d3a54c3f2cfbbf
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-14 13:49:29 +0300

    frontend: implement the appointment bottom sheet in the appointmentsScreen

--- frontend/src/screens/AppointmentsScreen/index.js
+++ frontend/src/screens/AppointmentsScreen/index.js
@@ -1,14 +1,31 @@
 import { View, Text, TouchableOpacity, FlatList } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styles from './styles'
+import RBSheet from "react-native-raw-bottom-sheet";
 import { useAppointments } from '../../Context/Appointments';
 
 export default function AppointmentsScreen() {
     const {appointments, setAppointments} = useAppointments()
+    const refRBSheet  = useRef();
 
-
+    const [id, setId] = useState()    
 
     return (
+        <>  
+        
+        <RBSheet
+          ref={refRBSheet}
+          closeOnDragDown={true}
+          closeOnPressMask={false}
+          customStyles={{
+            draggableIcon: {
+              backgroundColor: "#000"
+            }
+          }}
+        ><Text style={styles.sheetText}>Appointment action</Text>
+        <TouchableOpacity style={styles.cancel} onPress={()=>{deleteAppointment(id,appointments, setAppointments); refRBSheet.current.close();}}><Text style={styles.buttonText}>Cancel appointment</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.go}><Text style={styles.buttonText}>Go to appointment</Text></TouchableOpacity>
+        </RBSheet>
             <View style={styles.container}>
                 <Text style={styles.title}>Appointments</Text>
                 <FlatList data={appointments} renderItem={(appointmentData) =>{
