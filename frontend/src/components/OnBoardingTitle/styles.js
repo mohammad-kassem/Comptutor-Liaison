f9858b451a78bfcfb3d8292d3aeb11336bc5488b
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 01:57:39 +0300

    frontend: feat: impelement the OnBoardingTitle component styling

---
+++ frontend/src/components/OnBoardingTitle/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    title: {
+        fontSize: 24,
+        fontWeight: "700",
+        lineHeight: 29,
+    },
+})
