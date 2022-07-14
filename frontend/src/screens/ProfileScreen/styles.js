ce546baea3c8e746397cd8a4870e280fe95cae9c
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-14 14:28:56 +0300

    frontend: feat: impelement the ProfileScreen user profile image  styling

---
+++ frontend/src/screens/ProfileScreen/styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "center",
+        backgroundColor: "#f5f5f5",
+        margin: 30,
+    },
+
+    imageContainer: {
+        marginVertical: 75,
+        width: 150,
+        height: 150,
+    },
+
+    profile: {
+        flex: 1,
+        width: undefined,
+        height: undefined,
+        borderRadius: 50
+    },
+
+})
