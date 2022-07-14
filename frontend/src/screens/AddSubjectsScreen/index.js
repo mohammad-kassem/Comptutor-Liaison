4b35f7071e7d62ffc21010b3aef8c6558f771dde
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 02:19:22 +0300

    frontend: feat: display the subjects in the AddSubjectsScreen

--- frontend/src/screens/AddSubjectsScreen/index.js
+++ frontend/src/screens/AddSubjectsScreen/index.js
@@ -1,15 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, FlatList } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import styles from './styles'
 import OnBoardingTitle from '../../components/OnBoardingTitle'
 import OnBoardingPrompt from '../../components/OnBoardingPrompt'
+import { getSubjects } from './controller'
 
 export default function AddSubjectsScreen() {
     const [subjects, setSubjects] = useState([])
-  return (
-    <View style={styles.container}>
-      <OnBoardingTitle/>
-      <OnBoardingPrompt message="Pick your subjects"/>
-    </View>
-  )
+
+    useEffect(function(){
+        getSubjects(setSubjects);
+      }, []);
+
+    return (
+        <View style={styles.container}>
+        <OnBoardingTitle/>
+        <OnBoardingPrompt message="Pick your subjects"/>
+        <FlatList data={subjects} renderItem={(subjectData) =>{
+               return(<Text>{subjectData.item.subject}</Text>
+            )}
+        }
+        />
+        </View>
+    )
 }
