e098d91336745f548df6d1cb0da1bea45d2d706a
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-14 14:28:30 +0300

    frontend: feat: impelement the ProfileScreen user profile image

--- frontend/src/screens/ProfileScreen/index.js
+++ frontend/src/screens/ProfileScreen/index.js
@@ -1,10 +1,13 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import React from 'react'
+import styles from './styles'
 
 export default function ProfileScreen() {
   return (
-    <View>
-      <Text>ProfileScreen</Text>
+    <View style={styles.container}>
+      <View style={styles.imageContainer}>
+            <Image style={styles.profile} source={require('../../../assets/logo.png')}/>
+        </View>
     </View>
   )
 }
