6d2ed82f9726a7ed3f59df29ad602e149adc70ba
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 02:01:11 +0300

    frontend: feat: add the OnBoardingTitle and OnBoardingPrompt to the AddSubjectsScreen

--- frontend/src/screens/AddSubjectsScreen/index.js
+++ frontend/src/screens/AddSubjectsScreen/index.js
@@ -1,10 +1,14 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import styles from './styles'
+import OnBoardingTitle from '../../components/OnBoardingTitle'
+import OnBoardingPrompt from '../../components/OnBoardingPrompt'
 
 export default function AddSubjectsScreen() {
   return (
-    <View>
-      <Text>AddSubjectsScreen</Text>
+    <View style={styles.container}>
+      <OnBoardingTitle/>
+      <OnBoardingPrompt message="Pick your subjects"/>
     </View>
   )
 }
