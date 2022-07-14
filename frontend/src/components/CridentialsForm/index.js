e4521adb5e6583e1ecd9b9cf463d0d570f30022d
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-14 20:26:03 +0300

    frontend: feat: set up the CridentialsForm component

---
+++ frontend/src/components/CridentialsForm/index.js
@@ -0,0 +1,38 @@
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import styles from './styles'
+import { useNavigation } from '@react-navigation/core';
+import { useUser } from '../../Context/User';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+
+export default function CridentialsFrom({login }) {
+    const navigation = useNavigation()
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const {user, setUser} = useUser()
+
+    return (
+      <>
+    <View style={styles.container}>
+      <View style={styles.messageContainer}>
+        <Text style={styles.messageTitle}>Hello Again!</Text>
+        <Text style={styles.messageContent}>Welcome back you've been missed!</Text>
+      </View>
+      
+      <TextInput style={styles.input} placeholder="Email" onChangeText={(enteredText)=>{setEmail(enteredText)}}/>
+      
+      <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} onChangeText={(enteredText)=>{setPassword(enteredText)}}/>
+      <TouchableOpacity containerStyle={styles.fullWidthButton} onPress={()=>login({email, password})}>
+        <Text style={styles.fullWidthButtonText}>Log in</Text>
+      </TouchableOpacity>
+      <View style={styles.registerMessage}>
+        <Text>Not a member?</Text>
+        <TouchableOpacity>
+          <Text style={styles.registerLink} onPress={()=>navigation.navigate("RegisterScreen")}>Register</Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+    </>
+    )
+}
