f843dda92480b6cf1af2741d5383823c69feb74d
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 20:38:24 +0300

    frontend: feat: get the user info from route in the AddDegreeScreen

--- frontend/src/screens/AddDegreeScreen/index.js
+++ frontend/src/screens/AddDegreeScreen/index.js
@@ -6,10 +6,15 @@ import OnBoardingPrompt from '../../components/OnBoardingPrompt'
 import { TextInput } from 'react-native-gesture-handler'
 import FullWidthButton from '../../components/FullWidthButton'
 import { addDegree } from './controller'
+import { useNavigation } from '@react-navigation/native'
+import { useUser } from '../../Context/User'
 
-export default function AddDegreeScreen() {
+export default function AddDegreeScreen( {route} ) {
+    const user = route.params.user
     const [university, setUniversity] = useState("")
     const [degree, setDegree] = useState("")
+    const navigation = useNavigation()
+    const {setUser} = useUser()
 
   return (
     <View style={styles.container}>
@@ -21,7 +26,7 @@ export default function AddDegreeScreen() {
         <Text style={styles.inputTitle}>Degree</Text>
         <TextInput style={styles.input} placeholder="Degree" onChangeText={(enteredText)=>{setDegree(enteredText)}}/>
         <View style={styles.buttonContainer}>
-            <FullWidthButton text="Next" onHandlePress={()=>addDegree({university, degree})}/> 
+            <FullWidthButton text="Next" onHandlePress={()=>addDegree({university, degree}, user, setUser)}/> 
         </View>
     </View>
   )
