c137b178b53f9b2e151b072791274542e66940be
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 14:42:38 +0300

    frontend: feat: set up the AddDegreeScreen

---
+++ frontend/src/screens/AddDegreeScreen/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AddDegreeScreen() {
+  return (
+    <View>
+      <Text>AddDegreeScreen</Text>
+    </View>
+  )
+}
