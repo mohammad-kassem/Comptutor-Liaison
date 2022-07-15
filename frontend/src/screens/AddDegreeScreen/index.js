6b793888b4d7e9023596b0f54d32021b8550e8a3
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 14:54:21 +0300

    frontend: feat: add the OnBoardingPrompt component to the AddDegreeScreen

--- frontend/src/screens/AddDegreeScreen/index.js
+++ frontend/src/screens/AddDegreeScreen/index.js
@@ -2,11 +2,13 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import styles from './styles'
 import OnBoardingTitle from '../../components/OnBoardingTitle'
+import OnBoardingPrompt from '../../components/OnBoardingPrompt'
 
 export default function AddDegreeScreen() {
   return (
     <View style={styles.container}>
         <OnBoardingTitle/>
+        <OnBoardingPrompt message="Complete your info"/>
     </View>
   )
 }
