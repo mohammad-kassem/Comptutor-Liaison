bea2b2c065a2c4260e39bab1713ed4a157a1469f
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 15:48:48 +0300

    frontend: feat: implement the AddDegreeScreen university text input

--- frontend/src/screens/AddDegreeScreen/index.js
+++ frontend/src/screens/AddDegreeScreen/index.js
@@ -1,15 +1,18 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles'
 import OnBoardingTitle from '../../components/OnBoardingTitle'
 import OnBoardingPrompt from '../../components/OnBoardingPrompt'
 
 export default function AddDegreeScreen() {
+    const [university, setUniversity] = useState("")
   return (
     <View style={styles.container}>
         <OnBoardingTitle/>
         <OnBoardingPrompt message="Complete your info"/>
         <Text style={styles.sectionPrompt}>Add your degree to improve your opportunities</Text>
+        <Text style={styles.inputTitle}>University</Text>
+        <TextInput style={styles.input} placeholder="University or school" onChangeText={(enteredText)=>{setUniversity(enteredText)}}/>
     </View>
   )
 }
