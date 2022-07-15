d77bb159ada50a7251ba6c813db5232eae9dbeae
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 03:28:20 +0300

    frontend: feat: add the selectedSubjects local state to the AddSubjectsScreen

--- frontend/src/screens/AddSubjectsScreen/index.js
+++ frontend/src/screens/AddSubjectsScreen/index.js
@@ -1,4 +1,4 @@
-import { View, Text, FlatList, Image } from 'react-native'
+import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import styles from './styles'
 import OnBoardingTitle from '../../components/OnBoardingTitle'
@@ -7,6 +7,7 @@ import { getSubjects } from './controller'
 
 export default function AddSubjectsScreen() {
     const [subjects, setSubjects] = useState([])
+    const [selectedSubjects, setSelectedSubjects] = useState([])
     const numColumns = 2
     useEffect(function(){
         getSubjects(setSubjects);
@@ -18,9 +19,9 @@ export default function AddSubjectsScreen() {
         <OnBoardingPrompt message="Pick your subjects"/>
         <FlatList data={subjects} numColumns={numColumns} columnWrapperStyle={styles.subjectsContainer} renderItem={(subjectData) =>{
                return(
-                <View style={styles.imageContainer}>
+                <TouchableOpacity style={styles.imageContainer}>
                     <Image style={styles.tutorProfile} source={{uri:subjectData.item.image,}}/>
-                </View>
+                </TouchableOpacity>
             )}
         }
         />
