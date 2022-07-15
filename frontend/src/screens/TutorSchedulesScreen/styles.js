b81666c50e37656b8b4e930d37e3b413790d8a11
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 20:50:14 +0300

    frontend: feat: implement the TutorScheduleScreen styling

---
+++ frontend/src/screens/TutorSchedulesScreen/styles.js
@@ -0,0 +1,64 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        backgroundColor: "#f5f5f5",
+        paddingVertical: 25,
+        paddingHorizontal: "10%"
+    },
+
+    title: {
+        fontSize: 24,
+        fontWeight: "700",
+        lineHeight: 29
+    },
+
+    date: {
+        fontSize: 20,
+        lineHeight: 23,
+        fontWeight: "500",
+        borderBottomWidth: 1,
+        paddingTop: 5,
+    },
+
+    timesContainer: {
+        justifyContent: "flex-start",
+        alignItems: "center",
+        flexDirection: "row",
+        flexWrap: "wrap",
+    },
+
+    timeCardLeft: {
+        marginTop: 30,
+        width: "50%",
+        height: 50,
+        justifyContent: "center",
+        alignItems: "flex-start",
+    },
+
+    timeCardRight: {
+        marginTop: 30,
+        width: "50%",
+        height: 50,
+        justifyContent: "center",
+        alignItems: "flex-end",
+
+    },
+
+    timeContainer :{
+        backgroundColor: "white",
+        width: 100,
+        height: 50,
+        borderRadius: 10,
+        elevation: 5,
+        justifyContent: "center",
+    },
+
+    time: {
+        textAlign: "center",
+        textAlignVertical: "center"
+    },
+})
