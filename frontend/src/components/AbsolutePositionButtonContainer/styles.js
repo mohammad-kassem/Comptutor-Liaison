30e0a68d2b869f6d9a3470cb1664232e0b2a7a93
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 17:34:37 +0300

    frontend: feat: imeplement the AbsolutePositionButtonContainer componenet

---
+++ frontend/src/components/AbsolutePositionButtonContainer/styles.js
@@ -0,0 +1,10 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    buttonContainer: {
+        position: "absolute",
+        right: 15,
+        left:15,
+        bottom: 30,
+    },
+})
