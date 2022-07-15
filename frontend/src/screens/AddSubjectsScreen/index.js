4823ec300de041c1cab5c0040fd570055239a098
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 04:23:15 +0300

    frontend: feat: use the is selected function in the AddSubjectsScreen to toggle the icon

--- frontend/src/screens/AddSubjectsScreen/index.js
+++ frontend/src/screens/AddSubjectsScreen/index.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from 'react'
 import styles from './styles'
 import OnBoardingTitle from '../../components/OnBoardingTitle'
 import OnBoardingPrompt from '../../components/OnBoardingPrompt'
-import { getSubjects, handleSelect } from './controller'
+import { getSubjects, handleSelect, isSelected } from './controller'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 export default function AddSubjectsScreen() {
@@ -24,9 +24,10 @@ export default function AddSubjectsScreen() {
                return(
                 <TouchableOpacity style={styles.imageContainer} onPress={()=>handleSelect(subjectData.item.id, selectedSubjects, setSelectedSubjects)}>
                     <Image style={styles.subjectImage} source={{uri:subjectData.item.image,}}/>
+                    {isSelected(subjectData.item.id, selectedSubjects) &&
                     <View style={styles.icon}>
                         <Icon name="checkbox-marked-circle" size={24} color="#1877F2"/>
-                    </View>
+                    </View>}
                 </TouchableOpacity>
             )}
         }
