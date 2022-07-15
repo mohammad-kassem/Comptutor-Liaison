3527e78fd3ecc244fa1c29b20e6eb5b1c359e4fd
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 15:49:05 +0300

    frontend: feat: implement the AddDegreeScreen degree text input

--- frontend/src/screens/AddDegreeScreen/index.js
+++ frontend/src/screens/AddDegreeScreen/index.js
@@ -3,9 +3,12 @@ import React, { useState } from 'react'
 import styles from './styles'
 import OnBoardingTitle from '../../components/OnBoardingTitle'
 import OnBoardingPrompt from '../../components/OnBoardingPrompt'
+import { TextInput } from 'react-native-gesture-handler'
 
 export default function AddDegreeScreen() {
     const [university, setUniversity] = useState("")
+    const [degree, setDegree] = useState("")
+
   return (
     <View style={styles.container}>
         <OnBoardingTitle/>
@@ -13,6 +16,8 @@ export default function AddDegreeScreen() {
         <Text style={styles.sectionPrompt}>Add your degree to improve your opportunities</Text>
         <Text style={styles.inputTitle}>University</Text>
         <TextInput style={styles.input} placeholder="University or school" onChangeText={(enteredText)=>{setUniversity(enteredText)}}/>
+        <Text style={styles.inputTitle}>Degree</Text>
+        <TextInput style={styles.input} placeholder="Degree" onChangeText={(enteredText)=>{setDegree(enteredText)}}/> 
     </View>
   )
 }
