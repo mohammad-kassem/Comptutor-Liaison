b4b8bd4bcbaea210de0ba686d231a41742db8ae8
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-15 14:53:11 +0300

    frontend: feat: add the OnBoardingTitle component to the AddDegreeScreen

--- frontend/src/screens/AddDegreeScreen/index.js
+++ frontend/src/screens/AddDegreeScreen/index.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import styles from './styles'
+import OnBoardingTitle from '../../components/OnBoardingTitle'
 
 export default function AddDegreeScreen() {
   return (
-    <View>
-      <Text>AddDegreeScreen</Text>
+    <View style={styles.container}>
+        <OnBoardingTitle/>
     </View>
   )
 }
