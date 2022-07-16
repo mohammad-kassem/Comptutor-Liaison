9e175a92a36515af7d6a759c99106d5a396d6efa
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 02:00:26 +0300

    frontend: refactor: clean code and fix indentation of ScheduleScreen controller

--- frontend/src/screens/ScheduleScreen/controller.js
+++ frontend/src/screens/ScheduleScreen/controller.js
@@ -4,38 +4,32 @@ import { getToken } from '../../components/utility/Token';
 import axios from 'axios';
 
 export function groupSchedules(schedules) {
-  let groupedSchedule = []
-  if (schedules.length !== 0){
-    groupedSchedule = schedules.reduce(function (r, a) {
-        r[a.date] = r[a.date] || [];
-        r[a.date].push(a);
-        return r;
-    }, Object.create(null));
-  }
-    return groupedSchedule
+	let groupedSchedule = []
+	if (schedules.length !== 0){
+		groupedSchedule = schedules.reduce(function (r, a) {
+			r[a.date] = r[a.date] || [];
+			r[a.date].push(a);
+			return r;
+		}, Object.create(null));
+	}
+	return groupedSchedule
 }
 
 export async function getAvailableTimes(tutorId, setSchedules) {
-    // data = new FormData()
-    // data.append("schedule_id", 28)
     const token = await getToken()
-    console.log(token);
-    axios({
-        method: "get",
-        url: `http://192.168.1.105:8000/api/v1/tutor/schedule/available/${tutorId}`,
-        headers: {
-          "Content-type": "application/json",
-          "Authorization": `Bearer ${token}`
-        },
-      })
-      .then(function(response){
-        console.log("hello")
-        setSchedules(response.data.available_times)
-        // alert(await response.data.message);
-      })
-      .catch(error=>console.log(error))
-        // console.log("error")
-        // let message = Object.values( await error.response.data);
-        // alert(message[0]);
-      
+	axios({
+		method: "get",
+		url: `http://192.168.1.105:8000/api/v1/tutor/schedule/available/${tutorId}`,
+		headers: {
+		"Content-type": "application/json",
+		"Authorization": `Bearer ${token}`
+		},
+	})
+	.then(function(response){
+		setSchedules(response.data.available_times)
+	})
+	.catch(error=>console.log(error))
+		// let message = Object.values( await error.response.data);
+		// alert(message[0]);
+	
 };
