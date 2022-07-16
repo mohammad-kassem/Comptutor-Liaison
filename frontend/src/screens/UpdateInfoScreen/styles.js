de040df18408a944d34b228ec3c2c413b49ad8a1
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 18:06:44 +0300

    frontend: feat: impelement the UpdateInfoScreen section prompt styling

---
+++ frontend/src/screens/UpdateInfoScreen/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        backgroundColor: "#f5f5f5",
+        paddingVertical: 25,
+        paddingHorizontal: "10%"
+    },
+
+    sectionPrompt: {
+        fontSize: 20,
+        fontWeight: "500",
+        marginVertical: 30
+    },
