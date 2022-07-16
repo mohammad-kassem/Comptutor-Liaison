d61043439f5f98b1bf430b4323d9916573cb4e73
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 09:45:10 +0300

    frontend: feat: update the date and time pickers in the AddScheduleScreen

--- frontend/src/screens/AddScheduleScreen/index.js
+++ frontend/src/screens/AddScheduleScreen/index.js
@@ -2,7 +2,7 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import styles from './styles'
 import { toDateString, toTimeString } from './controller'
-import DateTimePicker from '@react-native-community/datetimepicker';
+import DateTimePicker from 'react-native-modal-datetime-picker';
 
 export default function AddScheduleScreen() {
     let [date, setDate] = useState(new Date())
@@ -11,8 +11,8 @@ export default function AddScheduleScreen() {
 
     return (
         <View style={styles.container}>
-            <DateTimePicker mode="date" value={date} minimumDate={new Date()} onChange={(e, d)=>setDate(d)}/>
-            <DateTimePicker mode="time" value={time} is24Hour={true} onChange={(e, t)=>setTime(t)}/>
+            <DateTimePicker mode="date" value={date} isVisible={datePickerIsVisable} minimumDate={new Date()} onConfirm={(d)=>{setDatePickerIsVisable(false); setDate(d)}} onCancel={()=>setDatePickerIsVisable(false)}/>
+            <DateTimePicker mode="time" value={time} isVisible={timePickerIsVisable} is24Hour={true} onConfirm={(t)=>{setTimePickerIsVisable(false); setTime(t)}} onCancel={()=>setTimePickerIsVisable(false)}/>
             <Text style={styles.sectionPrompt}>Add your available time</Text>
             <Text style={styles.title}>Duration</Text>
             <Text style={styles.field}>1 hour</Text>
