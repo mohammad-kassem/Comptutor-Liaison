50290d9d4d8d7e25cf3a4677dab9ab755c467339
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:49:56 +0300

    frontend: refactor: clean code and fix indentation of AppointmentModal controller

--- frontend/src/components/AppointmentModal/controller.js
+++ frontend/src/components/AppointmentModal/controller.js
@@ -4,36 +4,30 @@ import axios from 'axios';
 import { getToken } from '../utility/Token';
 
 export async function addAppointment(scheduleId, schedules, setSchedules, tutorId) {
-    console.log(scheduleId);
-    let data = {"schedule_id": scheduleId, "tutor_id": tutorId}
-    // data = new FormData()
-    // data.append("schedule_id", 28)
-    const token = await getToken()
-    console.log(token);
-    axios({
-        method: "post",
-        url: "http://192.168.1.105:8000/api/v1/appointment/add",
-        headers: {
-          "Content-type": "application/json",
-          "Authorization": `Bearer ${token}`
-        },
-        data: data,
-      })
-      .then(async function(response){
-        console.log("hello")
-        refreshAvailableTimes(scheduleId, schedules, setSchedules)
-        ToastAndroid.show(response.data.message, ToastAndroid.SHORT)
-      })
-      .catch(function(error){
-        // console.log("error")
-        let message = Object.values(error.response.data);
-        ToastAndroid.show(message[0], ToastAndroid.SHORT);
-      })
-};
+	let data = {"schedule_id": scheduleId, "tutor_id": tutorId}
+	const token = await getToken()
+	axios({
+		method: "post",
+		url: "http://192.168.1.105:8000/api/v1/appointment/add",
+		headers: {
+			"Content-type": "application/json",
+			"Authorization": `Bearer ${token}`
+		},
+		data: data,
+		})
+		.then(async function(response){
+		console.log("hello")
+		refreshAvailableTimes(scheduleId, schedules, setSchedules)
+		ToastAndroid.show(response.data.message, ToastAndroid.SHORT)
+		})
+		.catch(function(error){
+		let message = Object.values(error.response.data);
+		ToastAndroid.show(message[0], ToastAndroid.SHORT);
+		})
+	};
 
-export function refreshAvailableTimes(scheduleId, schedules, setSchedules){
-  // console.log(schedules)
-  setSchedules(schedules.filter((schedule)=>
-    schedule.id !== scheduleId
-  ))
-}
+	export function refreshAvailableTimes(scheduleId, schedules, setSchedules){
+	setSchedules(schedules.filter((schedule)=>
+	schedule.id !== scheduleId
+	))
+	}
