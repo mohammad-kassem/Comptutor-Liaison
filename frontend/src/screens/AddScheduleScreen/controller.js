7e9674a59a77664b96d6e4fac27ccadaa94113ec
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 11:22:28 +0300

    fronted: fix: change to date string function return to a valida date format

--- frontend/src/screens/AddScheduleScreen/controller.js
+++ frontend/src/screens/AddScheduleScreen/controller.js
@@ -2,7 +2,7 @@ import { View, Text } from 'react-native'
 import React from 'react'
 
 export function toDateString(date) {
-    return `${date.getFullYear()} - ${date.getMonth() + 1} - ${date.getDate()}`
+    return `${date.getFullYear()}-${date.getMonth() < 9 ? "0" + (date.getMonth() + 1) : date.getMonth() + 1}-${date.getDate() < 10 ? "0" + date.getDate() : date.getDate()}`
 }
 
 export function toTimeString(date) {
