6fcc4a73143511d79f3b3c2d9d1e094eced19e42
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:57:54 +0300

    frontend: refactor: clean code and fix indentation of AddSubjectsScreen controller

--- frontend/src/screens/AddSubjectsScreen/controller.js
+++ frontend/src/screens/AddSubjectsScreen/controller.js
@@ -2,11 +2,9 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import { getToken } from '../../components/utility/Token';
 import axios from 'axios';
-import { useNavigation } from '@react-navigation/native';
 
 export async function getSubjects(setSubjects) {
     const token = await getToken()
-    console.log(token);
     axios({
         method: "get",
         url: "http://192.168.1.105:8000/api/v1/subject",
@@ -16,9 +14,7 @@ export async function getSubjects(setSubjects) {
         },
       })
       .then(function(response){
-        console.log("hello")
         setSubjects(response.data.subjects)
-        // alert(await response.data.message);
       })
       .catch(error=>console.log(error))
 }
@@ -50,7 +46,6 @@ export function isSelected(id, selectedSubjects){
 
 export async function addSelectedSubjects(selectedSubjects, navigation, user, setUser){
     const token = await getToken()
-    // console.log(selectedSubjects);
     axios({
         method: "post",
         url: "http://192.168.1.105:8000/api/v1/subject/add",
@@ -65,7 +60,6 @@ export async function addSelectedSubjects(selectedSubjects, navigation, user, se
             user.role_id === 1 ? setUser(user) : navigation.navigate("OnBoardingStackTutor", { screen: "AddDegreeScreen",  params: { user: user },})
         })
         .catch(function(error){
-            console.log(error)
             let message = Object.values(error.response.data);
             alert(message[0]);
         })
