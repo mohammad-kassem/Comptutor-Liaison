8321af4fd725c0e0572e5ede64a7284ec7aed229
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 02:01:30 +0300

    frontend: refactor: clean code and fix indentation of TutorScreen controller

--- frontend/src/screens/TutorScreen/controller.js
+++ frontend/src/screens/TutorScreen/controller.js
@@ -1,31 +1,28 @@
 import { View, Text } from 'react-native'
 import React from 'react'
-import { useNavigation } from '@react-navigation/native'
 import { getToken } from '../../components/utility/Token'
 import axios from 'axios'
 
 export function onHandlePress(tutor, navigation) {
     navigation.navigate("ScheduleScreen", {"tutor": tutor})
-    console.log("hello")
 }
 
 export async function getTutor(id, setTutor){
     const token = await getToken();
-        axios({
-          method: "get",
-          url:`http://192.168.1.105:8000/api/v1/tutor/get/${id}`,
-          headers: {
-            "Content-type": "application/json",
-            "Authorization": `Bearer ${token}`}
-        })
-        .then(function(response){
-            // console.log(response.data.tutors);
-          setTutor(response.data.tutors);
-        })
-        .catch(function(error){
-          let message = Object.values(error.response.data);
-          alert(error);
-          console.log("error", error)
-        })
+    axios({
+        method: "get",
+        url:`http://192.168.1.105:8000/api/v1/tutor/get/${id}`,
+        headers: {
+        "Content-type": "application/json",
+        "Authorization": `Bearer ${token}`
+        }
+    })
+    .then(function(response){
+        setTutor(response.data.tutors);
+    })
+    .catch(function(error){
+        let message = Object.values(error.response.data);
+        alert(error);
+    })
 }
 
