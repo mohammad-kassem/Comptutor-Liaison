e52c82da393f5f0123f51df7178c3e1c722c8a6b
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 08:51:23 +0300

    frontend: feat: implement the date and time pickers in the AddScheduleScreen

--- frontend/src/screens/AddScheduleScreen/index.js
+++ frontend/src/screens/AddScheduleScreen/index.js
@@ -2,26 +2,27 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import React, { useState } from 'react'
 import styles from './styles'
 import { toDateString, toTimeString } from './controller'
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function AddScheduleScreen() {
     let [date, setDate] = useState(new Date())
     let [time, setTime] = useState(new Date())
-
-    date = toDateString(date)
-    time = toTimeString(time)   
+   
 
     return (
         <View style={styles.container}>
+            <DateTimePicker mode="date" value={date} minimumDate={new Date()} onChange={(e, d)=>setDate(d)}/>
+            <DateTimePicker mode="time" value={time} is24Hour={true} onChange={(e, t)=>setTime(t)}/>
             <Text style={styles.sectionPrompt}>Add your available time</Text>
             <Text style={styles.title}>Duration</Text>
             <Text style={styles.field}>1 hour</Text>
             <Text style={styles.title}>Date</Text>
             <TouchableOpacity style={styles.field}>
-                <Text style={styles.fieldText}>{date}</Text>
+                <Text style={styles.fieldText}>{toDateString(date)}</Text>
             </TouchableOpacity>
             <Text style={styles.title}>Start time</Text>
             <TouchableOpacity style={styles.field}>
-                <Text style={styles.fieldText}>{time}</Text>
+                <Text style={styles.fieldText}>{toTimeString(time)}</Text>
             </TouchableOpacity>
         </View>
     )
