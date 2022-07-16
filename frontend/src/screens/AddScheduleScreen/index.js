c915f6135118e32529a8a2aca61d7a170fbfd666
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 08:01:37 +0300

    frontend: feat: add the AddScheduleScreen date and time local states

--- frontend/src/screens/AddScheduleScreen/index.js
+++ frontend/src/screens/AddScheduleScreen/index.js
@@ -1,17 +1,21 @@
 import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles'
 
 export default function AddScheduleScreen() {
-  return (
-    <View style={styles.container}>
-        <Text style={styles.sectionPrompt}>Add your available time</Text>
-        <Text style={styles.title}>Duration</Text>
-        <Text style={styles.field}>1 hour</Text>
-        <Text style={styles.title}>Date</Text>
-        <Text style={styles.field}>2022-07-18</Text>
-        <Text style={styles.title}>Start time</Text>
-        <Text style={styles.field}>13:00</Text>
-    </View>
-  )
+    const [date, setDate] = useState(new Date().toDateString())
+    const [time, setTime] = useState(new Date().toTimeString())
+
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.sectionPrompt}>Add your available time</Text>
+            <Text style={styles.title}>Duration</Text>
+            <Text style={styles.field}>1 hour</Text>
+            <Text style={styles.title}>Date</Text>
+            <Text style={styles.field}>{date}</Text>
+            <Text style={styles.title}>Start time</Text>
+            <Text style={styles.field}>{time}</Text>
+        </View>
+    )
 }
