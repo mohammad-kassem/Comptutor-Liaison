edb116ff94f8e7f0da3d534b31858cb90959813b
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 07:25:30 +0300

    frontend: feat: set up the AddScheduleScreen

---
+++ frontend/src/screens/AddScheduleScreen/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function AddScheduleScreen() {
+  return (
+    <View>
+      <Text>AddScheduleScreen</Text>
+    </View>
+  )
+}
