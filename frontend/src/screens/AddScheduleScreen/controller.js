931f64c5d3f5205e8b76e7fb55cc7d47121d7a72
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 13:26:55 +0300

    frontend: feat: imeplement the schedule is clashing checker function

--- frontend/src/screens/AddScheduleScreen/controller.js
+++ frontend/src/screens/AddScheduleScreen/controller.js
@@ -15,8 +15,9 @@ export async function addSchedule(date, time, schedules, setSchedules, duration 
     const token = await getToken()
     const end_time = new Date(time.getTime() + (duration * 60 * 60 * 1000))
     console.log(toTimeString(time), toTimeString(end_time), toDateString(date))
-    const schedule = [{"date": toDateString(date), "start_time": toTimeString(time), "end_time": toTimeString(end_time)}]
-    let hours = {"hours": schedule}
+    const schedule = {"date": toDateString(date), "start_time": toTimeString(time), "end_time": toTimeString(end_time)}
+    if (scheduleIsClashing(schedule, schedules)) {alert("Schedule intervals are clashing"); return}
+    let hours = {"hours": [schedule]}
     axios({
         method: "post",
         url: "http://192.168.1.105:8000/api/v1/tutor/schedule/add",
@@ -36,4 +37,16 @@ export async function addSchedule(date, time, schedules, setSchedules, duration 
             let message = Object.values(error.response.data);
             alert(message[0]);
     })
+}
+
+export function scheduleIsClashing(schedule, schedules){
+    for (let i = 0; i < schedules.length; i++){
+        const startTime = new Date(schedules[i].date + "T" + schedules[i].start_time)
+        const endTime = new Date(schedules[i].date + "T" + schedules[i].end_time)
+        
+        const scheduleStartTime = new Date(schedule.date + "T" + schedule.start_time)
+        const scheduleEndTime = new Date(schedule.date + "T" + schedule.end_time)
+
+        if (scheduleStartTime.getTime() <= endTime.getTime() && scheduleEndTime.getTime() >= startTime.getTime()) return true 
+    }
 }
