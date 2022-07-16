c4042b89e39de6995b1203c4e36d72b6747987d1
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 19:35:23 +0300

    frontend: feat: link the update tutor info api to the UpdateInfoScreen

---
+++ frontend/src/screens/UpdateInfoScreen/controller.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+import { getToken } from "../../components/utility/Token";
+
+export async function addInfo(input, user, setUser){
+    const token = await getToken()
+    axios({
+        method: "put",
+        url: "http://192.168.1.105:8000/api/v1/tutor/update",
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": `Bearer ${token}`
+        },
+        data: JSON.stringify(input) 
+        })
+        .then(function(response){
+            setUser(user)
+        })
+        .catch(function(error){
+            console.log(error)
+            let message = Object.values(error.response.data);
+            alert(message[0]);
+        })
+    }
