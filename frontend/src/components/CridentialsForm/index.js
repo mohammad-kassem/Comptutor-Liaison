a5ffd9dfd55696a17d5dae65a7e766cff5d8e400
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:51:25 +0300

    frontend: refactor: clean code and fix indentation of CridentialsForm

--- frontend/src/components/CridentialsForm/index.js
+++ frontend/src/components/CridentialsForm/index.js
@@ -2,7 +2,6 @@ import { View, Text, TextInput } from 'react-native'
 import React, { useState } from 'react'
 import styles from './styles'
 import { useNavigation } from '@react-navigation/core';
-import { useUser } from '../../Context/User';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import Checkbox from 'expo-checkbox';
 
@@ -16,52 +15,52 @@ export default function CridentialsFrom({type, onPressHandler }) {
     const [isTutor, setIsTutor] = useState(false)
 
     return (
-      <>
-    <View style={styles.container}>
-      <View style={styles.messageContainer}>
-        <Text style={styles.messageTitle}>{type === "login" ? "Hello Again!" : "Hello friend!"}</Text>
-        <Text style={styles.messageContent}>{type === "login" ? "Welcome back you've been missed!" : "Welcome to Comptutor"}</Text>
-      </View>
-      {type === "register" && 
-      <>
-      <TextInput style={styles.input} placeholder="First name" onChangeText={(enteredText)=>{setFname(enteredText)}}/>
-      <TextInput style={styles.input} placeholder="Last name" onChangeText={(enteredText)=>{setLname(enteredText)}}/>
-      </>
-      }
-      <TextInput style={styles.input} placeholder="Email" onChangeText={(enteredText)=>{setEmail(enteredText)}}/>
-      <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} onChangeText={(enteredText)=>{setPassword(enteredText)}}/>
-
-      {type === "login" ?
-      (<>
-      <TouchableOpacity containerStyle={styles.fullWidthButton} onPress={()=>onPressHandler({email, password})}>
-        <Text style={styles.fullWidthButtonText}>Log in</Text>
-      </TouchableOpacity> 
-      <View style={styles.navigationMessage}>
-        <Text>Not a member?</Text>
-        <TouchableOpacity>
-          <Text style={styles.navigationLink} onPress={()=>navigation.navigate("RegisterScreen")}> Register</Text>
-        </TouchableOpacity> 
-        </View></>) : (
-        <>
-        <View style={styles.checkboxContainer}>
-          <Checkbox
-            disabled={false}
-            value={isTutor}
-            color={"#1877F2"}
-            onValueChange={(newValue) => setIsTutor(newValue)}
-          />
-          <Text style={styles.isTutorMessage}>Register as tutor</Text>
+    <>
+        <View style={styles.container}>
+        <View style={styles.messageContainer}>
+            <Text style={styles.messageTitle}>{type === "login" ? "Hello Again!" : "Hello friend!"}</Text>
+            <Text style={styles.messageContent}>{type === "login" ? "Welcome back you've been missed!" : "Welcome to Comptutor"}</Text>
         </View>
-        <TouchableOpacity containerStyle={styles.fullWidthButton} onPress={()=>onPressHandler({fname, lname, email, password, is_tutor: isTutor})}>
-          <Text style={styles.fullWidthButtonText}>Register</Text>
-        </TouchableOpacity>
+        {type === "register" && 
+        <>
+        <TextInput style={styles.input} placeholder="First name" onChangeText={(enteredText)=>{setFname(enteredText)}}/>
+        <TextInput style={styles.input} placeholder="Last name" onChangeText={(enteredText)=>{setLname(enteredText)}}/>
+        </>
+        }
+        <TextInput style={styles.input} placeholder="Email" onChangeText={(enteredText)=>{setEmail(enteredText)}}/>
+        <TextInput style={styles.input} placeholder="Password" secureTextEntry={true} onChangeText={(enteredText)=>{setPassword(enteredText)}}/>
+
+        {type === "login" ?
+        (<>
+        <TouchableOpacity containerStyle={styles.fullWidthButton} onPress={()=>onPressHandler({email, password})}>
+            <Text style={styles.fullWidthButtonText}>Log in</Text>
+        </TouchableOpacity> 
         <View style={styles.navigationMessage}>
-        <Text>Already a member?</Text>
-        <TouchableOpacity>
-          <Text style={styles.navigationLink} onPress={()=>navigation.navigate("LoginScreen")}> Login</Text>
-        </TouchableOpacity>
-        </View></>)}
-    </View>
+            <Text>Not a member?</Text>
+            <TouchableOpacity>
+            <Text style={styles.navigationLink} onPress={()=>navigation.navigate("RegisterScreen")}> Register</Text>
+            </TouchableOpacity> 
+            </View></>) : (
+            <>
+            <View style={styles.checkboxContainer}>
+            <Checkbox
+                disabled={false}
+                value={isTutor}
+                color={"#1877F2"}
+                onValueChange={(newValue) => setIsTutor(newValue)}
+            />
+            <Text style={styles.isTutorMessage}>Register as tutor</Text>
+            </View>
+            <TouchableOpacity containerStyle={styles.fullWidthButton} onPress={()=>onPressHandler({fname, lname, email, password, is_tutor: isTutor})}>
+            <Text style={styles.fullWidthButtonText}>Register</Text>
+            </TouchableOpacity>
+            <View style={styles.navigationMessage}>
+            <Text>Already a member?</Text>
+            <TouchableOpacity>
+            <Text style={styles.navigationLink} onPress={()=>navigation.navigate("LoginScreen")}> Login</Text>
+            </TouchableOpacity>
+            </View></>)}
+        </View>
     </>
     )
 }
