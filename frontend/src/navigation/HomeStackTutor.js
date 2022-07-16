bdfb535a55c43ffa2608d063352a6c2117dbd9ef
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:55:40 +0300

    frontend: refactor: clean code and fix indentation of HomeStackTutor

--- frontend/src/navigation/HomeStackTutor.js
+++ frontend/src/navigation/HomeStackTutor.js
@@ -8,14 +8,11 @@ export default function HomeStackTutor() {
     const RootStackNav = createStackNavigator();
 
     return (
-        // <NavigationContainer>
         <RootStackNav.Navigator>
-            
             <RootStackNav.Screen
             name="AddScheduleScreen"
             component={AddScheduleScreen}
             />
         </RootStackNav.Navigator>
-        // </NavigationContainer>
     )
 }
