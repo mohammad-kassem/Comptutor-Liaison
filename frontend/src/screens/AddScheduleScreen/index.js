b413f2589271f461d239069bcf9361385716adca
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 07:50:46 +0300

    frontend: feat: implement the AdfdScheduleScreen titles and fields

--- frontend/src/screens/AddScheduleScreen/index.js
+++ frontend/src/screens/AddScheduleScreen/index.js
@@ -1,10 +1,17 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import styles from './styles'
 
 export default function AddScheduleScreen() {
   return (
-    <View>
-      <Text>AddScheduleScreen</Text>
+    <View style={styles.container}>
+        <Text style={styles.sectionPrompt}>Add your available time</Text>
+        <Text style={styles.title}>Duration</Text>
+        <Text style={styles.field}>1 hour</Text>
+        <Text style={styles.title}>Date</Text>
+        <Text style={styles.field}>2022-07-18</Text>
+        <Text style={styles.title}>Start time</Text>
+        <Text style={styles.field}>13:00</Text>
     </View>
   )
 }
