a20b6c65dfd2d761ff0fa89f9498f1e783e43493
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:54:38 +0300

    frontend: refactor: clean code and fix indentation of BottomTabsStack

--- frontend/src/navigation/BottomTabsStack.js
+++ frontend/src/navigation/BottomTabsStack.js
@@ -1,10 +1,7 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator }  from '@react-navigation/bottom-tabs';
 import HomeScreen from "../screens/HomeScreen";
-import LoginScreen from '../screens/LoginScreen';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import HomeStack from './HomeStack';
 import AppointmentsScreen from '../screens/AppointmentsScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 
@@ -14,33 +11,31 @@ import ProfileScreen from '../screens/ProfileScreen';
 export default function BottomTabsStack() {
     const Tab = createBottomTabNavigator();
     return (
-      // <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen options={{
-          tabBarIcon: () => {
-            return <Icon name="home" size={24} color={"#1877F2"}/>;
-          },
-          tabBarShowLabel: false,
-          tabBarStyle: { height: 56 },
-          headerShown: false
-        }} name="Home" component={HomeScreen} />
-        <Tab.Screen options={{
-          tabBarIcon: () => {
-            return <Icon name="calendar" size={24} color={"#1877F2"}/>;
-          },
-          tabBarShowLabel: false,
-          tabBarStyle: { height: 56 },
-          headerShown: false
-        }} name="Appointments" component={AppointmentsScreen} />
-        <Tab.Screen options={{
-          tabBarIcon: () => {
-            return <Icon name="user" size={24} color={"#1877F2"}/>;
-          },
-          tabBarShowLabel: false,
-          tabBarStyle: { height: 56 },
-          headerShown: false
-        }} name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    // </NavigationContainer>
+		<Tab.Navigator>
+			<Tab.Screen options={{
+			tabBarIcon: () => {
+				return <Icon name="home" size={24} color={"#1877F2"}/>;
+			},
+			tabBarShowLabel: false,
+			tabBarStyle: { height: 56 },
+			headerShown: false
+			}} name="Home" component={HomeScreen} />
+			<Tab.Screen options={{
+			tabBarIcon: () => {
+				return <Icon name="calendar" size={24} color={"#1877F2"}/>;
+			},
+			tabBarShowLabel: false,
+			tabBarStyle: { height: 56 },
+			headerShown: false
+			}} name="Appointments" component={AppointmentsScreen} />
+			<Tab.Screen options={{
+			tabBarIcon: () => {
+				return <Icon name="user" size={24} color={"#1877F2"}/>;
+			},
+			tabBarShowLabel: false,
+			tabBarStyle: { height: 56 },
+			headerShown: false
+			}} name="Profile" component={ProfileScreen} />
+		</Tab.Navigator>
       );
 }
