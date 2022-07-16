b4f6f28889d733dd0db75ff4d108dc091b4721d3
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 19:36:58 +0300

    frontend: feat: call the add info function in the UpdateInfoScreen on button press

--- frontend/src/screens/UpdateInfoScreen/index.js
+++ frontend/src/screens/UpdateInfoScreen/index.js
@@ -4,12 +4,19 @@ import styles from './styles'
 import OnBoardingTitle from '../../components/OnBoardingTitle'
 import OnBoardingPrompt from '../../components/OnBoardingPrompt'
 import { TextInput } from 'react-native-gesture-handler'
+import { useNavigation } from '@react-navigation/native'
+import { useUser } from '../../Context/User'
 import FullWidthButton from '../../components/FullWidthButton'
+import { addInfo } from './controller'
 
-export default function UpdateInfoScreen() {
+export default function UpdateInfoScreen( {route} ) {
+    const user = route.params.user
     const [zoomRate, setZoomRate] = useState()
     const [chatRate, setChatRate] = useState()
     const [about, setAbout] = useState("")
+    const navigation = useNavigation()
+    const {setUser} = useUser()
+
   return (
     <View style={styles.container}>
         <OnBoardingTitle/>
@@ -22,7 +29,7 @@ export default function UpdateInfoScreen() {
         <Text style={styles.inputTitle}>About me</Text>
         <TextInput style={styles.aboutInput} placeholder="About me" multiline={true} onChangeText={(enteredText)=>{setAbout(enteredText)}}/>
         <View style={styles.buttonContainer}>
-            <FullWidthButton text="Next"/>
+            <FullWidthButton text="Next" onHandlePress={()=>addInfo({"rate_zoom": zoomRate, "rate_chat": chatRate, "about_me": about}, user, setUser)}/>
         </View>
     </View>
   )
