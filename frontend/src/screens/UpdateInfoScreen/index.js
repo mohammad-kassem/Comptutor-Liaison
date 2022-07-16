445a2f3211d5eef31eaa9acca1a551f760ce87f6
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 19:34:51 +0300

    frontend: feat: implement the UpdateInfoScreen input fields local state management

--- frontend/src/screens/UpdateInfoScreen/index.js
+++ frontend/src/screens/UpdateInfoScreen/index.js
@@ -7,17 +7,20 @@ import { TextInput } from 'react-native-gesture-handler'
 import FullWidthButton from '../../components/FullWidthButton'
 
 export default function UpdateInfoScreen() {
+    const [zoomRate, setZoomRate] = useState()
+    const [chatRate, setChatRate] = useState()
+    const [about, setAbout] = useState("")
   return (
     <View style={styles.container}>
         <OnBoardingTitle/>
         <OnBoardingPrompt message="Complete your info"/>
         <Text style={styles.sectionPrompt}>Fill your profile</Text>
         <Text style={styles.inputTitle}>Zoom rate</Text>
-        <TextInput style={styles.input} placeholder="Zoom rate" keyboardType='decimal-pad'/>
+        <TextInput style={styles.input} placeholder="Zoom rate" keyboardType='decimal-pad' onChangeText={(enteredText)=>{setZoomRate(enteredText)}}/>
         <Text style={styles.inputTitle}>Chat rate</Text>
-        <TextInput style={styles.input} placeholder="Chat rate" keyboardType='decimal-pad'/>
+        <TextInput style={styles.input} placeholder="Chat rate" keyboardType='decimal-pad' onChangeText={(enteredText)=>{setChatRate(enteredText)}}/>
         <Text style={styles.inputTitle}>About me</Text>
-        <TextInput style={styles.aboutInput} placeholder="About me" multiline={true}/>
+        <TextInput style={styles.aboutInput} placeholder="About me" multiline={true} onChangeText={(enteredText)=>{setAbout(enteredText)}}/>
         <View style={styles.buttonContainer}>
             <FullWidthButton text="Next"/>
         </View>
