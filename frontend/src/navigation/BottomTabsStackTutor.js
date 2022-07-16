c411e2bfa1f3c1a9a4809423a47a71ef8c358c2e
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 14:43:33 +0300

    frontend: feat: include the AppointmentsScreen in the BottomTabsStackTutor

--- frontend/src/navigation/BottomTabsStackTutor.js
+++ frontend/src/navigation/BottomTabsStackTutor.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator }  from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import TutorSchedulesScreen from '../screens/TutorSchedulesScreen';
 import ProfileScreen from '../screens/ProfileScreen';
+import AppointmentsScreen from '../screens/AppointmentsScreen';
 
 
 
@@ -20,6 +21,14 @@ export default function BottomTabsStackTutor() {
           tabBarStyle: { height: 56 },
           headerShown: false
         }} name="Home" component={TutorSchedulesScreen} />
+        <Tab.Screen options={{
+          tabBarIcon: () => {
+            return <Icon name="calendar" size={24} color={"#1877F2"}/>;
+          },
+          tabBarShowLabel: false,
+          tabBarStyle: { height: 56 },
+          headerShown: false
+        }} name="Appointments" component={AppointmentsScreen} />
         <Tab.Screen options={{
           tabBarIcon: () => {
             return <Icon name="user" size={24} color={"#1877F2"}/>;
