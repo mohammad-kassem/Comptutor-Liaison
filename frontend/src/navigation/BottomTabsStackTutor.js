c1264c6dfc7388fa9d1b0db3b3c31999a33ae419
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:55:14 +0300

    frontend: refactor: clean code and fix indentation of BottomTabsStackTutor

--- frontend/src/navigation/BottomTabsStackTutor.js
+++ frontend/src/navigation/BottomTabsStackTutor.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator }  from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import TutorSchedulesScreen from '../screens/TutorSchedulesScreen';
@@ -12,31 +11,31 @@ import AppointmentsScreen from '../screens/AppointmentsScreen';
 export default function BottomTabsStackTutor() {
     const Tab = createBottomTabNavigator();
     return (
-      <Tab.Navigator>
-        <Tab.Screen options={{
-          tabBarIcon: () => {
-            return <Icon name="home" size={24} color={"#1877F2"}/>;
-          },
-          tabBarShowLabel: false,
-          tabBarStyle: { height: 56 },
-          headerShown: false
-        }} name="Home" component={TutorSchedulesScreen} />
-        <Tab.Screen options={{
-          tabBarIcon: () => {
-            return <Icon name="calendar" size={24} color={"#1877F2"}/>;
-          },
-          tabBarShowLabel: false,
-          tabBarStyle: { height: 56 },
-          headerShown: false
-        }} name="Appointments" component={AppointmentsScreen} />
-        <Tab.Screen options={{
-          tabBarIcon: () => {
-            return <Icon name="user" size={24} color={"#1877F2"}/>;
-          },
-          tabBarShowLabel: false,
-          tabBarStyle: { height: 56 },
-          headerShown: false
-        }} name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-      );
+		<Tab.Navigator>
+			<Tab.Screen options={{
+			tabBarIcon: () => {
+				return <Icon name="home" size={24} color={"#1877F2"}/>;
+			},
+			tabBarShowLabel: false,
+			tabBarStyle: { height: 56 },
+			headerShown: false
+			}} name="Home" component={TutorSchedulesScreen} />
+			<Tab.Screen options={{
+			tabBarIcon: () => {
+				return <Icon name="calendar" size={24} color={"#1877F2"}/>;
+			},
+			tabBarShowLabel: false,
+			tabBarStyle: { height: 56 },
+			headerShown: false
+			}} name="Appointments" component={AppointmentsScreen} />
+			<Tab.Screen options={{
+			tabBarIcon: () => {
+				return <Icon name="user" size={24} color={"#1877F2"}/>;
+			},
+			tabBarShowLabel: false,
+			tabBarStyle: { height: 56 },
+			headerShown: false
+			}} name="Profile" component={ProfileScreen} />
+		</Tab.Navigator>
+    );
 }
