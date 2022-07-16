4e2261afda75142863bd55075e99e883635375e9
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 02:02:01 +0300

    frontend: refactor: clean code and fix indentation of UpdateInfoScreen controller

--- frontend/src/screens/UpdateInfoScreen/controller.js
+++ frontend/src/screens/UpdateInfoScreen/controller.js
@@ -9,15 +9,14 @@ export async function addInfo(input, user, setUser){
         headers: {
             "Content-type": "application/json",
             "Authorization": `Bearer ${token}`
-        },
+            },
         data: JSON.stringify(input) 
-        })
-        .then(function(response){
-            setUser(user)
-        })
-        .catch(function(error){
-            console.log(error)
-            let message = Object.values(error.response.data);
-            alert(message[0]);
-        })
-    }
+    })
+    .then(function(response){
+        setUser(user)
+    })
+    .catch(function(error){
+        let message = Object.values(error.response.data);
+        alert(message[0]);
+    })
+}
