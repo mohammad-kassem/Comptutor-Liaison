eaeaf625d020946fcec9348c004e50ca6adc24b4
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:58:36 +0300

    frontend: refactor: clean code and fix indentation of AppointmentsScreen controller

--- frontend/src/screens/AppointmentsScreen/controller.js
+++ frontend/src/screens/AppointmentsScreen/controller.js
@@ -15,12 +15,10 @@ export async function deleteAppointment(id, appointments,  setAppointments) {
     })
     .then(async function(response){
         // ToastAndroid.show(response.data.messsage, ToastAndroid.SHORT)
-        console.log(id)
         setAppointments(appointments.filter((appointment)=>
         appointment.schedule_id !== id))
     })
     .catch(function(error){
-        // console.log("error")
         // let message = Object.values(error.response.data);
         // ToastAndroid(message[0], ToastAndroid.SHORT) 
     })
@@ -28,22 +26,6 @@ export async function deleteAppointment(id, appointments,  setAppointments) {
 
 export async function getAppointments(setAppointments, stackType) {
     const token = await getToken();
-    console.log("tutor")
-//     // console.log(token)
-//     //     try {
-//     //       const res = await fetch("http://192.168.1.105:8000/api/v1/appointment/student",{
-//     //         headers: {"Authorization" : `Bearer ${token}`,
-//     //         "Content-type": "application/json",
-//     //     "Accept": "application/json"}
-//     //       });
-//     //       const data = await res.json();
-//     //       console.log("hello")
-//     //       console.log(typeof data)
-//     //       setAppointments(data.appointments)
-//     //     } catch (err) {
-//     //       console.log(err);
-//     //     }
-//     //   };
     axios({
         method: "get",
         url: `http://192.168.1.105:8000/api/v1/appointment/${stackType}`,
@@ -52,16 +34,10 @@ export async function getAppointments(setAppointments, stackType) {
         "Accept": "application/json"}
     })
     .then(function(response){
-        // console.log(response)
-        console.log("jshjsh")
         let json = response.data;
-        console.log(response.data)
-        // json = JSON.parse(response.data)
-        console.log(typeof response.data)
         setAppointments(json.appointments)
     })
     .catch(function(error){
-        console.log(error)
         let message = Object.values(error.response.data);
         ToastAndroid(message[0], ToastAndroid.SHORT) 
     })
