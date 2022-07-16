ea3350b73a73d2a3e0ef5af27d9948a5dbeaa656
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:59:33 +0300

    frontend: refactor: clean code and fix indentation of LoginScreen

--- frontend/src/screens/LoginScreen/index.js
+++ frontend/src/screens/LoginScreen/index.js
@@ -11,39 +11,36 @@ import CridentialsFrom from '../../components/CridentialsForm';
 
 
 export default function LoginScreen() {
-  const navigation = useNavigation()
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const {user, setUser} = useUser()
-  
-  function login(cridentials){
-    axios({
-      method: "post",
-      url: "http://192.168.1.105:8000/api/v1/auth/login",
-      headers: {
-        "Content-type": "application/json",
-      },
-      data: JSON.stringify(cridentials),
-    })
-    .then(function(response){
-      const storeData = async (response) => {
-        try {
-          await AsyncStorage.setItem('storage_Key', response.data.access_token);
-        } catch (e) {
-          alert("Error setting token")
-        }
-      }
+	const {user, setUser} = useUser()
+	
+	function login(cridentials){
+		axios({
+			method: "post",
+			url: "http://192.168.1.105:8000/api/v1/auth/login",
+			headers: {
+				"Content-type": "application/json",
+			},
+			data: JSON.stringify(cridentials),
+			})
+		.then(function(response){
+		const storeData = async (response) => {
+			try {
+				await AsyncStorage.setItem('storage_Key', response.data.access_token);
+			} catch (e) {
+			alert("Error setting token")
+			}
+		}
 
-      storeData(response);
-      setUser(response.data.user);
-    })
-    .catch(function(error){
-      let message = Object.values(error.response.data);
-      alert(message[0]);
-    })
-  };
-  
-  return (
-    <CridentialsFrom type="login" onPressHandler={login}/>
-  )
+		storeData(response);
+		setUser(response.data.user);
+		})
+		.catch(function(error){
+			let message = Object.values(error.response.data);
+			alert(message[0]);
+		})
+	};
+	
+	return (
+		<CridentialsFrom type="login" onPressHandler={login}/>
+	)
 }
