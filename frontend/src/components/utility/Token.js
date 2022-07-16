4e9186fe74bfc07b8b119df53877dc7635dbb510
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:53:42 +0300

    frontend: refactor: clean code and fix indentation of Token

--- frontend/src/components/utility/Token.js
+++ frontend/src/components/utility/Token.js
@@ -4,25 +4,21 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export async function getToken() {
-        let value;
-        try {
-          value = await AsyncStorage.getItem('storage_Key');
-          return value
-          if(value !== null) {
-          }
-        } catch(e) {
-            alert("Error getting token")
-            console.log(e);
-            return undefined
-        }
+export async function getToken(){
+	let value;
+	try {
+		value = await AsyncStorage.getItem('storage_Key');
+		return value
+	} catch(e) {
+		alert("Error getting token")
+		return undefined
+	}
+	}
 
-}
-
-export async function setToken(value) {
-  try {
-    await AsyncStorage.setItem('storage_Key', value)
-  } catch (e) {
-      alert("Error setting token")
-  }
-}
+	export async function setToken(value){
+	try {
+		await AsyncStorage.setItem('storage_Key', value)
+	} catch (e) {
+		alert("Error setting token")
+	}
+	}
