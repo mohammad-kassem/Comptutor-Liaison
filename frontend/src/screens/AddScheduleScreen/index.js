c9b0109532596f6de455e02dacd1e630e7364ac4
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 09:45:41 +0300

    frontend: feat: display and hide the date and time pickers in the AddScheduleScreen

--- frontend/src/screens/AddScheduleScreen/index.js
+++ frontend/src/screens/AddScheduleScreen/index.js
@@ -5,9 +5,10 @@ import { toDateString, toTimeString } from './controller'
 import DateTimePicker from 'react-native-modal-datetime-picker';
 
 export default function AddScheduleScreen() {
-    let [date, setDate] = useState(new Date())
-    let [time, setTime] = useState(new Date())
-   
+    const [date, setDate] = useState(new Date())
+    const [time, setTime] = useState(new Date())
+    const [datePickerIsVisable, setDatePickerIsVisable] = useState(false)
+    const [timePickerIsVisable, setTimePickerIsVisable] = useState(false)
 
     return (
         <View style={styles.container}>
@@ -17,11 +18,11 @@ export default function AddScheduleScreen() {
             <Text style={styles.title}>Duration</Text>
             <Text style={styles.field}>1 hour</Text>
             <Text style={styles.title}>Date</Text>
-            <TouchableOpacity style={styles.field}>
+            <TouchableOpacity style={styles.field} onPress={()=>setDatePickerIsVisable(true)}>
                 <Text style={styles.fieldText}>{toDateString(date)}</Text>
             </TouchableOpacity>
             <Text style={styles.title}>Start time</Text>
-            <TouchableOpacity style={styles.field}>
+            <TouchableOpacity style={styles.field} onPress={()=>setTimePickerIsVisable(true)}>
                 <Text style={styles.fieldText}>{toTimeString(time)}</Text>
             </TouchableOpacity>
         </View>
