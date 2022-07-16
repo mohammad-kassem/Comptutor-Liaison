0bf5fa2b47b334e7a70fd0a3651539880c06fa9e
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:52:58 +0300

    frontend: refactor: clean code and fix indentation of OnBoardingTitle

--- frontend/src/components/OnBoardingTitle/index.js
+++ frontend/src/components/OnBoardingTitle/index.js
@@ -3,9 +3,9 @@ import React from 'react'
 import styles from './styles'
 
 export default function OnBoardingTitle() {
-  return (
-    <>
-      <Text style={styles.title}>Welcome to Comptutor</Text>
-    </>
-  )
+  	return (
+		<>
+		<Text style={styles.title}>Welcome to Comptutor</Text>
+		</>
+  	)
 }
