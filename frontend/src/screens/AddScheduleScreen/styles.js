2638b986687dd70aab3a2d8f9908ab0e38e78566
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-16 07:51:07 +0300

    frontend: feat: implement the AdfdScheduleScreen titles and fields styling

---
+++ frontend/src/screens/AddScheduleScreen/styles.js
@@ -0,0 +1,32 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        backgroundColor: "#f5f5f5",
+        paddingVertical: 25,
+        paddingHorizontal: "10%"
+    },
+
+    sectionPrompt: {
+        fontSize: 20,
+        fontWeight: "500",
+        marginBottom: 15
+    },
+
+    title: {
+        fontSize: 18,
+        marginBottom: 5
+    },
+
+    field: {
+        borderBottomWidth: 1,
+        marginBottom: 20,
+        padding: 0,
+        textAlignVertical: 'bottom',
+        alignItems:"flex-start",
+        fontSize: 16,
+    },
+})
