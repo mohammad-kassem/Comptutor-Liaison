9104891ed0937489b5ee92a8918c1899c1d90f3a
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 01:50:05 +0300

    frontend: refactor: clean code and fix indentation of AppointmentModal

--- frontend/src/components/AppointmentModal/index.js
+++ frontend/src/components/AppointmentModal/index.js
@@ -6,30 +6,29 @@ import styles from './styles'
 import { addAppointment } from './controller';
 
 export default function AppointmentModal({ modalIsVisable, setModalIsVisable, time, date, tutor, schedules, setSchedules }) {
-  console.log(date, time)
 
-  return (
-      <View style={styles.container}>
-         <Modal
-            isVisible={modalIsVisable}
-            animationType='slide'
-            transparent={true}
-            backdropOpacity={0.3}
-         >
-          <View style={styles.modal}>
-            <Text style={styles.title}>Confirm appointment</Text>
-            <Text style={styles.content}>Confirm appointment with {tutor.fname} {tutor.lname}</Text>
-            <Text style={styles.content}>{date} {time && (time.start_time + "-" + time.end_time)}</Text>
-            <View style={styles.buttonsContainer}>
-              <TouchableOpacity style={styles.leftButton} onPress={()=>{setModalIsVisable(false); addAppointment(time.id, schedules, setSchedules, tutor.id)}}>
-                <Text style={styles.confirmButton}>Confirm</Text>
-              </TouchableOpacity>
-              <TouchableOpacity style={styles.rightButton} onPress={()=>setModalIsVisable(false)}>
-                <Text style={styles.cancelButton}>Cancel</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
-          </Modal>
-      </View>
-    )
-  }
+	return (
+		<View style={styles.container}>
+			<Modal
+			isVisible={modalIsVisable}
+			animationType='slide'
+			transparent={true}
+			backdropOpacity={0.3}
+			>
+			<View style={styles.modal}>
+			<Text style={styles.title}>Confirm appointment</Text>
+			<Text style={styles.content}>Confirm appointment with {tutor.fname} {tutor.lname}</Text>
+			<Text style={styles.content}>{date} {time && (time.start_time + "-" + time.end_time)}</Text>
+			<View style={styles.buttonsContainer}>
+				<TouchableOpacity style={styles.leftButton} onPress={()=>{setModalIsVisable(false); addAppointment(time.id, schedules, setSchedules, tutor.id)}}>
+				<Text style={styles.confirmButton}>Confirm</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.rightButton} onPress={()=>setModalIsVisable(false)}>
+				<Text style={styles.cancelButton}>Cancel</Text>
+				</TouchableOpacity>
+			</View>
+			</View>
+			</Modal>
+		</View>
+	)
+	}
