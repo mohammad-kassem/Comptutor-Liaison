fd657748289df34693ba55a9abe9d01f043448ec
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 02:00:01 +0300

    frontend: refactor: clean code and fix indentation of ProfileScreen

--- frontend/src/screens/ProfileScreen/index.js
+++ frontend/src/screens/ProfileScreen/index.js
@@ -5,27 +5,27 @@ import { useUser } from '../../Context/User'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 
 export default function ProfileScreen() {
-   const { user, setUser } = useUser()
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-            <Image style={styles.profile} source={require('../../../assets/logo.png')}/>
+    const { user, setUser } = useUser()
+    return (
+        <View style={styles.container}>
+        <View style={styles.imageContainer}>
+                <Image style={styles.profile} source={require('../../../assets/logo.png')}/>
+            </View>
+            <Text style={styles.name}>{user.fname} {user.lname}</Text>
+            <View style={styles.balanceContainer}>
+                <Text style={styles.balance}>Balance</Text>
+                <Text style={styles.balance}>$50</Text>
+            </View>
+            <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.editButton}>
+                    <Text style={styles.butonText}>Edit</Text>
+                    <Icon name="account-edit" size={24} color="white"/>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.logoutButton} onPress={()=>setUser({})}>
+                    <Text style={styles.butonText}>Logout</Text>
+                    <Icon name="logout" size={24} color="white"/>
+                </TouchableOpacity>
+            </View>
         </View>
-        <Text style={styles.name}>{user.fname} {user.lname}</Text>
-        <View style={styles.balanceContainer}>
-            <Text style={styles.balance}>Balance</Text>
-            <Text style={styles.balance}>$50</Text>
-        </View>
-        <View style={styles.buttonContainer}>
-            <TouchableOpacity style={styles.editButton}>
-                <Text style={styles.butonText}>Edit</Text>
-                <Icon name="account-edit" size={24} color="white"/>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.logoutButton} onPress={()=>setUser({})}>
-                <Text style={styles.butonText}>Logout</Text>
-                <Icon name="logout" size={24} color="white"/>
-            </TouchableOpacity>
-        </View>
-    </View>
-  )
+    )
 }
