c178ef9f26b73af96f0d5113a48adb7707e86146
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 15:49:42 +0300

    frontend: feat: implement the Container component

---
+++ frontend/src/components/Container/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: "flex-start",
+        alignItems: "stretch",
+        backgroundColor: "#f5f5f5",
+        paddingVertical: 25,
+        paddingHorizontal: 25
+    },
+})
