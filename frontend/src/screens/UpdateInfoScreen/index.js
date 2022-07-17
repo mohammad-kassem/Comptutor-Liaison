04e467cc9a3666fcffa6f8454955d083e2a73355
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-17 15:52:27 +0300

    frontend: reafctor: fix indentation of UpdateInfoScreen

--- frontend/src/screens/UpdateInfoScreen/index.js
+++ frontend/src/screens/UpdateInfoScreen/index.js
@@ -1,4 +1,4 @@
-import { View, Text } from 'react-native'
+import { View, Text, KeyboardAvoidingView, ScrollView } from 'react-native'
 import React, { useState } from 'react'
 import styles from './styles'
 import OnBoardingTitle from '../../components/OnBoardingTitle'
@@ -7,6 +7,7 @@ import { TextInput } from 'react-native-gesture-handler'
 import { useUser } from '../../Context/User'
 import FullWidthButton from '../../components/FullWidthButton'
 import { addInfo } from './controller'
+import Container from '../../components/Container'
 
 export default function UpdateInfoScreen( {route} ) {
     const user = route.params.user
@@ -15,20 +16,24 @@ export default function UpdateInfoScreen( {route} ) {
     const [about, setAbout] = useState("")
     const {setUser} = useUser()
 
-  return (
-    <View style={styles.container}>
-        <OnBoardingTitle/>
-        <OnBoardingPrompt message="Complete your info"/>
-        <Text style={styles.sectionPrompt}>Fill your profile</Text>
-        <Text style={styles.inputTitle}>Zoom rate</Text>
-        <TextInput style={styles.input} placeholder="Zoom rate" keyboardType='decimal-pad' onChangeText={(enteredText)=>{setZoomRate(enteredText)}}/>
-        <Text style={styles.inputTitle}>Chat rate</Text>
-        <TextInput style={styles.input} placeholder="Chat rate" keyboardType='decimal-pad' onChangeText={(enteredText)=>{setChatRate(enteredText)}}/>
-        <Text style={styles.inputTitle}>About me</Text>
-        <TextInput style={styles.aboutInput} placeholder="About me" multiline={true} onChangeText={(enteredText)=>{setAbout(enteredText)}}/>
-        <View style={styles.buttonContainer}>
-            <FullWidthButton text="Next" onHandlePress={()=>addInfo({"rate_zoom": zoomRate, "rate_chat": chatRate, "about_me": about}, user, setUser)}/>
-        </View>
-    </View>
-  )
+	return (
+		<KeyboardAvoidingView keyboardVerticalOffset={-250} behavior="padding">
+			<ScrollView>
+				<Container>
+					<OnBoardingTitle/>
+					<OnBoardingPrompt message="Complete your info"/>
+					<Text style={styles.sectionPrompt}>Fill your profile</Text>
+					<Text style={styles.inputTitle}>Zoom rate</Text>
+					<TextInput style={styles.input} placeholder="Zoom rate" keyboardType='decimal-pad' onChangeText={(enteredText)=>{setZoomRate(enteredText)}}/>
+					<Text style={styles.inputTitle}>Chat rate</Text>
+					<TextInput style={styles.input} placeholder="Chat rate" keyboardType='decimal-pad' onChangeText={(enteredText)=>{setChatRate(enteredText)}}/>
+					<Text style={styles.inputTitle}>About me</Text>
+					<TextInput style={styles.aboutInput} placeholder="About me" multiline={true} onChangeText={(enteredText)=>{setAbout(enteredText)}}/>
+					<View style={styles.buttonContainer}>
+						<FullWidthButton text="Next" onHandlePress={()=>addInfo({"rate_zoom": zoomRate, "rate_chat": chatRate, "about_me": about}, user, setUser)}/>
+					</View>
+				</Container>
+			</ScrollView>
+		</KeyboardAvoidingView>
+	)
 }
