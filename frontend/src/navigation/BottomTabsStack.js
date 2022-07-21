57ab1ee93f45b07047bdacf357865f9107ef3105
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-21 15:54:00 +0300

    frontend: feat: include the ChatRoomsScreen in the BottomTabsStack

--- frontend/src/navigation/BottomTabsStack.js
+++ frontend/src/navigation/BottomTabsStack.js
@@ -4,6 +4,7 @@ import HomeScreen from "../screens/HomeScreen";
 import Icon from 'react-native-vector-icons/FontAwesome';
 import AppointmentsScreen from '../screens/AppointmentsScreen';
 import ProfileScreen from '../screens/ProfileScreen';
+import ChatRoomScreen from '../screens/ChatRoomScreen';
 
 
 
@@ -28,6 +29,14 @@ export default function BottomTabsStack() {
 			tabBarStyle: { height: 56 },
 			headerShown: false
 			}} name="Appointments" component={AppointmentsScreen} />
+			<Tab.Screen options={{
+			tabBarIcon: () => {
+				return <Icon name="comments" size={24} color={"#1877F2"}/>;
+			},
+			tabBarShowLabel: false,
+			tabBarStyle: { height: 56 },
+			headerShown: false
+			}} name="Chat" component={ChatRoomScreen} />
 			<Tab.Screen options={{
 			tabBarIcon: () => {
 				return <Icon name="user" size={24} color={"#1877F2"}/>;
