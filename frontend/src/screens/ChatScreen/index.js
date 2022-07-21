623017f9e3860c058960b034e5fa68fcce7475d3
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-21 16:29:47 +0300

    frontend: feat: set up getting the messages from the firebase database

--- frontend/src/screens/ChatScreen/index.js
+++ frontend/src/screens/ChatScreen/index.js
@@ -11,6 +11,21 @@ export default function ChatScreen() {
 	const WIDTH = 200 
 	const HEIGHT = 2000
 
+	useEffect(() => {
+		console.log("error")
+		var arr = []
+		database()
+		.ref('room1')
+		.on('value', snapshot => {
+			if (snapshot.val()){
+				arr = []
+				for (const item in (snapshot.val().messages)) {
+				arr = [...arr, snapshot.val().messages[item]]
+			}
+		}
+		setMessages(arr)
+		});
+	}, [])
 
 	return (
 		<>
