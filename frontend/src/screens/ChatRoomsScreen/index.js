f45ce6c1a59eb03e0744e875951e0c861c294a38
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-21 16:03:57 +0300

    frontend: refactor: update the name of the ChatRoomsScreen

---
+++ frontend/src/screens/ChatRoomsScreen/index.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ChatRoomsScreen() {
+
+  return (
+    <View>
+      <Text>ChatRoomScreen</Text>
+    </View>
+  )
+}
