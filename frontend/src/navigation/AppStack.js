8227439de0d68b924bcf7495135fd47727bac56a
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-21 16:22:55 +0300

    frontend: feat: include the ChatStack in the AppStack

--- frontend/src/navigation/AppStack.js
+++ frontend/src/navigation/AppStack.js
@@ -2,6 +2,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from 'react'
 import BottomTabsStack from "./BottomTabsStack";
+import ChatStack from "./ChatStack";
 import HomeStack from "./HomeStack";
 
 export default function AppStack() {
@@ -20,6 +21,10 @@ export default function AppStack() {
             <RootStackNav.Screen
             name="HomeStack"
             component={HomeStack}
+            />
+            <RootStackNav.Screen
+            name="ChatStack"
+            component={ChatStack}
             />
         </RootStackNav.Navigator>
         </NavigationContainer>
