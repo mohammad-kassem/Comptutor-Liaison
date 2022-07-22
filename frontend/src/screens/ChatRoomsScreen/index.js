b9ff9f8fbf38b19f3f929a51bd1cdcf9c9566982
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-22 03:12:38 +0300

    frontend: feat: display the rooms info in the ChatRoomsScreen

--- frontend/src/screens/ChatRoomsScreen/index.js
+++ frontend/src/screens/ChatRoomsScreen/index.js
@@ -8,6 +8,7 @@ import { useUser } from '../../Context/User'
 export default function ChatRoomsScreen() {
 	const {user} = useUser()
 	const [rooms, setRooms] = useState([])
+	
 
 	useEffect(function(){
 		let arr = []
@@ -19,9 +20,43 @@ export default function ChatRoomsScreen() {
 			setRooms(arr[0])
 		})
       }, []);
+
 	return (
 		<>
 		<Text style={styles.title}>Chat Rooms</Text>
+		<View style={styles.roomContainer}>
+			<FlatList data={rooms} renderItem={(roomData) =>{
+                return(
+					<TouchableOpacity style={styles.room}>
+						{user.role_id === 1 ? 
+						<>
+						<View style={styles.imageContainer}>
+							{roomData.item.tutorImage === '../../../assets/logo.png' ?
+							<Image style={styles.profile} source={require('../../../assets/logo.png')}/>
+							:<Image style={styles.profile} source={{uri: roomData.item.tutorImage}}/>}
+						</View>
+						<View style={styles.content}>
+							<Text style={styles.contact}>{roomData.item.tutorName}</Text>
+							<Text style={styles.message}>Last message goes here</Text>
+						</View>
+						</>
+						:
+						<>
+						<View style={styles.imageContainer}>
+							{roomData.item.studentImage === '../../../assets/logo.png' ?
+							<Image style={styles.profile} source={require('../../../assets/logo.png')}/>
+							:<Image style={styles.profile} source={{uri: roomData.item.studentImage}}/>}
+						</View>
+						<View style={styles.content}>
+							<Text style={styles.contact}>{roomData.item.studentName}</Text>
+							<Text style={styles.message}>Last message goes here</Text>
+						</View>
+						</>
+					}
+				 	</TouchableOpacity>
+				)
+			}}/>
+			</View>
 		</>
 	)
 }
