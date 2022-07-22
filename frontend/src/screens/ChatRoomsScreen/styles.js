41f7a1e78ba6cd0e6fb53a408337ad2af692fe83
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-22 03:13:48 +0300

    frontend: feat: implement the ChatRoomsScreen rooms styling

--- frontend/src/screens/ChatRoomsScreen/styles.js
+++ frontend/src/screens/ChatRoomsScreen/styles.js
@@ -10,5 +10,41 @@ export default StyleSheet.create({
         marginHorizontal: 25
     },
 
+    roomContainer: {
+        backgroundColor: "white",
+    },
 
+
+    room: {
+        flexDirection: "row",
+        paddingVertical: 10,
+        paddingHorizontal: 25,
+        alignItems: "center",
+        borderBottomWidth: 1,
+        borderColor: "#656565",
+    },
+
+    imageContainer: {
+        width: 62,
+        height: 62, 
+        marginRight: 15    
+    },
+
+    profile: {
+        flex: 1,
+        width: undefined,
+        height: undefined,
+        borderRadius: 50
+    },
+
+
+    contact: {
+        fontSize: 20,
+        fontWeight: "600",
+        color: "#1877F2",
+    },
+
+    message: {
+        color: "#656565",
+    }
 })
