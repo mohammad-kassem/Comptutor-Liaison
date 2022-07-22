1d6b279b57a141aa813a058d400939357152ea13
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-22 19:11:37 +0300

    frontend: feat: implement navigation from ChatRoomsScreen to ChatScreen

--- frontend/src/screens/ChatRoomsScreen/index.js
+++ frontend/src/screens/ChatRoomsScreen/index.js
@@ -1,12 +1,14 @@
 import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import styles from './styles'
+import { useNavigation } from '@react-navigation/native'
 import database from '@react-native-firebase/database'
 import { useUser } from '../../Context/User'
 import { filterRooms } from './controller'
 
 
 export default function ChatRoomsScreen() {
+	const navigation = useNavigation()
 	const {user} = useUser()
 	let [rooms, setRooms] = useState([])
 	
@@ -31,29 +33,29 @@ export default function ChatRoomsScreen() {
 		<View style={styles.roomContainer}>
 			<FlatList data={rooms} renderItem={(roomData) =>{
                 return(
-					<TouchableOpacity style={styles.room}>
+					<TouchableOpacity style={styles.room} onPress={() => navigation.navigate("ChatStack", { screen: "ChatScreen",  params: { reciever: {"id": user.role_id === 1 ? roomData.item[0].split("-")[1] : roomData.item[0].split("-")[0]}},})}>
 						{user.role_id === 1 ? 
 						<>
 						<View style={styles.imageContainer}>
-							{roomData.item.tutorImage === '../../../assets/logo.png' ?
+							{roomData.item[1].tutorImage === '../../../assets/logo.png' ?
 							<Image style={styles.profile} source={require('../../../assets/logo.png')}/>
-							:<Image style={styles.profile} source={{uri: roomData.item.tutorImage}}/>}
+							:<Image style={styles.profile} source={{uri: roomData.item[1].tutorImage}}/>}
 						</View>
 						<View style={styles.content}>
-							<Text style={styles.contact}>{roomData.item.tutorName}</Text>
-							<Text style={styles.message}>Last message goes here</Text>
+							<Text style={styles.contact}>{roomData.item[1].tutorName}</Text>
+							<Text style={styles.message}>{roomData.item[1].lastMessage}</Text>
 						</View>
 						</>
 						:
 						<>
 						<View style={styles.imageContainer}>
-							{roomData.item.studentImage === '../../../assets/logo.png' ?
+							{roomData.item[1].studentImage === '../../../assets/logo.png' ?
 							<Image style={styles.profile} source={require('../../../assets/logo.png')}/>
-							:<Image style={styles.profile} source={{uri: roomData.item.studentImage}}/>}
+							:<Image style={styles.profile} source={{uri: roomData.item[1].studentImage}}/>}
 						</View>
 						<View style={styles.content}>
-							<Text style={styles.contact}>{roomData.item.studentName}</Text>
-							<Text style={styles.message}>Last message goes here</Text>
+							<Text style={styles.contact}>{roomData.item[1].studentName}</Text>
+							<Text style={styles.message}>{roomData.item[1].lastMessage}</Text>
 						</View>
 						</>
 					}
