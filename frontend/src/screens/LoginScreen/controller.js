9e3a81c81e3f0a905c07ffffd24afd165e24c6cd
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-22 19:35:25 +0300

    frontend: feat: link the set FCM api with successful login of user

--- frontend/src/screens/LoginScreen/controller.js
+++ frontend/src/screens/LoginScreen/controller.js
@@ -22,3 +22,23 @@ export function login(cridentials, setUser){
         ToastAndroid.show(message[0][0], ToastAndroid.SHORT)
     })
 };
+export function setFCM(user, FCM_token){
+    axios({
+        method: "put",
+        url: "http://192.168.1.105:8000/api/v1/FCM/set",
+        headers: {
+            "Content-type": "application/json",
+            },
+        data: JSON.stringify({...user, "FCM_token": FCM_token}),
+    })
+    .then(function(response){
+        console.log("success")
+    })
+    .catch(function(error){
+        console.log("error", error)
+        let message = Object.values(error.response.data);
+        console.log(message);
+        ToastAndroid.show(message[0][0], ToastAndroid.SHORT)
+    })
+
+}
