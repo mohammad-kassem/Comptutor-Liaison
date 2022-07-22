34bae12620d604be918499d89c404af22e57fd59
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-22 03:08:50 +0300

    frontend: feat: implement the ChatRoomsScreen title styling

---
+++ frontend/src/screens/ChatRoomsScreen/styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    title: {
+        fontSize: 24,
+        fontWeight: "700",
+        lineHeight: 29,
+        marginBottom: 25,
+        marginTop: 50,
+        marginHorizontal: 25
+    },
+
+
+})
