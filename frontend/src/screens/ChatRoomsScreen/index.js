ef19e729eec1442e837baa7a7a995270303712ab
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-22 03:11:19 +0300

    frontend: feat: get the  rooms info from the firebase database

--- frontend/src/screens/ChatRoomsScreen/index.js
+++ frontend/src/screens/ChatRoomsScreen/index.js
@@ -1,9 +1,24 @@
 import { View, Text, Image, FlatList, TouchableOpacity } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import styles from './styles'
+import database from '@react-native-firebase/database'
+import { useUser } from '../../Context/User'
+
 
 export default function ChatRoomsScreen() {
+	const {user} = useUser()
+	const [rooms, setRooms] = useState([])
 
+	useEffect(function(){
+		let arr = []
+        database()
+		.ref("rooms")
+		.on('value', snapshot => {
+			arr = [Object.values(snapshot.val())]
+			console.log(arr[0][0])
+			setRooms(arr[0])
+		})
+      }, []);
 	return (
 		<>
 		<Text style={styles.title}>Chat Rooms</Text>
