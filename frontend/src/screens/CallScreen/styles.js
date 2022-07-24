c776328faefd91a23e86a53fa47ee42db19c91eb
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-24 18:16:06 +0300

    frontend: feat: impleemnt the CallScreen

---
+++ frontend/src/screens/CallScreen/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export default styles = StyleSheet.create({
+    callContainerBase: {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+    },
+    callContainerLandscape: {
+      flexDirection: 'row',
+    },
+  });
