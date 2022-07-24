6884869fba8130d4849c6587635d503b3ed9413b
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-24 18:19:46 +0300

    frontend: feat: implement the TrayButton

---
+++ frontend/src/components/TrayButton/styles.js
@@ -0,0 +1,33 @@
+import { StyleSheet } from "react-native";
+
+
+export default styles = StyleSheet.create({
+    iconBase: {
+      height: 32,
+      width: 32,
+    },
+    iconPortrait: {
+      marginHorizontal: 16,
+    },
+    iconLandscape: {
+      marginTop: 16,
+    },
+    iconLeave: {
+      height: 28,
+      width: 36,
+    },
+    disabled: {
+      opacity: 0.6,
+    },
+    controlContainer: {
+      alignItems: 'center',
+    },
+    controlText: {
+      fontWeight: '500',
+      paddingTop: 4,
+      color: "#1877F2",
+    },
+    offText: {
+      color: "#e71115",
+    },
+  });
