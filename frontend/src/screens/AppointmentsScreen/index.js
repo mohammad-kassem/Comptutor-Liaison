a92d3a6a966f863f725c6be56599893f7ba4c6c4
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-24 18:21:03 +0300

    frontend: feat: navigate from the AppointmentScreen to the CallScreen

--- frontend/src/screens/AppointmentsScreen/index.js
+++ frontend/src/screens/AppointmentsScreen/index.js
@@ -4,7 +4,7 @@ import styles from './styles'
 import RBSheet from "react-native-raw-bottom-sheet";
 import { deleteAppointment, getAppointments } from './controller';
 import { useUser } from '../../Context/User';
-import { useFocusEffect } from '@react-navigation/native';
+import { useFocusEffect, useNavigation } from '@react-navigation/native';
 
 
 export default function AppointmentsScreen() {
@@ -15,7 +15,7 @@ export default function AppointmentsScreen() {
     const {user, setUser} = useUser()   
     const appointmentWith = user.role_id === 1 ? "tutor" : "student"
     const stackType = user.role_id === 1 ? "student" : "tutor"
-
+    navigation = useNavigation()
 
     useFocusEffect(
         React.useCallback(() => {
@@ -38,8 +38,8 @@ export default function AppointmentsScreen() {
           }}>
             
         <Text style={styles.sheetText}>Appointment action</Text>
-        <TouchableOpacity style={styles.cancel} onPress={()=>{deleteAppointment(id,appointments, setAppointments); refRBSheet.current.close();}}><Text style={styles.buttonText}>Cancel appointment</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.go}><Text style={styles.buttonText}>Go to appointment</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.cancel} onPress={()=>{deleteAppointment(id, appointments, setAppointments); refRBSheet.current.close();}}><Text style={styles.buttonText}>Cancel appointment</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.go} onPress={()=>navigation.navigate("AppointmentStack", { screen: "CallScreen", params: { appointmentId: id },})}><Text style={styles.buttonText}>Go to appointment</Text></TouchableOpacity>
         </RBSheet>
             <View style={styles.container}>
                 <Text style={styles.title}>Appointments</Text>
