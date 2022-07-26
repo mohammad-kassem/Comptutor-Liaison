345d018c1f6f32b247c9c7e21c764713b0ff34cf
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-26 14:08:40 +0300

    fronetnd: feat: link the delete subject api with the EditProfileScreen

--- frontend/src/screens/EditProfileScreen/controller.js
+++ frontend/src/screens/EditProfileScreen/controller.js
@@ -27,3 +27,27 @@ export async function deleteDegree(id, degrees, setDegrees, user, setUser){
         ToastAndroid.show(message[0][0], ToastAndroid.SHORT)
     })
 }
+
+export async function deleteSubject(id, subjects, setSubjects, user, setUser){
+    const token = await getToken()
+    axios({
+        method: "delete",
+        url: `http://192.168.1.105:8000/api/v1/subject/delete/${id}`,
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+        })
+    .then(function(response){
+        const newSubjects = subjects.filter((subject)=>
+        subject.id !== id)
+        setSubjects(newSubjects)
+        user.subjects = newSubjects
+        setUser(user)
+    })
+    .catch(function(error){
+        console.log(error)
+        let message = Object.values(error.response.data);
+        ToastAndroid.show(message[0][0], ToastAndroid.SHORT)
+    })
+}
