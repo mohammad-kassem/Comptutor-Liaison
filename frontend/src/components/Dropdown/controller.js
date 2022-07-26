64864ed7e40c68f30325421667f058e9737beacc
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-26 14:51:13 +0300

    frontend: feat: implement generating the dropdown data

---
+++ frontend/src/components/Dropdown/controller.js
@@ -0,0 +1,16 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export function generateData(groupedSchedules) {
+    let data;
+    try{
+        data = Object.keys(groupedSchedules).map(date => (
+        {label: date, value: date}
+    ));}
+    catch{
+            data = Object.keys(groupedSchedules).map(appointment => (
+            {label: appointment.date, value: appointment.date}
+        )); 
+    }
+    return data    
+}
