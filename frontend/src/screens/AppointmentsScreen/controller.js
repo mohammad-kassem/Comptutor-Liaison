6ce704fe278e6ca1d0b2a99ec4972460b5b34814
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-27 00:35:48 +0300

    frontend: feat: implement only allowing access to the go to appointment button during appointments in teh AppointmentsScreen

--- frontend/src/screens/AppointmentsScreen/controller.js
+++ frontend/src/screens/AppointmentsScreen/controller.js
@@ -67,3 +67,12 @@ export function filterAppointments(appointments) {
 	})
 	)
 }
+
+export function isAppointmentTime(appointment){
+    const appointmentStartTime = new Date(appointment.schedule.date + "T" + appointment.schedule.start_time).getTime()
+    const appointmentEndTime = new Date(appointment.schedule.date + "T" + appointment.schedule.end_time).getTime()
+	console.log(appointmentStartTime, new Date().toLocaleString(), appointmentEndTime, appointment.schedule.date, appointment.schedule.start_time, appointment.schedule.end_time, appointmentStartTime < new Date().getTime() && appointmentEndTime > new Date().getTime())
+    let d = new Date()
+    d = d.getTime() - d.getTimezoneOffset() * 60 * 1000
+    return (appointmentStartTime > d || appointmentEndTime < d)
+}
--- frontend/src/screens/AppointmentsScreen/index.js
+++ frontend/src/screens/AppointmentsScreen/index.js
@@ -2,7 +2,7 @@ import { View, Text, TouchableOpacity, FlatList } from 'react-native'
 import React, { useEffect, useRef, useState } from 'react'
 import styles from './styles'
 import RBSheet from "react-native-raw-bottom-sheet";
-import { deleteAppointment, filterAppointments, getAppointments, groupAppointments } from './controller';
+import { deleteAppointment, filterAppointments, getAppointments, groupAppointments, isAppointmentTime } from './controller';
 import { useUser } from '../../Context/User';
 import { useFocusEffect, useNavigation } from '@react-navigation/native';
 import DropdownComponent from '../../components/Dropdown';
@@ -13,6 +13,7 @@ export default function AppointmentsScreen() {
     const refRBSheet  = useRef();
     const [date, setDate] = useState("")
     const [id, setId] = useState() 
+    const [buttonDisabled, setButtonDisabled] = useState(false)
     const {user, setUser} = useUser()   
     const appointmentWith = user.role_id === 1 ? "tutor" : "student"
     const stackType = user.role_id === 1 ? "student" : "tutor"
@@ -38,10 +39,9 @@ export default function AppointmentsScreen() {
               backgroundColor: "#000"
             }
           }}>
-            
         <Text style={styles.sheetText}>Appointment action</Text>
         <TouchableOpacity style={styles.cancel} onPress={()=>{deleteAppointment(id, appointments, setAppointments); refRBSheet.current.close();}}><Text style={styles.buttonText}>Cancel appointment</Text></TouchableOpacity>
-        <TouchableOpacity style={styles.go} onPress={()=>{navigation.navigate("AppointmentStack", { screen: "CallScreen", params: { appointmentId: id },}); refRBSheet.current.close();}}><Text style={styles.buttonText}>Go to appointment</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.go} disabled={buttonDisabled} onPress={()=>{navigation.navigate("AppointmentStack", { screen: "CallScreen", params: { appointmentId: id },}); refRBSheet.current.close();}}><Text style={styles.buttonText}>Go to appointment</Text></TouchableOpacity>
         </RBSheet>
             <View style={styles.container}>
                 <Text style={styles.title}>Appointments</Text>
@@ -49,7 +49,7 @@ export default function AppointmentsScreen() {
                 <FlatList data={groupedAppointments[date]} renderItem={(dateData) =>{
                     return(
                         <>
-                        <TouchableOpacity style={styles.appointmentCard} onPress={()=>{refRBSheet.current.open(); setId(dateData.item.schedule_id);}}>
+                        <TouchableOpacity style={styles.appointmentCard} onPress={()=>{refRBSheet.current.open(); setId(dateData.item.schedule_id); setButtonDisabled(isAppointmentTime(dateData.item))}}>
                             <View style={styles.cardContent}>
                                 <Text style={styles.date}>{dateData.item.schedule.date} {dateData.item.schedule.start_time} - {dateData.item.schedule.end_time}</Text>
                                 <Text style={styles.details}>Appointment with {dateData.item[appointmentWith].fname} {dateData.item[appointmentWith].lname}</Text>
