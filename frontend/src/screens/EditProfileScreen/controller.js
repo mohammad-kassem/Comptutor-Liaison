ea5ba9a25abd5b4507b6b84dc2375e1af4fe6403
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-26 14:08:13 +0300

    fronetnd: feat: link the delete degree api with the EditProfileScreen

---
+++ frontend/src/screens/EditProfileScreen/controller.js
@@ -0,0 +1,29 @@
+import { View, Text, ToastAndroid } from 'react-native'
+import React from 'react'
+import { getToken } from '../../components/utility/Token'
+import axios from 'axios'
+
+
+export async function deleteDegree(id, degrees, setDegrees, user, setUser){
+    const token = await getToken()
+    axios({
+        method: "delete",
+        url: `http://192.168.1.105:8000/api/v1/tutor/degree/delete/${id}`,
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": `Bearer ${token}`
+        }
+        })
+    .then(function(response){
+        const newDegrees = degrees.filter((degree)=>
+        degree.id !== id)
+        setDegrees(newDegrees)
+        user.degrees = newDegrees
+        setUser(user)
+    })
+    .catch(function(error){
+
+        let message = Object.values(error.response.data);
+        ToastAndroid.show(message[0][0], ToastAndroid.SHORT)
+    })
+}
