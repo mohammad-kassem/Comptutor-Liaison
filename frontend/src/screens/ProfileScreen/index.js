db0e9f9176e4dd269b33a0b2305520c32cd8c9bf
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-27 00:42:32 +0300

    frontend: feat: implement the image picker in the ProfileScreen

--- frontend/src/screens/ProfileScreen/index.js
+++ frontend/src/screens/ProfileScreen/index.js
@@ -1,19 +1,47 @@
 import { View, Text, Image, TouchableOpacity } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './styles'
 import { useUser } from '../../Context/User'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import { useNavigation } from '@react-navigation/native'
 import { setToken } from '../../components/utility/Token'
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import * as ImagePicker from 'expo-image-picker';
+import { updateImage } from './controller'
+
+
 
 export default function ProfileScreen() {
+
     const { user, setUser } = useUser()
+    const [image, setImage] = useState(user.profile_image);
+
     const navigation = useNavigation()
+    async function pickImage(){
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.All,
+          allowsEditing: true,
+          base64: true,
+          aspect: [4, 4],
+          quality: 1,
+        });
+        
+        if (!result.cancelled) {
+            setImage(`data:image/jpg;base64,${result.base64}`);
+            updateImage(`data:image/jpg;base64,${result.base64}`, user)
+          }
+        };
+
+
     return (
         <View style={styles.container}>
-        <View style={styles.imageContainer}>
-                <Image style={styles.profile} source={require('../../../assets/logo.png')}/>
-            </View>
+            <TouchableOpacity style={styles.imageContainer} onPress={pickImage}>
+                {image ? 
+                (<Image style={styles.profile} source={{uri: image}}/>
+                ) : (
+                    <Image style={styles.profile} source={require('../../../assets/logo.png')}/>
+                )}
+            </TouchableOpacity>
             <Text style={styles.name}>{user.fname} {user.lname}</Text>
             {user.role_id === 1 && <View style={styles.balanceContainer}>
                 <Text style={styles.balance}>Balance</Text>
