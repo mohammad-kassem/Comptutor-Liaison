de182236a7d555ae62471967c25bf43b8ccfda5b
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-26 14:54:06 +0300

    frontend: feat: impelement the date dorpdown in teh SchedulesScreen

--- frontend/src/screens/ScheduleScreen/index.js
+++ frontend/src/screens/ScheduleScreen/index.js
@@ -1,61 +1,54 @@
 import { View, Text } from 'react-native'
 import React, { useEffect, useState } from 'react'
 import styles from './styles'
-import { getAvailableTimes, groupSchedules } from './controller'
+import { filterAvailableTimes, getAvailableTimes, groupSchedules } from './controller'
 import { FlatList, ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
 import AppointmentModal from '../../components/AppointmentModal'
 import { useFocusEffect } from '@react-navigation/native'
 import Container from '../../components/Container'
+import DropdownComponent from '../../components/Dropdown'
+
+
 
 export default function ScheduleScreen({ route }) {
     const [modalIsVisable, setModalIsVisable] = useState(false);
     const tutor = route.params.tutor
-    const [schedules, setSchedules] = useState([])
+    let [schedules, setSchedules] = useState([])
     let [modalData, setModalData] = useState([])
-    let groupedSchedules = groupSchedules(schedules);
-
+    const [date, setDate] = useState("");
 
     useFocusEffect(
         React.useCallback(()=>{
             getAvailableTimes(tutor.id, setSchedules);
         }, [])
     )
+    
+    schedules = filterAvailableTimes(schedules)
+    const groupedSchedules = groupSchedules(schedules);
 
     return (
         <>
-    
-        <ScrollView>
         <Container>
             <Text style={styles.title}>{tutor.fname} {tutor.lname}'s Schedule</Text>
-            {Object.keys(groupedSchedules).map((scheduleDate) => {
-                return (
-                <>
-                <Text style={styles.date}>{scheduleDate}</Text>
-                <View style={styles.timesContainer}>
-                {groupedSchedules[scheduleDate].map((time, index)=>{
-                     time.start_time = time.start_time.split(":")[0] + ":" + time.start_time.split(":")[1]
-                     time.end_time = time.end_time.split(":")[0] + ":" + time.end_time.split(":")[1]
-                     return(
-                     index % 2 === 0 ? <View style={styles.timeCardLeft}>
-                        <TouchableOpacity style={styles.timeContainer} onPress={()=>{setModalIsVisable(true); setModalData([time, scheduleDate])}}>
-                            <Text style={styles.time}>{time.start_time}</Text>
-                            <Text style={styles.time}>{time.end_time}</Text>
-                        </TouchableOpacity>
-                     </View>
-                     :
-                     <View style={styles.timeCardRight}>
-                        <TouchableOpacity style={styles.timeContainer} onPress={()=>{setModalIsVisable(true); setModalData([time, scheduleDate])}}>
-                            <Text style={styles.time}>{time.start_time}</Text>
-                            <Text style={styles.time}>{time.end_time}</Text>
-                        </TouchableOpacity>
-                     </View>)})}
-                </View>
-                </>
-            )})}
+            <DropdownComponent date={date} setDate={setDate} groupedSchedules={groupedSchedules}/>
+            <FlatList numColumns={2} columnWrapperStyle={styles.timesContainer} data={groupedSchedules[date]} renderItem={(dateData) =>{
+                dateData.item.start_time = dateData.item.start_time.split(":")[0] + ":" + dateData.item.start_time.split(":")[1]
+                dateData.item.end_time = dateData.item.end_time.split(":")[0] + ":" + dateData.item.end_time.split(":")[1]
+                return(
+                    <>
+                    <TouchableOpacity style={styles.timeContainer} onPress={()=>{setModalIsVisable(true); setModalData([dateData.item, date])}}>
+                        <Text style={styles.time}>{dateData.item.start_time}</Text>
+                        <Text style={styles.time}>{dateData.item.start_time}</Text>
+                    </TouchableOpacity>
+                    </>
+                    
+                )}
+            }
+            />
+            
             <AppointmentModal modalIsVisable={modalIsVisable} setModalIsVisable={setModalIsVisable} time={modalData[0]} date={modalData[1]} tutor={tutor}
                 schedules={schedules} setSchedules={setSchedules}/>
         </Container>
-        </ScrollView>
         </>
     )
 }
