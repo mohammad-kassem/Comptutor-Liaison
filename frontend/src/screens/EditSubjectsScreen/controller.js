dfa210c49ad227f466f6c9e6b963cf2471cda5cb
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-26 14:30:38 +0300

    frontend: feat: implement the selcting subjects handeling in the EditSubjectsScreen

--- frontend/src/screens/EditSubjectsScreen/controller.js
+++ frontend/src/screens/EditSubjectsScreen/controller.js
@@ -22,3 +22,28 @@ export async function getSubjects(setSubjects, user) {
         ToastAndroid.show(message[0][0], ToastAndroid.SHORT)
     })
 }
+
+export function selectedSubject(id, subject, image, selectedSubjects, setSelectedSubjects) {
+    const selectedSubject = {"id": id, "subject": subject, "image": image}
+    setSelectedSubjects([...selectedSubjects, selectedSubject])
+}
+
+export function deselectedSubject(id, subject, selectedSubjects, setSelectedSubjects) {
+    setSelectedSubjects(selectedSubjects.filter((selectedSubject)=>(
+            selectedSubject.id !== id
+        ))
+    )
+}
+
+export function handleSelect(id, subject, image, selectedSubjects, setSelectedSubjects){
+    selectedSubjects.some((selectedSubject) => selectedSubject.id === id) ? deselectedSubject(id, subject, selectedSubjects, setSelectedSubjects)
+    : selectedSubject(id, subject, image, selectedSubjects, setSelectedSubjects)
+}
+
+export function isSelected(id, selectedSubjects){
+    let found = false;
+    selectedSubjects.forEach((selectedSubject) => {
+        if (selectedSubject.id === id) found = true
+    });
+    return found
+}
