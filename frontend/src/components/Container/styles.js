9a4aef134fcf5502397c9bf1a54af8168514cfc6
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-31 00:49:48 +0300

    frontend: feat: update the Continer styling

--- frontend/src/components/Container/styles.js
+++ frontend/src/components/Container/styles.js
@@ -2,10 +2,9 @@ import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
     container:{
-        flex: 1,
         justifyContent: "flex-start",
         alignItems: "stretch",
-        backgroundColor: "#f5f5f5",
+        // backgroundColor: "#f5f5f5",
         paddingVertical: 25,
         paddingHorizontal: 25,
     },
