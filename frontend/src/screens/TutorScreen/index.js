d657301fca29ee968613318a189c3ce1a70ac2c2
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-31 01:20:41 +0300

    frontend: feat: update the TutorScreen styling

--- frontend/src/screens/TutorScreen/index.js
+++ frontend/src/screens/TutorScreen/index.js
@@ -1,4 +1,4 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
 import React, { useState } from 'react'
 import styles from './styles'
 import FullWidthButton from '../../components/FullWidthButton'
@@ -7,11 +7,13 @@ import { useFocusEffect, useNavigation } from '@react-navigation/native'
 import { getTutor, onHandlePress } from './controller'
 import AbsolutePositionButtonContainer from '../../components/AbsolutePositionButtonContainer'
 import Icon from 'react-native-vector-icons/FontAwesome'
+import Container from '../../components/Container'
 
 export default function TutorScreen( {route} ) {
     const [tutor, setTutor] = useState({"degrees": [], "subjects": []})
     const tutorId = route.params.tutor.id
     const navigation = useNavigation() 
+    const flexDirection = [{marginRight: "3%"}, {alignSelf: "center"}, {marginLeft: "3%"}]
 
     useFocusEffect(
         React.useCallback(()=>{
@@ -21,8 +23,8 @@ export default function TutorScreen( {route} ) {
 
     return (
         <>
-        <View style={styles.container}>
         <ScrollView contentContainerStyle={styles.scrollContainer}>
+        <Container>
             <View style={styles.imageContainer}>
             {tutor.profile_image ? 
             (<Image style={styles.tutorProfile} source={{uri: tutor.profile_image}}/>
@@ -34,15 +36,15 @@ export default function TutorScreen( {route} ) {
                 <View style={styles.titleContainer}>
                     <View style={styles.title}>
                         <Text style={styles.tutorName}>{tutor.fname} {tutor.lname}</Text>
-                        <Text style={styles.tutorExperince}>Teaching since {}</Text>
+                        <Text style={styles.titleContent}>Teaching Since: {tutor.since}</Text>
+                        <View style={styles.rateContainer}>
+                            <Text style={styles.rate}>{tutor.rate}$ / H</Text>
+                            <Text style={styles.titleContent}>for live session</Text>
+                        </View>
                     </View>
-                    <TouchableOpacity style={styles.chatting} onPress={() => navigation.navigate("ChatStack", { screen: "ChatScreen",  params: { reciever: tutor, isStudent: true },})}>
-                        <Icon name="comments" size={24} color="white"/>
-                        <Text style={styles.chattingText}>Chat with {tutor.fname}</Text>
-                    </TouchableOpacity>
                 </View>
-                <View style={styles.ratesContainer}>
-                    <Text style={styles.videoRate}>${tutor.rate} /H for Live Session</Text>
+                <View style={styles.chat}>
+                    <FullWidthButton text="Start a Chat" onHandlePress={() => navigation.navigate("ChatStack", { screen: "ChatScreen",  params: { reciever: tutor, isStudent: true },})} icon="send"/>
                 </View>
                 <View style={styles.tutorDetails}>
                     <View style={styles.infoCard}>
@@ -61,9 +63,11 @@ export default function TutorScreen( {route} ) {
                     </View>
                     <View style={styles.infoCard}>
                         <Text style={styles.infoTitle}>Subjects</Text>
-                        <View style={styles.infoMain}>
-                            {tutor.subjects.map((subject) =>(
-                            <Text style={styles.infoText}>{subject.subject}</Text>
+                        <View style={styles.subjectsContainer}>
+                            {tutor.subjects.map((subject, index) =>(
+                            <View style={[styles.subjectContainer, flexDirection[index % 3]]}>
+                            <Text style={styles.subjectName}>{subject.subject}</Text>
+                            </View>
                             ))}
                         </View>
                     </View>
@@ -72,11 +76,12 @@ export default function TutorScreen( {route} ) {
                     </View>
                 </View>
             </View>
+            </Container>
+
         </ScrollView>
         <AbsolutePositionButtonContainer>
-            <FullWidthButton text="Book an appointment" onHandlePress={()=>onHandlePress(tutor, navigation)}/>
+            <FullWidthButton text="Book an appointment" onHandlePress={()=>onHandlePress(tutor, navigation)} color="green"/>
         </AbsolutePositionButtonContainer>
-        </View>
         </>
     )
 }
--- frontend/src/screens/TutorScreen/styles.js
+++ frontend/src/screens/TutorScreen/styles.js
@@ -1,12 +1,6 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    container:{
-        flex: 1,
-        backgroundColor: "#f5f5f5",
-        position: "relative"
-    },
-
     scrollContainer:{
         flexGrow: 1,
         justifyContent: "flex-start",
@@ -14,8 +8,10 @@ export default StyleSheet.create({
     },
 
     imageContainer: {
-        width: "100%",
-        height: 240,
+        marginTop: 30,
+        width: 100,
+        height: 100,
+        alignSelf: "center",
     },
 
     tutorProfile: {
@@ -23,18 +19,22 @@ export default StyleSheet.create({
         width: undefined,
         height: undefined,
         resizeMode: "stretch",
+        borderRadius: 100,
+
     },
 
     infoContainer: {
         backgroundColor: "#f5f5f5",
-        elevation: 1,
-        width: "100%"
+        width: "100%",
+        justifyContent: "center",
+        alignItems: "stretch",
     },
 
     titleContainer: {
         flexDirection: "row",
         alignItems: "center",
-        justifyContent: "space-between"
+        justifyContent: "center",
+        marginBottom: 10
     },
 
     title: {
@@ -43,73 +43,70 @@ export default StyleSheet.create({
     },
 
     tutorName: {
-        fontWeight: "600",
-        fontSize: 24
+        fontWeight: "700",
+        fontSize: 22,
+        textAlign: "center"
     },
 
-    tutorExperince: {
+    titleContent: {
         fontSize: 18,
-        color: "#656565"
+        color: "#919090",
+        textAlign: "center"
     },
 
-    chatting: {
+    rateContainer: {
         flexDirection: "row",
-        marginHorizontal: 25,
-        backgroundColor: "#1877F2",
-        padding: 15,
-        borderRadius: 15,
-    },
-
-    chattingText: {
-        paddingLeft: 5,
-        fontSize: 14,
-        color: "white",
-    },
-
-    ratesContainer: {
-        width: "100%",
-        height: 60,
-        backgroundColor: "white",
-        flexDirection: "row",
-        alignItems: "stretch",
         justifyContent: "center",
+        alignItems: "center",
+        marginBottom: 10
     },
 
-    videoRate: {
-        textAlignVertical: "center",
-        textAlign: "center",
-        fontSize: 18,
-        color: "#1877F2",
-        alignSelf: "stretch",
-        paddingHorizontal: 60,
-        lineHeight: 22,
+    rate: {
+        fontSize: 22,
+        fontWeight: "600",
+        color: "#E8A624",
+        marginRight: 5
     },
 
+    chat: {
+        marginBottom: 15
+    },
 
     infoCard: {
-        margin: 15,
-        marginHorizontal: 25,
         padding: 15,
-        backgroundColor: "white",
-        borderRadius: 20,
-        elevation: 10,  
     },
 
     infoTitle: {
-        fontSize: 18,
-        lineHeight: 22,
-        color: "#FF3142",
+        fontSize: 22,
         paddingBottom: 10,
-    },
-
-    infoMain: {
-        paddingBottom: 10,
+        fontWeight: "500"
     },
 
     infoText: {
         fontSize: 18,
         lineHeight: 30,
         textAlign: "justify"
+    },
+
+    subjectsContainer: {
+        flexDirection: "row",
+        flexWrap: "wrap",
+        alignItems: "center"
+    },
+
+    subjectContainer: {
+        padding: 10,
+        backgroundColor: "#C7E9F1",
+        borderRadius: 20,
+        width: "30%",
+        alignSelf: "center"
+    },
+
+    subjectName:{
+        textAlign: "center",
+        fontSize: 12,
+        color: "#1877F2",
+        fontWeight: "700",
     },
 
     blank: {
