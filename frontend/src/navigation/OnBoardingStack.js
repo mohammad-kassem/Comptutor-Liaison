ae034b3e9cc7514e1974e3f94f8aaa7758d16f69
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-07-31 12:22:16 +0300

    frontend: fix: resolve the imports in the OnBoardingStack

--- frontend/src/navigation/OnBoardingStack.js
+++ frontend/src/navigation/OnBoardingStack.js
@@ -1,4 +1,4 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 
 import LoginScreen from "../screens/LoginScreen";
@@ -7,8 +7,6 @@ import RegisterScreen from "../screens/RegisterScreen";
 import React from 'react'
 import AddSubjectsScreen from "../screens/AddSubjectsScreen";
 import OnBoardingStackTutor from "./OnBoardingStackTutor";
-import EditProfileScreen from "../screens/EditProfileScreen";
-import EditProfileStack from "./EditProfileStack";
 
 export default function OnBoardingStack() {
     const RootStackNav = createStackNavigator();
