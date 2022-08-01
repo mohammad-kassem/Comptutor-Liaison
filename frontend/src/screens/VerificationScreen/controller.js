9b02f3cfcdb8bbeab2b6371c5c4fd8337867efe9
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-01 16:55:49 +0300

    frontend: feat: link the verify email api with the VerificationScreen

---
+++ frontend/src/screens/VerificationScreen/controller.js
@@ -0,0 +1,28 @@
+import { View, Text, ToastAndroid } from 'react-native'
+import React from 'react'
+import { getToken } from '../../components/utility/Token'
+import axios from 'axios'
+import { localHostV1 } from '../../contsants/constants'
+
+export async function verifyEmail(code, user, navigation) {
+    if (code.length !== 4) return 
+    const token = await getToken()
+    axios({
+        method: "put",
+        url: `${localHostV1}/email/verify`,
+        headers: {
+            "Content-type": "application/json",
+            "Authorization": `Bearer ${token}`
+            },
+        data: JSON.stringify({code}) 
+    })
+    .then(function(response){
+        user = response.data.user
+        navigation.navigate('AddSubjectsScreen', {user: response.data.user})
+    })
+    .catch(function(error){
+        console.log(error)
+        let message = Object.values(error.response.data);
+        ToastAndroid.show(message[0][0], ToastAndroid.SHORT)
+    })
+}
