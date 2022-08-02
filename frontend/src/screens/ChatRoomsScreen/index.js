b09aab109fa3137bdc108018b4da1034131b6945
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-02 11:17:46 +0300

    frontend: feat: use the ChatRoom component in the ChatRoomsScreen

--- frontend/src/screens/ChatRoomsScreen/index.js
+++ frontend/src/screens/ChatRoomsScreen/index.js
@@ -6,15 +6,13 @@ import database from '@react-native-firebase/database'
 import { useUser } from '../../Context/User'
 import { filterRooms } from './controller'
 import { useRooms } from '../../Context/Rooms'
+import ChatRoom from '../../components/ChatRoom'
 
 
 export default function ChatRoomsScreen() {
 	const navigation = useNavigation()
 	const {user} = useUser()
 	let {rooms, setRooms} = useRooms()
-	const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "June",
-	"July", "Aug", "Sept", "Oct", "Nov", "Dec"];
-	console.log(rooms)
 	
 	rooms = filterRooms(rooms, user)
 
@@ -24,43 +22,7 @@ export default function ChatRoomsScreen() {
 		<View style={styles.roomContainer}>
 			<FlatList data={rooms} renderItem={(roomData) =>{
                 return(
-					<TouchableOpacity style={styles.room} onPress={() => navigation.navigate("ChatStack", { screen: "ChatScreen",  params: { reciever: {"id": user.role_id === 1 ? roomData.item[0].split("-")[1] : roomData.item[0].split("-")[0]}},})}>
-						{user.role_id === 1 ? 
-						<>
-						<View style={styles.imageContainer}>
-							{roomData.item[1].tutorImage === '../../../assets/logo.png' ?
-							<Image style={styles.profile} source={require('../../../assets/logo.png')}/>
-							:<Image style={styles.profile} source={{uri: roomData.item[1].tutorImage}}/>}
-						</View>
-						<View style={styles.content}>
-						<Text style={styles.time}>{monthNames[new Date(roomData.item[1].lastSent).getMonth()+1]} {new Date(roomData.item[1].lastSent).getDate()}, 
-							{new Date(roomData.item[1].lastSent).getHours()}:{new Date(roomData.item[1].lastSent).getMinutes() < 10 ? "0" : ""}{new Date(roomData.item[1].lastSent).getMinutes()}
-							</Text>
-							<View style={styles.header}>
-							<Text style={styles.contact}>{roomData.item[1].tutorName}</Text>
-							</View>
-							<Text style={styles.message}>{roomData.item[1].lastMessage.substring(0, 25)}{roomData.item[1].lastMessage.length > 20 && "..."}</Text>
-						</View>
-						</>
-						:
-						<>
-						<View style={styles.imageContainer}>
-							{roomData.item[1].studentImage === '../../../assets/logo.png' ?
-							<Image style={styles.profile} source={require('../../../assets/logo.png')}/>
-							:<Image style={styles.profile} source={{uri: roomData.item[1].studentImage}}/>}
-						</View>
-						<View style={styles.content}>
-						<Text style={styles.time}>{monthNames[new Date(roomData.item[1].lastSent).getMonth()+1]} {new Date(roomData.item[1].lastSent).getDate()}, 
-							{new Date(roomData.item[1].lastSent).getHours()}:{new Date(roomData.item[1].lastSent).getMinutes() < 10 ? "0" : ""}{new Date(roomData.item[1].lastSent).getMinutes()}
-							</Text>
-							<View style={styles.header}>
-							<Text style={styles.contact}>{roomData.item[1].studentName}</Text>
-							</View>
-							<Text style={styles.message}>{roomData.item[1].lastMessage}</Text>
-						</View>
-						</>
-					}
-				 	</TouchableOpacity>
+					<ChatRoom roomData={roomData}/>
 				)
 			}}/>
 			</View>
