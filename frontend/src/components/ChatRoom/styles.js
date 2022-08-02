39044045a68ddcaf7701be1dbeb2f26a1704ae2c
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-02 11:17:23 +0300

    frontend: feat: implement the ChatRoom component

---
+++ frontend/src/components/ChatRoom/styles.js
@@ -0,0 +1,60 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    room: {
+        backgroundColor: "white",
+        flexDirection: "row",
+        marginBottom: 20,
+        marginHorizontal: 25,
+        padding: 15,
+        alignItems: "center",
+        borderRadius: 20,
+        elevation: 10
+    },
+
+    imageContainer: {
+        width: 62,
+        height: 62, 
+        marginRight: 15    
+    },
+
+    profile: {
+        flex: 1,
+        width: undefined,
+        height: undefined,
+        borderRadius: 50
+    },
+
+    content:{
+        width: "70%"
+    },
+
+    time: {
+        color: "#E8A624",
+        fontSize: 12,
+        textAlignVertical: "center",
+        alignSelf: "flex-end",
+        fontWeight: "500"
+
+    },
+
+    header:{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+    },
+
+
+    contact: {
+        fontSize: 18,
+        fontWeight: "600",
+        marginBottom: 10,
+        includeFontPadding: false
+    },
+
+    message: {
+        color: "#919090",
+        fontSize: 16,
+        marginBottom: 15
+    },
+})
