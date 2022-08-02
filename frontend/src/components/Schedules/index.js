ebaaf18e887f64d23ae03954d1143459aa658d63
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-02 12:15:11 +0300

    frontend: feat: implement the Schedules component

---
+++ frontend/src/components/Schedules/index.js
@@ -0,0 +1,32 @@
+import { View, Text, TouchableOpacity } from 'react-native'
+import React from 'react'
+import { FlatList } from 'react-native-gesture-handler'
+import { useUser } from '../../Context/User'
+import styles from './styles'
+
+export default function Schedules({groupedSchedules, date, setModalIsVisable, setModalData}) {
+    const {user} = useUser()
+
+    return (
+        <FlatList numColumns={2} columnWrapperStyle={styles.timesContainer} data={groupedSchedules[date]} renderItem={(dateData) =>{
+            dateData.item.start_time = dateData.item.start_time.split(":")[0] + ":" + dateData.item.start_time.split(":")[1]
+            dateData.item.end_time = dateData.item.end_time.split(":")[0] + ":" + dateData.item.end_time.split(":")[1]
+            return(
+                <>
+                {user.role_id === 1 ?
+                <TouchableOpacity style={styles.timeContainer} onPress={()=>{setModalIsVisable(true); setModalData([dateData.item, date])}}>
+                    <Text style={styles.time}>{dateData.item.start_time}</Text>
+                    <Text style={styles.time}>{dateData.item.end_time}</Text>
+                </TouchableOpacity>
+                :
+                <TouchableOpacity style={styles.timeContainer}>
+                    <Text style={styles.time}>{dateData.item.start_time}</Text>
+                    <Text style={styles.time}>{dateData.item.end_time}</Text>
+                </TouchableOpacity>
+                }
+                </>
+            )}
+        }
+        />
+    )
+}
---
+++ frontend/src/components/Schedules/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    timesContainer:{
+        flex: 1,
+        justifyContent: "space-between",
+        margin: 25,
+    },
+
+
+    timeContainer: {
+        backgroundColor: "white",
+        borderRadius: 15,
+        elevation: 5,
+        justifyContent: "center",
+        paddingVertical: 10,
+        paddingHorizontal: 25,
+        elevation: 10
+    },
+
+    time: {
+        textAlign: "center",
+        textAlignVertical: "center",
+        fontSize: 18,
+        fontWeight: "500",
+        lineHeight: 22,
+        textAlign: "center",
+        letterSpacing: 2
+    },
+})
