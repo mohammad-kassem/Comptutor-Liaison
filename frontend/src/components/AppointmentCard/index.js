da308ffb254f59b33da883aa793ead068f7f50d8
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-02 12:19:01 +0300

    frontend: feat: implement the AppointmentCard component

---
+++ frontend/src/components/AppointmentCard/index.js
@@ -0,0 +1,30 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+import React from 'react'
+import { isAppointmentTime, isCancelTime } from '../../screens/AppointmentsScreen/controller';
+import styles from './styles';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import { useUser } from '../../Context/User';
+
+export default function AppointmentCard({refRBSheet, setId, dateData, setGoButtonDisabled, setCancelButtonDisabled}) {
+    const {user} = useUser()
+    const appointmentWith = user.role_id === 1 ? "tutor" : "student"
+    
+    return (
+        <>
+        <TouchableOpacity style={styles.appointmentCard} onPress={()=>{refRBSheet.current.open(); setId(dateData.item.schedule_id); setGoButtonDisabled(isAppointmentTime(dateData.item)); setCancelButtonDisabled(!isCancelTime(dateData.item))}}>
+                <Text style={styles.date}>{dateData.item.schedule.start_time.slice(0, -3)} - {dateData.item.schedule.end_time.slice(0, -3)}</Text>
+                <View style={styles.appointmentWith}>
+                    <Icon name="account-multiple" size={20} color="#4FC7E6"/>
+                    <View style={styles.imageContainer}>
+                    {dateData.item[appointmentWith].profile_image ? 
+                    (<Image style={styles.profile} source={{uri: dateData.item[appointmentWith].profile_image}}/>
+                    ) : (
+                    <Image style={styles.profile} source={require('../../../assets/logo.png')}/>
+                    )}
+                    </View>
+                    <Text style={styles.details}>{dateData.item[appointmentWith].fname} {dateData.item[appointmentWith].lname}</Text>
+                </View>
+        </TouchableOpacity>
+        </>
+    )
+}
---
+++ frontend/src/components/AppointmentCard/styles.js
@@ -0,0 +1,50 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    appointmentCard: {
+        padding: 20,
+        marginVertical: 15,
+        marginHorizontal: 25,
+        borderRadius: 20,
+        backgroundColor: "white",
+        elevation: 10,
+        alignSelf: "stretch",
+        height: 100,
+        justifyContent: "space-between",
+        alignItems: "center"
+    },
+
+    date:{
+        fontSize: 20,
+        fontWeight: "500",
+        lineHeight: 22,
+        textAlign: "center",
+        letterSpacing: 5
+    },
+
+    appointmentWith:{
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center"
+    },
+
+    imageContainer: {
+        width: 24,
+        height: 24, 
+        marginHorizontal: 5    
+    },
+
+    profile: {
+        flex: 1,
+        width: undefined,
+        height: undefined,
+        borderRadius: 50
+    },
+
+    details: {
+        fontSize: 16,
+        fontWeight: "500",
+        lineHeight: 22,
+        textAlign: "center"
+    },
+})
