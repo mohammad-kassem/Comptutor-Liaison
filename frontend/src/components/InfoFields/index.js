88ab1d4a5a222e3501007f880e9379777dc0c912
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-02 09:39:22 +0300

    frontend: feat: implement the InfoFields component

---
+++ frontend/src/components/InfoFields/index.js
@@ -0,0 +1,29 @@
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
+import styles from './styles'
+import { useUser } from '../../Context/User'
+
+export default function InfoFields({type, setFname, setLname, setRate, setYears, setAbout, user}) {
+
+    return (
+        <>
+        {type === "editinfo" &&
+        <>
+        <Text style={styles.fieldTitle}>First name</Text>
+        <TextInput style={styles.field} defaultValue={user.fname} placeholder="First name" onChangeText={(enteredText)=>{setFname(enteredText)}}/>
+        <Text style={styles.fieldTitle}>Last name</Text>
+        <TextInput style={styles.field} defaultValue={user.lname} placeholder="Last name" onChangeText={(enteredText)=>{setLname(enteredText)}}/>
+        </>
+        }
+        {user.role_id === 2 && 
+        <>
+        <Text style={styles.fieldTitle}>Rate</Text>
+        <TextInput style={styles.field} defaultValue={user.rate && user.rate.toFixed()} placeholder="Rate" keyboardType='decimal-pad' onChangeText={(enteredText)=>{setRate(enteredText)}}/>
+        <Text style={styles.fieldTitle}>Teaching since</Text>
+        <TextInput style={styles.field} defaultValue={user.since && user.since.toFixed()} placeholder="Teaching since" keyboardType='decimal-pad' onChangeText={(enteredText)=>{setYears(enteredText)}}/>
+        <Text style={styles.fieldTitle} multiline={true}>About</Text>
+        <TextInput style={styles.about} defaultValue={user.about} placeholder="About" multiline={true} onChangeText={(enteredText)=>{setAbout(enteredText)}}/>
+        </>}
+        </>
+    )
+}
---
+++ frontend/src/components/InfoFields/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from "react-native";
+
+export default StyleSheet.create({
+    fieldTitle: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: "#919090",
+    fontWeight: "500"
+    },
+
+    field: {
+    marginBottom: 20,
+    paddingBottom: 5,
+    textAlignVertical: 'bottom',
+    alignItems:"flex-start",
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderColor: "#919090"
+    },
+
+    about: {
+    marginBottom: 20,
+    paddingBottom: 5,
+    textAlignVertical: 'top',
+    alignItems:"flex-start",
+    fontSize: 18,
+    height: 60,
+    borderBottomWidth: 1,
+    borderColor: "#919090"
+    },
+})
