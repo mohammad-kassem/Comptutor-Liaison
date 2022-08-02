5f472395824b78a4b4ac55ebc174a7427b0c0e7e
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-02 09:40:34 +0300

    frontend: feat: use the InfoFields component in the EditInfoScreen

--- frontend/src/screens/EditInfoScreen/index.js
+++ frontend/src/screens/EditInfoScreen/index.js
@@ -8,6 +8,7 @@ import { addInfo } from './controller'
 import Container from '../../components/Container'
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
 import FullWidthButton from '../../components/FullWidthButton'
+import InfoFields from '../../components/InfoFields'
 
 export default function EditInfoScreen() {
     const {user, setUser} = useUser()
@@ -25,19 +26,7 @@ export default function EditInfoScreen() {
                 <Text style={styles.title}>Edit Personal Info</Text>
             </View>
             <Container>
-            <Text style={styles.fieldTitle}>First name</Text>
-            <TextInput style={styles.field} defaultValue={fname} placeholder="First name" onChangeText={(enteredText)=>{setFname(enteredText)}}/>
-            <Text style={styles.fieldTitle}>Last name</Text>
-            <TextInput style={styles.field} defaultValue={lname} placeholder="Last name" onChangeText={(enteredText)=>{setLname(enteredText)}}/>
-            {user.role_id === 2 && 
-            <>
-            <Text style={styles.fieldTitle}>Rate</Text>
-            <TextInput style={styles.field} defaultValue={user.rate.toFixed()} placeholder="Rate" keyboardType='decimal-pad' onChangeText={(enteredText)=>{setRate(enteredText)}}/>
-            <Text style={styles.fieldTitle}>Teaching since</Text>
-            <TextInput style={styles.field} defaultValue={user.since.toFixed()} placeholder="Teaching since" keyboardType='decimal-pad' onChangeText={(enteredText)=>{setYears(enteredText)}}/>
-            <Text style={styles.fieldTitle} multiline={true}>About</Text>
-            <TextInput style={styles.about} defaultValue={about} placeholder="About" multiline={true} onChangeText={(enteredText)=>{setAbout(enteredText)}}/>
-            </>}
+            <InfoFields type="editinfo" setFname={setFname} setLname={setLname} setRate={setRate} setYears={setYears} setAbout={setAbout} user={user}/>
             <View style={[styles.buttonsContainer, user.role_id === 1 && styles.studentButton]}>
                 <FullWidthButton text="Save Changes" onHandlePress={()=>addInfo({"fname": fname, "lname": lname, "years": parseInt(years), "rate": parseInt(rate),"about_me": about}, user, setUser, navigation)}/>
             </View>
