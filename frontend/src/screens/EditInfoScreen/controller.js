3bb4235e4d4419aa0537616eca853e5358baf5cf
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-03 17:09:23 +0300

    frontend: feat: implement real time update of the chat room profile details

--- frontend/src/screens/EditInfoScreen/controller.js
+++ frontend/src/screens/EditInfoScreen/controller.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import { ToastAndroid } from "react-native";
 import { getToken } from "../../components/utility/Token";
 import { localHostV1 } from "../../contsants/constants";
+import database from '@react-native-firebase/database';
 
 
 export async function addInfo(input, user, setUser, navigation, setFname, setLname, setRate, setYears, setAbout){
@@ -20,6 +21,37 @@ export async function addInfo(input, user, setUser, navigation, setFname, setLna
         user = {...user, ...response.data[role]}
         setUser(user)
         navigation.goBack()
+        if (user.role_id === 1){
+            database()
+            .ref(`rooms`)
+            .orderByChild("studentId")
+            .equalTo(user.id)
+            .once("value").then(snapshot=>{
+                snapshot._snapshot.childKeys.forEach(element => {
+                    database()
+                    .ref(`rooms/${element}`)
+                    .update({
+                        studentName: `${user.fname} ${user.lname}` 
+                    })
+                });
+            })
+        }
+        else {
+            database()
+            .ref(`rooms`)
+            .orderByChild("tutorId")
+            .equalTo(user.id)
+            .once("value").then(snapshot=>{
+                console.log(snapshot._snapshot.childKeys)
+                snapshot._snapshot.childKeys.forEach(element => {
+                    database()
+                    .ref(`rooms/${element}`)
+                    .update({
+                        tutorName: `${user.fname} ${user.lname}` 
+                    })
+                });
+            })
+        }
     })
     .catch(function(error){
         console.log(error)
