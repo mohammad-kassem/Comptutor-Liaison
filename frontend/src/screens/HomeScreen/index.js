2c4b90ca073016e4d9f542da140b9f2fa84a827d
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:30:30 +0300

    frontend: refactor: clean code HomeScreen

--- frontend/src/screens/HomeScreen/index.js
+++ frontend/src/screens/HomeScreen/index.js
@@ -1,39 +1,37 @@
-import { View, Text ,Image , FlatList, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import styles from './styles'
-import { useUser } from '../../Context/User';
-import  SearchBar  from '../../components/SearchBar';
-import { useFocusEffect, useNavigation } from '@react-navigation/native';
-import { filterTutors, getTutors } from './controller';
-import Container from '../../components/Container';
-import TutorCard from '../../components/TutorCard';
+import { View, Text, FlatList } from "react-native";
+import React, { useState } from "react";
+import styles from "./styles";
+import { useUser } from "../../Context/User";
+import SearchBar from "../../components/SearchBar";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import { filterTutors, getTutors } from "./controller";
+import TutorCard from "../../components/TutorCard";
 
 export default function HomeScreen({ navigation }) {
-    let [tutors, setTutors] = useState([])
-    const [original, setOriginal] = useState([])
-    const {user, setUser} = useUser()
+  let [tutors, setTutors] = useState([]);
+  const [original, setOriginal] = useState([]);
+  const { user, setUser } = useUser();
 
-    useFocusEffect(
-        React.useCallback(()=>{
-            getTutors(setTutors, setOriginal)
-        }, [])
-    )
+  useFocusEffect(
+    React.useCallback(() => {
+      getTutors(setTutors, setOriginal);
+    }, [])
+  );
 
-    tutors = filterTutors(tutors, user)
-        
-    return (
-        <>
-        <Text style={styles.title}>Home</Text>
-        <SearchBar original={original} setTutors={setTutors}/>
+  tutors = filterTutors(tutors, user);
 
+  return (
+    <>
+      <Text style={styles.title}>Home</Text>
+      <SearchBar original={original} setTutors={setTutors} />
 
-            <FlatList data={tutors} renderItem={(tutorData) =>{
-                return(
-                    <TutorCard tutorData={tutorData}/>
-                )
-            }}
-            // keyExtractor={(tutor) => tutor.id}
-            />
-        </>
-    )
+      <FlatList
+        data={tutors}
+        renderItem={(tutorData) => {
+          return <TutorCard tutorData={tutorData} />;
+        }}
+        // keyExtractor={(tutor) => tutor.id}
+      />
+    </>
+  );
 }
--- frontend/src/screens/HomeScreen/styles.js
+++ frontend/src/screens/HomeScreen/styles.js
@@ -1,95 +1,90 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+    lineHeight: 29,
+    marginVertical: 25,
+    alignSelf: "center",
+  },
 
-    title: {
-        fontSize: 24,
-        fontWeight: "700",
-        lineHeight: 29,
-        marginVertical: 25,
-        alignSelf: "center"
-    },
+  tutorCard: {
+    padding: 15,
+    paddingBottom: 10,
+    marginVertical: 15,
+    marginHorizontal: 25,
+    borderRadius: 20,
+    backgroundColor: "white",
+    elevation: 10,
+  },
 
-    tutorCard: {
-        padding: 15,
-        paddingBottom: 10,
-        marginVertical: 15,
-        marginHorizontal: 25,
-        borderRadius: 20,
-        backgroundColor: "white",
-        elevation: 10
-    },
+  cardContent: {
+    flexDirection: "row",
+  },
 
-    
-    cardContent:{
-        flexDirection: "row",
-    },
+  imageContainer: {
+    width: 62,
+    height: 62,
+    marginRight: 15,
+  },
 
-    imageContainer: {
-        width: 62,
-        height: 62, 
-        marginRight: 15    
-    },
+  tutorProfile: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
+    borderRadius: 50,
+  },
 
-    tutorProfile: {
-        flex: 1,
-        width: undefined,
-        height: undefined,
-        borderRadius: 50
-    },
+  tutorInfo: {
+    flex: 1,
+    alignSelf: "center",
+  },
 
-    tutorInfo: {
-        flex: 1,
-        alignSelf: "center"
-    },
+  tutorName: {
+    fontSize: 16,
+    fontWeight: "500",
+    includeFontPadding: false,
+  },
 
-    tutorName: {
-        fontSize: 16,
-        fontWeight: "500",
-        includeFontPadding: false
-    },
+  tutorDetail: {
+    fontSize: 14,
+    marginTop: 5,
+    color: "#919090",
+  },
 
-    tutorDetail: {
-        fontSize: 14,
-        marginTop: 5,
-        color: "#919090"
-    },
+  rateContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignSelf: "flex-start",
+  },
 
-    rateContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignSelf: "flex-start"
-    },
+  rate: {
+    fontSize: 12,
+    color: "#E8A624",
+    fontWeight: "500",
+  },
 
-    rate: {
-        fontSize: 12,
-        color: "#E8A624",
-        fontWeight: "500"
-    },
+  subjectsContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+    alignItems: "center",
+    marginLeft: "27%",
+    marginTop: 15,
+  },
 
+  subjectContainer: {
+    padding: 5,
+    backgroundColor: "#C7E9F1",
+    borderRadius: 20,
+    width: "30%",
+    alignSelf: "center",
+  },
 
-    subjectsContainer: {
-        flexDirection: "row",
-        flexWrap: "wrap",
-        alignItems: "center",
-        marginLeft: "27%",
-        marginTop: 15
-    },
-
-    subjectContainer: {
-        padding: 5,
-        backgroundColor: "#C7E9F1",
-        borderRadius: 20,
-        width: "30%",
-        alignSelf: "center"
-    },
-
-    subjectName:{
-        textAlign: "center",
-        fontSize: 10,
-        fontWeight: "700",
-        color: "#1877F2"
-    },
-
-
-})
+  subjectName: {
+    textAlign: "center",
+    fontSize: 10,
+    fontWeight: "700",
+    color: "#1877F2",
+  },
+});
