3d5cf38bf222dd1395c6d767b74828dd62ebf0ae
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:38:42 +0300

    frontend: refactor: clean code SegmentedControl

--- frontend/src/components/SegmentedControl/index.js
+++ frontend/src/components/SegmentedControl/index.js
@@ -1,18 +1,32 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React from 'react'
-import styles from './styles'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import styles from "./styles";
 
-export default function SegmenetdControl({type, setType}) {
-    return (
-        <View style={styles.segmentsContainer}>
-            <View style={styles.segments}>
-            <TouchableOpacity style={type === "approved" ? styles.activeButton : styles.inactiveButton} onPress={()=>setType("approved")}>
-                <Text style={type === "approved" ? styles.active : styles.inactive}>Approved</Text>
-            </TouchableOpacity>
-            <TouchableOpacity style={type === "pending" ? styles.activeButton : styles.inactiveButton} onPress={()=>setType("pending")}>
-                <Text style={type === "pending" ? styles.active : styles.inactive}>Pending</Text>
-            </TouchableOpacity>
-            </View>
-        </View>
-    )
+export default function SegmenetdControl({ type, setType }) {
+  return (
+    <View style={styles.segmentsContainer}>
+      <View style={styles.segments}>
+        <TouchableOpacity
+          style={
+            type === "approved" ? styles.activeButton : styles.inactiveButton
+          }
+          onPress={() => setType("approved")}
+        >
+          <Text style={type === "approved" ? styles.active : styles.inactive}>
+            Approved
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={
+            type === "pending" ? styles.activeButton : styles.inactiveButton
+          }
+          onPress={() => setType("pending")}
+        >
+          <Text style={type === "pending" ? styles.active : styles.inactive}>
+            Pending
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
--- frontend/src/components/SegmentedControl/styles.js
+++ frontend/src/components/SegmentedControl/styles.js
@@ -1,45 +1,45 @@
 import { StyleSheet } from "react-native";
 
-export default StyleSheet.create({   
-    segmentsContainer: {
-        backgroundColor: "white",
-        padding: 25,
-    },
+export default StyleSheet.create({
+  segmentsContainer: {
+    backgroundColor: "white",
+    padding: 25,
+  },
 
-    segments: {
-        flexDirection: "row",
-        justifyContent: "space-evenly",
-        alignItems: "center",
-        borderRadius: 10,
-        borderWidth: 2,
-        borderColor: "#1877F2",
-    },
+  segments: {
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    borderRadius: 10,
+    borderWidth: 2,
+    borderColor: "#1877F2",
+  },
 
-    activeButton: {
-        backgroundColor: "#1877F2",
-        padding: 10,
-        alignSelf: "stretch",
-        flex: 1,
-    },
+  activeButton: {
+    backgroundColor: "#1877F2",
+    padding: 10,
+    alignSelf: "stretch",
+    flex: 1,
+  },
 
-    active: {
-        fontSize: 18,
-        fontWeight: "600",
-        color: "white",
-        textAlign: "center",
-    },
+  active: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "white",
+    textAlign: "center",
+  },
 
-    inactiveButton: {
-        backgroundColor: "white",
-        padding: 10,
-        flex: 1,
-        borderRadius: 10
-    },
+  inactiveButton: {
+    backgroundColor: "white",
+    padding: 10,
+    flex: 1,
+    borderRadius: 10,
+  },
 
-    inactive: {
-        fontSize: 18,
-        fontWeight: "600",
-        color: "#1877F2",
-        textAlign: "center"
-    },
-})
+  inactive: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#1877F2",
+    textAlign: "center",
+  },
+});
