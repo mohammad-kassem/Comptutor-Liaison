1f33503fefa06343a039b93039f367379a8c6ab0
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:28:30 +0300

    frontend: refactor: clean code TutorSchedulesScreen

--- frontend/src/screens/ScheduleScreen/index.js
+++ frontend/src/screens/ScheduleScreen/index.js
@@ -1,39 +1,57 @@
-import { View, Text } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import styles from './styles'
-import { filterAvailableTimes, getAvailableTimes, groupSchedules } from './controller'
-import { FlatList, ScrollView, TouchableOpacity } from 'react-native-gesture-handler'
-import AppointmentModal from '../../components/AppointmentModal'
-import { useFocusEffect } from '@react-navigation/native'
-import Container from '../../components/Container'
-import DropdownComponent from '../../components/Dropdown'
-import Schedules from '../../components/Schedules'
-
-
+import { View, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import styles from "./styles";
+import {
+  filterAvailableTimes,
+  getAvailableTimes,
+  groupSchedules,
+} from "./controller";
+import AppointmentModal from "../../components/AppointmentModal";
+import { useFocusEffect } from "@react-navigation/native";
+import DropdownComponent from "../../components/Dropdown";
+import Schedules from "../../components/Schedules";
 
 export default function ScheduleScreen({ route }) {
-    const [modalIsVisable, setModalIsVisable] = useState(false);
-    const tutor = route.params.tutor
-    let [schedules, setSchedules] = useState([])
-    let [modalData, setModalData] = useState([])
-    const [date, setDate] = useState("");
+  const [modalIsVisable, setModalIsVisable] = useState(false);
+  const tutor = route.params.tutor;
+  let [schedules, setSchedules] = useState([]);
+  let [modalData, setModalData] = useState([]);
+  const [date, setDate] = useState("");
 
-    useFocusEffect(
-        React.useCallback(()=>{
-            getAvailableTimes(tutor.id, setSchedules);
-        }, [])
-    )
-    
-    schedules = filterAvailableTimes(schedules)
-    const groupedSchedules = groupSchedules(schedules);
+  useFocusEffect(
+    React.useCallback(() => {
+      getAvailableTimes(tutor.id, setSchedules);
+    }, [])
+  );
 
-    return (
-        <>
-            <Text style={styles.title}>{tutor.fname} {tutor.lname}'s Schedule</Text>
-            <DropdownComponent date={date} setDate={setDate} groupedSchedules={groupedSchedules}/>
-            <Schedules groupedSchedules={groupedSchedules} date={date} setModalIsVisable={setModalIsVisable} setModalData={setModalData}/>
-            <AppointmentModal modalIsVisable={modalIsVisable} setModalIsVisable={setModalIsVisable} time={modalData[0]} date={modalData[1]} tutor={tutor}
-                schedules={schedules} setSchedules={setSchedules}/>
-        </>
-    )
+  schedules = filterAvailableTimes(schedules);
+  const groupedSchedules = groupSchedules(schedules);
+
+  return (
+    <>
+      <Text style={styles.title}>
+        {tutor.fname} {tutor.lname}'s Schedule
+      </Text>
+      <DropdownComponent
+        date={date}
+        setDate={setDate}
+        groupedSchedules={groupedSchedules}
+      />
+      <Schedules
+        groupedSchedules={groupedSchedules}
+        date={date}
+        setModalIsVisable={setModalIsVisable}
+        setModalData={setModalData}
+      />
+      <AppointmentModal
+        modalIsVisable={modalIsVisable}
+        setModalIsVisable={setModalIsVisable}
+        time={modalData[0]}
+        date={modalData[1]}
+        tutor={tutor}
+        schedules={schedules}
+        setSchedules={setSchedules}
+      />
+    </>
+  );
 }
--- frontend/src/screens/ScheduleScreen/styles.js
+++ frontend/src/screens/ScheduleScreen/styles.js
@@ -1,40 +1,37 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+    lineHeight: 29,
+    marginVertical: 25,
+    alignSelf: "center",
+  },
 
-    title: {
-        fontSize: 24,
-        fontWeight: "700",
-        lineHeight: 29,
-        marginVertical: 25,
-        alignSelf: "center"
-    },
-    
-    timesContainer:{
-        flex: 1,
-        justifyContent: "space-between",
-        margin: 25,
-    },
+  timesContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+    margin: 25,
+  },
 
+  timeContainer: {
+    backgroundColor: "white",
+    borderRadius: 15,
+    elevation: 5,
+    justifyContent: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    elevation: 10,
+  },
 
-    timeContainer: {
-        backgroundColor: "white",
-        borderRadius: 15,
-        elevation: 5,
-        justifyContent: "center",
-        paddingVertical: 10,
-        paddingHorizontal: 25,
-        elevation: 10
-    },
-
-    time: {
-        textAlign: "center",
-        textAlignVertical: "center",
-        fontSize: 18,
-        fontWeight: "500",
-        lineHeight: 22,
-        textAlign: "center",
-        letterSpacing: 2
-    },
-
-})
+  time: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 18,
+    fontWeight: "500",
+    lineHeight: 22,
+    textAlign: "center",
+    letterSpacing: 2,
+  },
+});
