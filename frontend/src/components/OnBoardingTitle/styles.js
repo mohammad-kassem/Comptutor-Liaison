69446a91007b51f3716b234587fa054cbff30c4f
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:40:46 +0300

    frontend: refactor: clean code OnBoardingTitle

--- frontend/src/components/OnBoardingTitle/styles.js
+++ frontend/src/components/OnBoardingTitle/styles.js
@@ -1,10 +1,10 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    title: {
-        fontSize: 24,
-        fontWeight: "700",
-        lineHeight: 29,
-        alignSelf: "center",
-    },
-})
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+    lineHeight: 29,
+    alignSelf: "center",
+  },
+});
