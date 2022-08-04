24152173da90f7847c74e40246680648d46f3288
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:26:59 +0300

    frontend: refactor: clean code UpdateInfoScreen

--- frontend/src/screens/UpdateInfoScreen/index.js
+++ frontend/src/screens/UpdateInfoScreen/index.js
@@ -1,36 +1,54 @@
-import { View, Text, KeyboardAvoidingView, ScrollView } from 'react-native'
-import React, { useState } from 'react'
-import styles from './styles'
-import OnBoardingTitle from '../../components/OnBoardingTitle'
-import OnBoardingPrompt from '../../components/OnBoardingPrompt'
-import { TextInput } from 'react-native-gesture-handler'
-import { useUser } from '../../Context/User'
-import FullWidthButton from '../../components/FullWidthButton'
-import { addInfo } from './controller'
-import Container from '../../components/Container'
-import InfoFields from '../../components/InfoFields'
-import { useUserOnBoarding } from '../../Context/UserOnBoarding'
+import { View, Text, KeyboardAvoidingView, ScrollView } from "react-native";
+import React, { useState } from "react";
+import styles from "./styles";
+import OnBoardingTitle from "../../components/OnBoardingTitle";
+import OnBoardingPrompt from "../../components/OnBoardingPrompt";
+import { useUser } from "../../Context/User";
+import FullWidthButton from "../../components/FullWidthButton";
+import { addInfo } from "./controller";
+import Container from "../../components/Container";
+import InfoFields from "../../components/InfoFields";
+import { useUserOnBoarding } from "../../Context/UserOnBoarding";
 
 export default function UpdateInfoScreen() {
-    const [rate, setRate] = useState()
-    const [years, setYears] = useState()
-    const [about, setAbout] = useState()
-    const {setUser} = useUser()
-	const {userOnBoarding, setUserOnBoarding} = useUserOnBoarding()
+  const [rate, setRate] = useState();
+  const [years, setYears] = useState();
+  const [about, setAbout] = useState();
+  const { setUser } = useUser();
+  const { userOnBoarding, setUserOnBoarding } = useUserOnBoarding();
 
-	return (
-		<KeyboardAvoidingView keyboardVerticalOffset={-250} behavior="padding">
-			<ScrollView>
-				<Container>
-					<OnBoardingTitle/>
-					<OnBoardingPrompt message="Complete your info"/>
-					<Text style={styles.sectionPrompt}>Fill your profile</Text>
-					<InfoFields setRate={setRate} setYears={setYears} setAbout={setAbout} user={userOnBoarding}/>
-					<View style={styles.buttonsContainer}>
-						<FullWidthButton text="Next" onHandlePress={()=>addInfo({"years": parseInt(years), "rate": parseInt(rate),"about": about}, userOnBoarding, setUserOnBoarding, setUser)}/>
-					</View>
-				</Container>
-			</ScrollView>
-		</KeyboardAvoidingView>
-	)
+  return (
+    <KeyboardAvoidingView keyboardVerticalOffset={-250} behavior="padding">
+      <ScrollView>
+        <Container>
+          <OnBoardingTitle />
+          <OnBoardingPrompt message="Complete your info" />
+          <Text style={styles.sectionPrompt}>Fill your profile</Text>
+          <InfoFields
+            setRate={setRate}
+            setYears={setYears}
+            setAbout={setAbout}
+            user={userOnBoarding}
+          />
+          <View style={styles.buttonsContainer}>
+            <FullWidthButton
+              text="Next"
+              onHandlePress={() =>
+                addInfo(
+                  {
+                    years: parseInt(years),
+                    rate: parseInt(rate),
+                    about: about,
+                  },
+                  userOnBoarding,
+                  setUserOnBoarding,
+                  setUser
+                )
+              }
+            />
+          </View>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
--- frontend/src/screens/UpdateInfoScreen/styles.js
+++ frontend/src/screens/UpdateInfoScreen/styles.js
@@ -1,43 +1,43 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    sectionPrompt: {
-        fontSize: 20,
-        fontWeight: "500",
-        marginVertical: 25
-    },
+  sectionPrompt: {
+    fontSize: 20,
+    fontWeight: "500",
+    marginVertical: 25,
+  },
 
-    fieldTitle: {
-        fontSize: 16,
-        marginBottom: 5,
-        color: "#919090",
-        fontWeight: "500"
-    },
+  fieldTitle: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: "#919090",
+    fontWeight: "500",
+  },
 
-    field: {
-        marginBottom: 20,
-        paddingBottom: 5,
-        textAlignVertical: 'bottom',
-        alignItems:"flex-start",
-        fontSize: 18,
-        borderBottomWidth: 1,
-        borderColor: "#919090"
-    },
+  field: {
+    marginBottom: 20,
+    paddingBottom: 5,
+    textAlignVertical: "bottom",
+    alignItems: "flex-start",
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderColor: "#919090",
+  },
 
-    about: {
-        marginBottom: 20,
-        paddingBottom: 5,
-        textAlignVertical: 'top',
-        alignItems:"flex-start",
-        fontSize: 18,
-        height: 60,
-        borderBottomWidth: 1,
-        borderColor: "#919090"
-    },
+  about: {
+    marginBottom: 20,
+    paddingBottom: 5,
+    textAlignVertical: "top",
+    alignItems: "flex-start",
+    fontSize: 18,
+    height: 60,
+    borderBottomWidth: 1,
+    borderColor: "#919090",
+  },
 
-    buttonsContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginTop: "55%",
-    },
-})
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: "55%",
+  },
+});
