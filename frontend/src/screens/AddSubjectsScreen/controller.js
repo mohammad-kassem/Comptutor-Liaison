e61f4347f0d4e2bd3f8200e4fffdd6b6328c8f77
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:34:28 +0300

    frontend: refactor: clean code AddSubjectsScreen

--- frontend/src/screens/AddSubjectsScreen/controller.js
+++ frontend/src/screens/AddSubjectsScreen/controller.js
@@ -1,76 +1,98 @@
-import { View, Text, ToastAndroid } from 'react-native'
-import React from 'react'
-import { getToken } from '../../components/utility/Token';
-import axios from 'axios';
+import { View, Text, ToastAndroid } from "react-native";
+import React from "react";
+import { getToken } from "../../components/utility/Token";
+import axios from "axios";
 import { localHostV1 } from "../../contsants/constants";
 
-
 export async function getSubjects(setSubjects) {
-    const token = await getToken()
-    axios({
-        method: "get",
-        url: `${localHostV1}/subject`,
-        headers: {
-          "Content-type": "application/json",
-          "Authorization": `Bearer ${token}`
-        },
-      })
-    .then(function(response){
-        setSubjects(response.data.subjects)
+  const token = await getToken();
+  axios({
+    method: "get",
+    url: `${localHostV1}/subject`,
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  })
+    .then(function (response) {
+      setSubjects(response.data.subjects);
     })
-    .catch(function(error){
-        let message = Object.values(error.response.data);
-        ToastAndroid.show(message[0][0], ToastAndroid.SHORT)
-    })
-}
-
-export function selectedSubject(id, subject, selectedSubjects, setSelectedSubjects) {
-    const selectedSubject = {"id": id, "subject": subject}
-    setSelectedSubjects([...selectedSubjects, selectedSubject])
-}
-
-export function deselectedSubject(id, subject, selectedSubjects, setSelectedSubjects) {
-    setSelectedSubjects(selectedSubjects.filter((selectedSubject)=>(
-            selectedSubject.id !== id
-        ))
-    )
-}
-
-export function handleSelect(id, subject, selectedSubjects, setSelectedSubjects){
-    selectedSubjects.some((selectedSubject) => selectedSubject.id === id) ? deselectedSubject(id, subject, selectedSubjects, setSelectedSubjects)
-    : selectedSubject(id, subject, selectedSubjects, setSelectedSubjects)
-}
-
-export function isSelected(id, selectedSubjects){
-    let found = false;
-    selectedSubjects.forEach((selectedSubject) => {
-        if (selectedSubject.id === id) found = true
+    .catch(function (error) {
+      let message = Object.values(error.response.data);
+      ToastAndroid.show(message[0][0], ToastAndroid.SHORT);
     });
-    return found
 }
 
-export async function addSelectedSubjects(selectedSubjects, navigation, userOnBoarding, setUserOnBoarding, setUser){
-    const token = await getToken()
-    axios({
-        method: "post",
-        url: `${localHostV1}/subject/add`,
-        headers: {
-            "Content-type": "application/json",
-            "Authorization": `Bearer ${token}`
-        },
-        data: JSON.stringify({"subjects": selectedSubjects}) 
-        })
-    .then(function(response){
-        const newSubjects = response.data.subjects
-        userOnBoarding = {...userOnBoarding, "subjects": newSubjects}
-        if (userOnBoarding.role_id === 1) setUser(userOnBoarding) 
-        else 
-            {navigation.navigate("OnBoardingStackTutor", { screen: "AddDegreeScreen"}); 
-            setUserOnBoarding(userOnBoarding)
-        }
+export function selectedSubject(
+  id,
+  subject,
+  selectedSubjects,
+  setSelectedSubjects
+) {
+  const selectedSubject = { id: id, subject: subject };
+  setSelectedSubjects([...selectedSubjects, selectedSubject]);
+}
+
+export function deselectedSubject(
+  id,
+  subject,
+  selectedSubjects,
+  setSelectedSubjects
+) {
+  setSelectedSubjects(
+    selectedSubjects.filter((selectedSubject) => selectedSubject.id !== id)
+  );
+}
+
+export function handleSelect(
+  id,
+  subject,
+  selectedSubjects,
+  setSelectedSubjects
+) {
+  selectedSubjects.some((selectedSubject) => selectedSubject.id === id)
+    ? deselectedSubject(id, subject, selectedSubjects, setSelectedSubjects)
+    : selectedSubject(id, subject, selectedSubjects, setSelectedSubjects);
+}
+
+export function isSelected(id, selectedSubjects) {
+  let found = false;
+  selectedSubjects.forEach((selectedSubject) => {
+    if (selectedSubject.id === id) found = true;
+  });
+  return found;
+}
+
+export async function addSelectedSubjects(
+  selectedSubjects,
+  navigation,
+  userOnBoarding,
+  setUserOnBoarding,
+  setUser
+) {
+  const token = await getToken();
+  axios({
+    method: "post",
+    url: `${localHostV1}/subject/add`,
+    headers: {
+      "Content-type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    data: JSON.stringify({ subjects: selectedSubjects }),
+  })
+    .then(function (response) {
+      const newSubjects = response.data.subjects;
+      userOnBoarding = { ...userOnBoarding, subjects: newSubjects };
+      if (userOnBoarding.role_id === 1) setUser(userOnBoarding);
+      else {
+        navigation.navigate("OnBoardingStackTutor", {
+          screen: "AddDegreeScreen",
+        });
+        setUserOnBoarding(userOnBoarding);
+      }
     })
-    .catch(function(error){
-        let message = Object.values(error.response.data);
-        ToastAndroid.show(message[0][0], ToastAndroid.SHORT)
-    })
+    .catch(function (error) {
+      let message = Object.values(error.response.data);
+      ToastAndroid.show(message[0][0], ToastAndroid.SHORT);
+    });
 }
--- frontend/src/screens/AddSubjectsScreen/index.js
+++ frontend/src/screens/AddSubjectsScreen/index.js
@@ -1,41 +1,57 @@
-import { View, Text, FlatList, Image, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import styles from './styles'
-import OnBoardingTitle from '../../components/OnBoardingTitle'
-import OnBoardingPrompt from '../../components/OnBoardingPrompt'
-import { addSelectedSubjects, getSubjects, handleSelect, isSelected } from './controller'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import FullWidthButton from '../../components/FullWidthButton'
-import { useNavigation } from '@react-navigation/native'
-import AbsolutePositionButtonContainer from '../../components/AbsolutePositionButtonContainer'
-import { useUser } from '../../Context/User'
-import Container from '../../components/Container'
-import Subjcets from '../../components/Subjects'
-import { useUserOnBoarding } from '../../Context/UserOnBoarding'
+import { View, Text, FlatList, Image, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import OnBoardingTitle from "../../components/OnBoardingTitle";
+import OnBoardingPrompt from "../../components/OnBoardingPrompt";
+import {
+  addSelectedSubjects,
+  getSubjects
+} from "./controller";
+import FullWidthButton from "../../components/FullWidthButton";
+import { useNavigation } from "@react-navigation/native";
+import AbsolutePositionButtonContainer from "../../components/AbsolutePositionButtonContainer";
+import { useUser } from "../../Context/User";
+import Container from "../../components/Container";
+import Subjcets from "../../components/Subjects";
+import { useUserOnBoarding } from "../../Context/UserOnBoarding";
 
-export default function AddSubjectsScreen(){
-    const [subjects, setSubjects] = useState([])
-    const [selectedSubjects, setSelectedSubjects] = useState([])
-    const {setUser} = useUser()
-    const navigation = useNavigation()
-    const {userOnBoarding, setUserOnBoarding} = useUserOnBoarding()
+export default function AddSubjectsScreen() {
+  const [subjects, setSubjects] = useState([]);
+  const [selectedSubjects, setSelectedSubjects] = useState([]);
+  const { setUser } = useUser();
+  const navigation = useNavigation();
+  const { userOnBoarding, setUserOnBoarding } = useUserOnBoarding();
 
-    useEffect(function(){
-        getSubjects(setSubjects);
-      }, []);
+  useEffect(function () {
+    getSubjects(setSubjects);
+  }, []);
 
-    return (
-            <>
-            <Container>
-            <OnBoardingTitle/>
-            <OnBoardingPrompt message="Pick your subjects"/>
-            <Subjcets subjects={subjects} selectedSubjects={selectedSubjects} setSelectedSubjects={setSelectedSubjects}/>
-            </Container>
-            <>
-            <AbsolutePositionButtonContainer>
-                <FullWidthButton text="Next" onHandlePress={()=>addSelectedSubjects(selectedSubjects, navigation, userOnBoarding, setUserOnBoarding, setUser)}/>
-            </AbsolutePositionButtonContainer>
-            </>
-            </>
-    )
+  return (
+    <>
+      <Container>
+        <OnBoardingTitle />
+        <OnBoardingPrompt message="Pick your subjects" />
+        <Subjcets
+          subjects={subjects}
+          selectedSubjects={selectedSubjects}
+          setSelectedSubjects={setSelectedSubjects}
+        />
+      </Container>
+      <>
+        <AbsolutePositionButtonContainer>
+          <FullWidthButton
+            text="Next"
+            onHandlePress={() =>
+              addSelectedSubjects(
+                selectedSubjects,
+                navigation,
+                userOnBoarding,
+                setUserOnBoarding,
+                setUser
+              )
+            }
+          />
+        </AbsolutePositionButtonContainer>
+      </>
+    </>
+  );
 }
