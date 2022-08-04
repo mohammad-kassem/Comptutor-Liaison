b78c03156971d22abed6dc76b590939ae6f46c4c
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:43:40 +0300

    frontend: refactor: clean code FullWidthButton

--- frontend/src/components/FullWidthButton/index.js
+++ frontend/src/components/FullWidthButton/index.js
@@ -1,16 +1,31 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { TouchableOpacity } from 'react-native-gesture-handler'
-import styles from './styles'
-import Icon from 'react-native-vector-icons/FontAwesome'
+import { View, Text } from "react-native";
+import React from "react";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import styles from "./styles";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 export default function FullWidthButton({ text, onHandlePress, icon, color }) {
-    return (
-        <TouchableOpacity containerStyle={color === "green" ? [styles.fullWidthButton, styles.green] : styles.fullWidthButton} onPress={onHandlePress}>
-            <View style={styles.buttonContent}>
-            {icon && <Icon name={icon} style={styles.icon}/>} 
-            <Text style={color === "green" ? [styles.fullWidthButtonText, styles.white] : styles.fullWidthButtonText}>{text}</Text>
-            </View>
-        </TouchableOpacity>
-    )
+  return (
+    <TouchableOpacity
+      containerStyle={
+        color === "green"
+          ? [styles.fullWidthButton, styles.green]
+          : styles.fullWidthButton
+      }
+      onPress={onHandlePress}
+    >
+      <View style={styles.buttonContent}>
+        {icon && <Icon name={icon} style={styles.icon} />}
+        <Text
+          style={
+            color === "green"
+              ? [styles.fullWidthButtonText, styles.white]
+              : styles.fullWidthButtonText
+          }
+        >
+          {text}
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
 }
--- frontend/src/components/FullWidthButton/styles.js
+++ frontend/src/components/FullWidthButton/styles.js
@@ -1,37 +1,37 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    fullWidthButton:{
-        backgroundColor: "#C7E9F1",
-        justifyContent: "center",
-        alignItems: "center",
-        padding: 15,
-        borderRadius: 20,
-        width: "100%",
-        elevation: 5,
-    },
+  fullWidthButton: {
+    backgroundColor: "#C7E9F1",
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 15,
+    borderRadius: 20,
+    width: "100%",
+    elevation: 5,
+  },
 
-    buttonContent: {
-        flexDirection: "row",
-    },
+  buttonContent: {
+    flexDirection: "row",
+  },
 
-    fullWidthButtonText:{
-        fontSize: 18,
-        fontWeight: '500',
-        color: "#1877F2",
-        marginLeft: 5
-    },
+  fullWidthButtonText: {
+    fontSize: 18,
+    fontWeight: "500",
+    color: "#1877F2",
+    marginLeft: 5,
+  },
 
-    green: {
-        backgroundColor: "#A8D057"
-    },
+  green: {
+    backgroundColor: "#A8D057",
+  },
 
-    white: {
-        color: "white"
-    },
+  white: {
+    color: "white",
+  },
 
-    icon:{
-        fontSize: 24,
-        color: "#1877F2"
-    }
-})
+  icon: {
+    fontSize: 24,
+    color: "#1877F2",
+  },
+});
