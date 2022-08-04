f35ad2763dd32a9b19c3811950030c99126d4e79
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:31:47 +0300

    frontend: refactor: clean code EditDegreeScreen

--- frontend/src/screens/EditDegreeScreen/index.js
+++ frontend/src/screens/EditDegreeScreen/index.js
@@ -1,46 +1,61 @@
-import { View, Text, KeyboardAvoidingView, ScrollView, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import OnBoardingTitle from '../../components/OnBoardingTitle'
-import OnBoardingPrompt from '../../components/OnBoardingPrompt'
-import { TextInput } from 'react-native-gesture-handler'
-import FullWidthButton from '../../components/FullWidthButton'
-import { addDegree, getUniversities } from './controller'
-import { useNavigation } from '@react-navigation/native'
-import Container from '../../components/Container'
-import SaveCancelButtons from '../../components/SaveCancelButtons'
-import styles from './styles'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  ScrollView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useEffect, useState } from "react";
+import FullWidthButton from "../../components/FullWidthButton";
+import { addDegree, getUniversities } from "./controller";
+import { useNavigation } from "@react-navigation/native";
+import Container from "../../components/Container";
+import styles from "./styles";
+import { useUser } from "../../Context/User";
+import DegreeFields from "../../components/DegreeFields";
+import { useDegrees } from "../../Context/Degrees";
 
-import { useUser } from '../../Context/User'
-import { Dropdown } from 'react-native-element-dropdown'
-import DegreeFields from '../../components/DegreeFields'
-import { useDegrees } from '../../Context/Degrees'
+export default function EditDegreeScreen({ route }) {
+  const { user, setUser } = useUser();
+  const { setDegrees } = useDegrees();
+  const [university, setUniversity] = useState("");
+  const [degree, setDegree] = useState("");
+  const [universities, setUniversities] = useState([]);
+  const navigation = useNavigation();
 
-export default function EditDegreeScreen( {route} ) {
-	const {user, setUser} = useUser()
-	const {setDegrees} = useDegrees()
-    const [university, setUniversity] = useState("")
-    const [degree, setDegree] = useState("")
-	const [universities, setUniversities] = useState([])
-    const navigation = useNavigation()
+  useEffect(function () {
+    getUniversities(setUniversities);
+  }, []);
 
-	useEffect(function(){
-        getUniversities(setUniversities);
-      }, []);
-
-	  return (
-		<KeyboardAvoidingView keyboardVerticalOffset={-250} behavior="padding">
-			<ScrollView>
-					<View style={styles.titleContainer}>
-						<Text style={styles.title}>Add a Degree</Text>	
-					</View>
-					<Container>
-					<DegreeFields setDegree={setDegree} setUniversity={setUniversity} university={university} universities={universities}/>
-					<View style={styles.buttonsContainer}>
-						<FullWidthButton text="Save Changes" onHandlePress={()=>addDegree({university, degree}, user, setUser, navigation, setDegrees)}/>
-					</View>
-					</Container>
-			</ScrollView>
-		</KeyboardAvoidingView>
-	)
+  return (
+    <KeyboardAvoidingView keyboardVerticalOffset={-250} behavior="padding">
+      <ScrollView>
+        <View style={styles.titleContainer}>
+          <Text style={styles.title}>Add a Degree</Text>
+        </View>
+        <Container>
+          <DegreeFields
+            setDegree={setDegree}
+            setUniversity={setUniversity}
+            university={university}
+            universities={universities}
+          />
+          <View style={styles.buttonsContainer}>
+            <FullWidthButton
+              text="Save Changes"
+              onHandlePress={() =>
+                addDegree(
+                  { university, degree },
+                  user,
+                  setUser,
+                  navigation,
+                  setDegrees
+                )
+              }
+            />
+          </View>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
--- frontend/src/screens/EditDegreeScreen/styles.js
+++ frontend/src/screens/EditDegreeScreen/styles.js
@@ -1,65 +1,65 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    titleContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "flex-end",
-        margin: 25,
-        marginBottom: 0
+  titleContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "flex-end",
+    margin: 25,
+    marginBottom: 0,
+  },
+
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+    lineHeight: 29,
+    alignSelf: "center",
+  },
+
+  dropdown: {
+    marginTop: 10,
+    marginBottom: 20,
+    height: 62,
+    backgroundColor: "white",
+    borderRadius: 12,
+    padding: 12,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
     },
 
-    title: {
-        fontSize: 24,
-        fontWeight: "700",
-        lineHeight: 29,
-        alignSelf: "center",
-    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.4,
+    elevation: 10,
+  },
 
-    dropdown: {
-        marginTop: 10,
-        marginBottom: 20,
-        height: 62,
-        backgroundColor: 'white',
-        borderRadius: 12,
-        padding: 12,
-        shadowColor: '#000',
-        shadowOffset: {
-          width: 0,
-          height: 1,
-        },
-  
-        shadowOpacity: 0.2,
-        shadowRadius: 1.4,
-        elevation: 10,
-    },
+  selectedTextStyle: {
+    fontSize: 18,
+    height: 50,
+    textAlignVertical: "center",
+  },
 
-    selectedTextStyle: {
-        fontSize: 18,
-        height: 50,
-        textAlignVertical: "center"
-    },
+  fieldTitle: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: "#919090",
+    fontWeight: "500",
+  },
 
-    fieldTitle: {
-        fontSize: 16,
-        marginBottom: 5,
-        color: "#919090",
-        fontWeight: "500"
-    },
+  field: {
+    marginBottom: 20,
+    paddingBottom: 5,
+    textAlignVertical: "bottom",
+    alignItems: "flex-start",
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderColor: "#919090",
+  },
 
-    field: {
-        marginBottom: 20,
-        paddingBottom: 5,
-        textAlignVertical: 'bottom',
-        alignItems:"flex-start",
-        fontSize: 18,
-        borderBottomWidth: 1,
-        borderColor: "#919090"
-    },
-
-    buttonsContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginTop: "93%",
-    },
-})
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: "93%",
+  },
+});
