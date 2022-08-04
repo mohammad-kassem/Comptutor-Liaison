d637e013e31fc39ac9aaf74c3cb754f62a6cb4c0
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:34:57 +0300

    frontend: refactor: clean code AddScheduleScreen

--- frontend/src/screens/AddScheduleScreen/styles.js
+++ frontend/src/screens/AddScheduleScreen/styles.js
@@ -1,38 +1,38 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    title: {
-        fontSize: 24,
-        fontWeight: "700",
-        lineHeight: 29,
-        alignSelf: "center",
-        marginBottom: 25,
-    },
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+    lineHeight: 29,
+    alignSelf: "center",
+    marginBottom: 25,
+  },
 
-    fieldTitle: {
-        fontSize: 16,
-        marginBottom: 5,
-        color: "#919090",
-        fontWeight: "500"
-    },
+  fieldTitle: {
+    fontSize: 16,
+    marginBottom: 5,
+    color: "#919090",
+    fontWeight: "500",
+  },
 
-    field: {
-        marginBottom: 20,
-        paddingBottom: 5,
-        textAlignVertical: 'bottom',
-        alignItems:"flex-start",
-        fontSize: 18,
-        borderBottomWidth: 1,
-        borderColor: "#919090"
-    },
+  field: {
+    marginBottom: 20,
+    paddingBottom: 5,
+    textAlignVertical: "bottom",
+    alignItems: "flex-start",
+    fontSize: 18,
+    borderBottomWidth: 1,
+    borderColor: "#919090",
+  },
 
-    fieldText: {
-        fontSize: 20
-    },
+  fieldText: {
+    fontSize: 20,
+  },
 
-    buttonsContainer: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginTop: "80%",
-    },
-})
+  buttonsContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: "80%",
+  },
+});
