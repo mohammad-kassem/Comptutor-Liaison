09c85641d057c139aedcebf280791b0f58bc8753
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:39:06 +0300

    frontend: refactor: clean code SearchBar

--- frontend/src/components/SearchBar/index.js
+++ frontend/src/components/SearchBar/index.js
@@ -1,19 +1,23 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import Icon from 'react-native-vector-icons/FontAwesome';
-import styles from './styles'
-import { TextInput } from 'react-native-gesture-handler'
-import { filter } from './controller';
+import { View, Text } from "react-native";
+import React from "react";
+import Icon from "react-native-vector-icons/FontAwesome";
+import styles from "./styles";
+import { TextInput } from "react-native-gesture-handler";
+import { filter } from "./controller";
 
-
-export default function SearchBar({original, setTutors}) {
-
-    return (
-        <View style={styles.searchContainer}>
-        <View style={styles.searchBar}>
-            <Icon name="search" style={styles.searchIcon}/>
-            <TextInput style={styles.searchInput} placeholder="Search" onChangeText={(searchText)=>filter(searchText.toLowerCase(), original, setTutors)}/>
-        </View>
-        </View>
-    )
+export default function SearchBar({ original, setTutors }) {
+  return (
+    <View style={styles.searchContainer}>
+      <View style={styles.searchBar}>
+        <Icon name="search" style={styles.searchIcon} />
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search"
+          onChangeText={(searchText) =>
+            filter(searchText.toLowerCase(), original, setTutors)
+          }
+        />
+      </View>
+    </View>
+  );
 }
--- frontend/src/components/SearchBar/styles.js
+++ frontend/src/components/SearchBar/styles.js
@@ -1,25 +1,25 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    searchBar: {
-        backgroundColor: "#ffff",
-        flexDirection: "row",
-        height: 45,
-        padding: 15,
-        borderRadius: 30,
-        elevation: 10,
-        marginHorizontal: 25,
-    },
+  searchBar: {
+    backgroundColor: "#ffff",
+    flexDirection: "row",
+    height: 45,
+    padding: 15,
+    borderRadius: 30,
+    elevation: 10,
+    marginHorizontal: 25,
+  },
 
-    searchIcon: {
-        color: "#1877F2",
-        fontSize: 24,
-        margin: -7.5,
-        marginRight: 10,
-    },
+  searchIcon: {
+    color: "#1877F2",
+    fontSize: 24,
+    margin: -7.5,
+    marginRight: 10,
+  },
 
-    searchInput: {
-        flex: 1,
-        textAlignVertical: "center",
-    },
-})
+  searchInput: {
+    flex: 1,
+    textAlignVertical: "center",
+  },
+});
