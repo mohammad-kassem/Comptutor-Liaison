2e2f864f64e17ae0a6de4387e1c630344edd8be3
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:27:43 +0300

    frontend: refactor: clean code TutorSchedulesScreen

--- frontend/src/screens/TutorSchedulesScreen/index.js
+++ frontend/src/screens/TutorSchedulesScreen/index.js
@@ -1,37 +1,47 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React, { useEffect, useRef, useState } from 'react'
-import styles from './styles'
-import { FlatList, ScrollView } from 'react-native-gesture-handler'
-import getSchedules, { filterAllTimes } from './controller'
-import { groupSchedules } from '../ScheduleScreen/controller'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
-import { useFocusEffect, useNavigation } from '@react-navigation/native'
-import DropdownComponent from '../../components/Dropdown'
-import Schedules from '../../components/Schedules'
-
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useState } from "react";
+import styles from "./styles";
+import getSchedules, { filterAllTimes } from "./controller";
+import { groupSchedules } from "../ScheduleScreen/controller";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
+import { useFocusEffect, useNavigation } from "@react-navigation/native";
+import DropdownComponent from "../../components/Dropdown";
+import Schedules from "../../components/Schedules";
 
 export default function TutorSchedulesScreen() {
-    let [schedules, setSchedules] = useState([])
-    const [date, setDate] = useState("");
-    const navigation = useNavigation()
+  let [schedules, setSchedules] = useState([]);
+  const [date, setDate] = useState("");
+  const navigation = useNavigation();
 
-    useFocusEffect(
-        React.useCallback(()=>{
-        getSchedules(setSchedules);
-      }, [])
-    )
+  useFocusEffect(
+    React.useCallback(() => {
+      getSchedules(setSchedules);
+    }, [])
+  );
 
-    schedules = filterAllTimes(schedules)
-    const groupedSchedules = groupSchedules(schedules);
+  schedules = filterAllTimes(schedules);
+  const groupedSchedules = groupSchedules(schedules);
 
-    return (
-        <>
-            <Text style={styles.title}>Schedule</Text>
-            <DropdownComponent date={date} setDate={setDate} groupedSchedules={groupedSchedules}/>
-            <Schedules groupedSchedules={groupedSchedules} date={date}/>
-            <TouchableOpacity style={styles.addButton} onPress={()=>{navigation.navigate("HomeStackTutor", { screen: "AddScheduleScreen",  params: { schedules: schedules, setSchedules: setSchedules },}); console.log("hello")}}>
-                <Icon name="plus" style={styles.icon}/>
-            </TouchableOpacity>
-        </>
-    )
+  return (
+    <>
+      <Text style={styles.title}>Schedule</Text>
+      <DropdownComponent
+        date={date}
+        setDate={setDate}
+        groupedSchedules={groupedSchedules}
+      />
+      <Schedules groupedSchedules={groupedSchedules} date={date} />
+      <TouchableOpacity
+        style={styles.addButton}
+        onPress={() => {
+          navigation.navigate("HomeStackTutor", {
+            screen: "AddScheduleScreen",
+            params: { schedules: schedules, setSchedules: setSchedules },
+          });
+        }}
+      >
+        <Icon name="plus" style={styles.icon} />
+      </TouchableOpacity>
+    </>
+  );
 }
--- frontend/src/screens/TutorSchedulesScreen/styles.js
+++ frontend/src/screens/TutorSchedulesScreen/styles.js
@@ -1,54 +1,51 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    title: {
-        fontSize: 24,
-        fontWeight: "700",
-        lineHeight: 29,
-        marginVertical: 25,
-        alignSelf: "center"
-    },
+  title: {
+    fontSize: 24,
+    fontWeight: "700",
+    lineHeight: 29,
+    marginVertical: 25,
+    alignSelf: "center",
+  },
 
-    
-    timesContainer:{
-        flex: 1,
-        justifyContent: "space-between",
-        margin: 25,
-    },
+  timesContainer: {
+    flex: 1,
+    justifyContent: "space-between",
+    margin: 25,
+  },
 
+  timeContainer: {
+    backgroundColor: "white",
+    borderRadius: 15,
+    elevation: 5,
+    justifyContent: "center",
+    paddingVertical: 10,
+    paddingHorizontal: 25,
+    elevation: 10,
+  },
 
-    timeContainer :{
-        backgroundColor: "white",
-        borderRadius: 15,
-        elevation: 5,
-        justifyContent: "center",
-        paddingVertical: 10,
-        paddingHorizontal: 25,
-        elevation: 10
-    },
+  time: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 18,
+    fontWeight: "500",
+    lineHeight: 22,
+    textAlign: "center",
+    letterSpacing: 2,
+  },
 
-    time: {
-        textAlign: "center",
-        textAlignVertical: "center",
-        fontSize: 18,
-        fontWeight: "500",
-        lineHeight: 22,
-        textAlign: "center",
-        letterSpacing: 2
-    },
+  addButton: {
+    backgroundColor: "#C7E9F1",
+    position: "absolute",
+    bottom: 30,
+    right: 25,
+    padding: 10,
+    borderRadius: 15,
+  },
 
-    addButton: {
-        backgroundColor: "#C7E9F1",
-        position: "absolute",
-        bottom: 30,
-        right: 25,
-        padding: 10,
-        borderRadius: 15,
-    },
-
-    icon: {
-        fontSize: 40, 
-        color: "#1877F2"
-    }
-
-})
+  icon: {
+    fontSize: 40,
+    color: "#1877F2",
+  },
+});
