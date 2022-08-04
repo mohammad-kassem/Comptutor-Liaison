016f4a74b1497c521a8c618de4bd9f5034df802e
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 22:35:24 +0300

    frontend: refactor: clean code AddDegreeScreen

--- frontend/src/screens/AddDegreeScreen/index.js
+++ frontend/src/screens/AddDegreeScreen/index.js
@@ -1,42 +1,56 @@
-import { View, Text, KeyboardAvoidingView, ScrollView } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import styles from './styles'
-import OnBoardingTitle from '../../components/OnBoardingTitle'
-import OnBoardingPrompt from '../../components/OnBoardingPrompt'
-import { TextInput } from 'react-native-gesture-handler'
-import FullWidthButton from '../../components/FullWidthButton'
-import { addDegree, getUniversities } from './controller'
-import { useNavigation } from '@react-navigation/native'
-import Container from '../../components/Container'
-import { Dropdown } from 'react-native-element-dropdown'
-import DegreeFields from '../../components/DegreeFields'
-import { useUserOnBoarding } from '../../Context/UserOnBoarding'
+import { View, Text, KeyboardAvoidingView, ScrollView } from "react-native";
+import React, { useEffect, useState } from "react";
+import styles from "./styles";
+import OnBoardingTitle from "../../components/OnBoardingTitle";
+import OnBoardingPrompt from "../../components/OnBoardingPrompt";
+import FullWidthButton from "../../components/FullWidthButton";
+import { addDegree, getUniversities } from "./controller";
+import { useNavigation } from "@react-navigation/native";
+import Container from "../../components/Container";
+import DegreeFields from "../../components/DegreeFields";
+import { useUserOnBoarding } from "../../Context/UserOnBoarding";
 
 export default function AddDegreeScreen() {
-    const [university, setUniversity] = useState("")
-    const [degree, setDegree] = useState("")
-	const [universities, setUniversities] = useState([])
-    const navigation = useNavigation()
-	const {userOnBoarding, setUserOnBoarding} = useUserOnBoarding()
+  const [university, setUniversity] = useState("");
+  const [degree, setDegree] = useState("");
+  const [universities, setUniversities] = useState([]);
+  const navigation = useNavigation();
+  const { userOnBoarding, setUserOnBoarding } = useUserOnBoarding();
 
-	useEffect(function(){
-        getUniversities(setUniversities);
-      }, []);
+  useEffect(function () {
+    getUniversities(setUniversities);
+  }, []);
 
-
-	return (
-		<KeyboardAvoidingView keyboardVerticalOffset={-250} behavior="padding">
-			<ScrollView>
-				<Container>
-					<OnBoardingTitle/>
-					<OnBoardingPrompt message="Complete your info"/>
-					<Text style={styles.sectionPrompt}>Add your degree to improve your opportunities</Text>
-					<DegreeFields setDegree={setDegree} setUniversity={setUniversity} university={university} universities={universities}/>
-					<View style={styles.buttonsContainer}>
-						<FullWidthButton text="Next" onHandlePress={()=>addDegree({university, degree}, userOnBoarding, setUserOnBoarding, navigation)}/> 
-					</View>
-				</Container>
-			</ScrollView>
-		</KeyboardAvoidingView>
-	)
+  return (
+    <KeyboardAvoidingView keyboardVerticalOffset={-250} behavior="padding">
+      <ScrollView>
+        <Container>
+          <OnBoardingTitle />
+          <OnBoardingPrompt message="Complete your info" />
+          <Text style={styles.sectionPrompt}>
+            Add your degree to improve your opportunities
+          </Text>
+          <DegreeFields
+            setDegree={setDegree}
+            setUniversity={setUniversity}
+            university={university}
+            universities={universities}
+          />
+          <View style={styles.buttonsContainer}>
+            <FullWidthButton
+              text="Next"
+              onHandlePress={() =>
+                addDegree(
+                  { university, degree },
+                  userOnBoarding,
+                  setUserOnBoarding,
+                  navigation
+                )
+              }
+            />
+          </View>
+        </Container>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
 }
