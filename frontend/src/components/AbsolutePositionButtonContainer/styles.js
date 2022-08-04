ec5a918fca853056d1c1ba138d15bbed573fa78c
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-04 23:24:11 +0300

    frontend: refactor: clean code AbsolutePositionButtonContainer styling

--- frontend/src/components/AbsolutePositionButtonContainer/styles.js
+++ frontend/src/components/AbsolutePositionButtonContainer/styles.js
@@ -1,10 +1,10 @@
 import { StyleSheet } from "react-native";
 
 export default StyleSheet.create({
-    buttonContainer: {
-        position: "absolute",
-        right: 25,
-        left:25,
-        bottom: 30,
-    },
+	buttonContainer: {
+		position: "absolute",
+		right: 25,
+		left:25,
+		bottom: 30,
+	},
 })
