be287d0062a0f24a52a2237da410fd95c0e0515d
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:34:10 +0300

    backend: refactor: clean the code user subject migration

--- backend/database/migrations/2022_06_27_124625_create_user_subject_table.php
+++ backend/database/migrations/2022_06_27_124625_create_user_subject_table.php
@@ -4,17 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUserSubjectTable extends Migration{
-    public function up(){
-        Schema::create('user_subject', function (Blueprint $table) {
-            $table->integer('user_id');
-            $table->integer('subject_id');
-            $table->primary(array('user_id', 'subject_id'));
-            $table->timestamps();
-        });
-    }
+class CreateUserSubjectTable extends Migration
+{
+  public function up()
+  {
+    Schema::create('user_subject', function (Blueprint $table) {
+      $table->integer('user_id');
+      $table->integer('subject_id');
+      $table->primary(array('user_id', 'subject_id'));
+      $table->timestamps();
+    });
+  }
 
-    public function down(){
-        Schema::dropIfExists('user_subject');
-    }
+  public function down()
+  {
+    Schema::dropIfExists('user_subject');
+  }
 }
