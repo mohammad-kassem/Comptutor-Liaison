9ab2556e135ff2bcf1fb570f56e2037aa844a8aa
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:36:23 +0300

    backend: refactor: clean the code Degree model

--- backend/app/Models/Degree.php
+++ backend/app/Models/Degree.php
@@ -5,16 +5,18 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Degree extends Model{
-    use HasFactory;
+class Degree extends Model
+{
+  use HasFactory;
 
-    protected $fillable = [
-        'university',
-        'degree',
-        'tutor_id'
-    ];
+  protected $fillable = [
+    'university',
+    'degree',
+    'tutor_id'
+  ];
 
-    public function user(){
-        return $this->belongsTo(User::class, 'tutor_id');
-    }
+  public function user()
+  {
+    return $this->belongsTo(User::class, 'tutor_id');
+  }
 }
