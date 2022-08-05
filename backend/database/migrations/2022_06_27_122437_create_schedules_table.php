c0f3f4d940a76d9c9243b96e824be7172f292b0d
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:34:30 +0300

    backend: refactor: clean the code schedules migration

--- backend/database/migrations/2022_06_27_122437_create_schedules_table.php
+++ backend/database/migrations/2022_06_27_122437_create_schedules_table.php
@@ -4,19 +4,22 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSchedulesTable extends Migration{
-    public function up(){
-        Schema::create('schedules', function (Blueprint $table) {
-            $table->id();
-            $table->date('date');
-            $table->time('start_time');
-            $table->time('end_time');
-            $table->integer('tutor_id');
-            $table->timestamps();
-        });
-    }
+class CreateSchedulesTable extends Migration
+{
+  public function up()
+  {
+    Schema::create('schedules', function (Blueprint $table) {
+      $table->id();
+      $table->date('date');
+      $table->time('start_time');
+      $table->time('end_time');
+      $table->integer('tutor_id');
+      $table->timestamps();
+    });
+  }
 
-    public function down(){
-        Schema::dropIfExists('schedules');
-    }
+  public function down()
+  {
+    Schema::dropIfExists('schedules');
+  }
 }
