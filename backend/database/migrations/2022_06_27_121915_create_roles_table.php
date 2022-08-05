44253c457049a0fae3f843096782a73f2ff0389b
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:34:56 +0300

    backend: refactor: clean the code roles migration

--- backend/database/migrations/2022_06_27_121915_create_roles_table.php
+++ backend/database/migrations/2022_06_27_121915_create_roles_table.php
@@ -4,16 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRolesTable extends Migration{
-    public function up(){
-        Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->string('role');
-            $table->timestamps();
-        });
-    }
+class CreateRolesTable extends Migration
+{
+  public function up()
+  {
+    Schema::create('roles', function (Blueprint $table) {
+      $table->id();
+      $table->string('role');
+      $table->timestamps();
+    });
+  }
 
-    public function down(){
-        Schema::dropIfExists('roles');
-    }
+  public function down()
+  {
+    Schema::dropIfExists('roles');
+  }
 }
