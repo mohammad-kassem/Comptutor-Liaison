ea27c7d83e9b77b427fc8af973b2159475a364b7
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:36:55 +0300

    backend: refactor: clean the code tutor middleware

--- backend/app/Http/Middleware/TutorMiddleware.php
+++ backend/app/Http/Middleware/TutorMiddleware.php
@@ -6,13 +6,15 @@ use Auth;
 use Closure;
 use Illuminate\Http\Request;
 
-class TutorMiddleware{
-    public function handle(Request $request, Closure $next){
-        $user = auth()->user();
-        if($user and $user->role_id == 2 ){
-            return $next($request);
-        }
-
-        return response()->json(['error' => 'Unauthorized'], 403);
+class TutorMiddleware
+{
+  public function handle(Request $request, Closure $next)
+  {
+    $user = auth()->user();
+    if ($user and $user->role_id == 2) {
+      return $next($request);
     }
+
+    return response()->json(['error' => 'Unauthorized'], 403);
+  }
 }
