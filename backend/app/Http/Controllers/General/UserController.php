81cc7af74c5c8318b9e498fadedc8b548101a2ab
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:39:14 +0300

    backend: refactor: clean the code user controller

--- backend/app/Http/Controllers/General/UserController.php
+++ backend/app/Http/Controllers/General/UserController.php
@@ -8,49 +8,52 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class UserController extends Controller{
-    public function setFCM(Request $request){
-        $validator = Validator::make($request->all(), [
-            'FCM_token' => 'required|string',
-        ]);
-        
+class UserController extends Controller
+{
+  public function setFCM(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'FCM_token' => 'required|string',
+    ]);
 
-        if($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-        
-        $user = User::find($request->id);
 
-        $user->update([
-            'FCM_token' => $request->FCM_token,
-        ]);
-        
-
-        return response()->json([
-            'message' => 'FCM token successfully set',
-            'user' => $user,
-        ], 200);
+    if ($validator->fails()) {
+      return response()->json($validator->errors(), 400);
     }
 
-    public function updateImage(Request $request){   
-        $validator = Validator::make($request->all(), [
-            'image' => 'required|string',
-        ]);
-        
+    $user = User::find($request->id);
 
-        if($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
+    $user->update([
+      'FCM_token' => $request->FCM_token,
+    ]);
 
-        $user = auth()->user();
 
-        $user->update([
-            'profile_image' => $request->image
-        ]);
+    return response()->json([
+      'message' => 'FCM token successfully set',
+      'user' => $user,
+    ], 200);
+  }
 
-        return response()->json([
-            'message' => 'Account image successfully updated',
-            'user' => $user,
-        ], 200);
+  public function updateImage(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'image' => 'required|string',
+    ]);
+
+
+    if ($validator->fails()) {
+      return response()->json($validator->errors(), 400);
     }
+
+    $user = auth()->user();
+
+    $user->update([
+      'profile_image' => $request->image
+    ]);
+
+    return response()->json([
+      'message' => 'Account image successfully updated',
+      'user' => $user,
+    ], 200);
+  }
 }
