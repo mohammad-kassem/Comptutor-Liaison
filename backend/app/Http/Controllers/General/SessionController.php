554c27e47a33cd7647df9d9c74a37a55dbfea984
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:39:41 +0300

    backend: refactor: clean the code session controller

--- backend/app/Http/Controllers/General/SessionController.php
+++ backend/app/Http/Controllers/General/SessionController.php
@@ -8,34 +8,35 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 
-class SessionController extends Controller{
-    public function create(Request $request){
-        $validator = Validator::make($request->all(), [
-            'url' => 'required|string',
-        ]);
+class SessionController extends Controller
+{
+  public function create(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'url' => 'required|string',
+    ]);
 
-        if($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        $appointment = Appointment::where('schedule_id', $request->id)->first();
-
-        $appointment->update([
-            'url' => $request->url
-        ]);
-        
-        return response()->json([
-            'message' => 'Session successfully created'
-        ], 200);
-
+    if ($validator->fails()) {
+      return response()->json($validator->errors(), 400);
     }
 
-    public function get($id){
-        $appointment = Appointment::where('schedule_id', $id)->first();
-        
-        return response()->json([
-            'url' => $appointment->url
-        ], 200);
+    $appointment = Appointment::where('schedule_id', $request->id)->first();
 
-    }
+    $appointment->update([
+      'url' => $request->url
+    ]);
+
+    return response()->json([
+      'message' => 'Session successfully created'
+    ], 200);
+  }
+
+  public function get($id)
+  {
+    $appointment = Appointment::where('schedule_id', $id)->first();
+
+    return response()->json([
+      'url' => $appointment->url
+    ], 200);
+  }
 }
