72351b766d33e32a37baf2ba758fcf71e7d345d5
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:39:26 +0300

    backend: refactor: clean the code subject controller

--- backend/app/Http/Controllers/General/SubjectController.php
+++ backend/app/Http/Controllers/General/SubjectController.php
@@ -8,58 +8,62 @@ use App\Models\Subject;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class SubjectController extends Controller{
-    public function addUserSubjects(Request $request){
-        $subjects = $request->subjects;
-        $user = auth()->user();
-        foreach ($subjects as $subject) {
-            $subject = json_decode(json_encode($subject));
-            $added_subject = $user->subjects()->where('id', $subject->id)->first();
+class SubjectController extends Controller
+{
+  public function addUserSubjects(Request $request)
+  {
+    $subjects = $request->subjects;
+    $user = auth()->user();
+    foreach ($subjects as $subject) {
+      $subject = json_decode(json_encode($subject));
+      $added_subject = $user->subjects()->where('id', $subject->id)->first();
 
-            if ($added_subject) return response()->json(['error' => ['This subject has already been entered']], 409);
+      if ($added_subject) return response()->json(['error' => ['This subject has already been entered']], 409);
 
-            $user->subjects()->attach($subject->id);
-        }
-
-        return response()->json([
-            'message' => 'User subjects successfully added',
-            'subjects' => $user->subjects
-        ], 200);
+      $user->subjects()->attach($subject->id);
     }
 
-    
-    public function getUserSubjects(){
-        $user= auth()->user();
+    return response()->json([
+      'message' => 'User subjects successfully added',
+      'subjects' => $user->subjects
+    ], 200);
+  }
 
-        $subjects = $user->subjects()->get();
 
-        return response()->json([
-            'subjects' => $subjects,
-        ], 200);
+  public function getUserSubjects()
+  {
+    $user = auth()->user();
+
+    $subjects = $user->subjects()->get();
+
+    return response()->json([
+      'subjects' => $subjects,
+    ], 200);
+  }
+
+  public function get()
+  {
+    $subjects = Subject::get();
+
+    return response()->json([
+      'subjects' => $subjects,
+    ], 200);
+  }
+
+  public function delete($id)
+  {
+    $user = auth()->user();
+
+    $subject = $user->subjects()->where('subject_id', $id)->first();
+
+    if ($subject === null) {
+      return response()->json(['message' => 'User subject does not exist'], 204);
     }
 
-    public function get(){
-        $subjects = Subject::get();
+    $user->subjects()->detach($id);
 
-        return response()->json([
-            'subjects' => $subjects,
-        ], 200);
-    }
-
-    public function delete($id){
-        $user= auth()->user();
-        
-        $subject = $user->subjects()->where('subject_id', $id)->first();
-
-        if ($subject === null){ 
-            return response()->json(['message' => 'User subject does not exist'], 204);
-        }
-        
-        $user->subjects()->detach($id);
-
-        return response()->json([
-            'message' => 'User subject successfully deleted',
-        ], 200);
-    }
-
+    return response()->json([
+      'message' => 'User subject successfully deleted',
+    ], 200);
+  }
 }
