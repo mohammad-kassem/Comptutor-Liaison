08668950d9e5bda21344fafa5c5f8dc35acc2099
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:37:16 +0300

    backend: refactor: clean the code student middleware

--- backend/app/Http/Middleware/StudentMiddleware.php
+++ backend/app/Http/Middleware/StudentMiddleware.php
@@ -6,13 +6,15 @@ use Auth;
 use Closure;
 use Illuminate\Http\Request;
 
-class StudentMiddleware{
-    public function handle(Request $request, Closure $next){
-        $user = auth()->user();
-        if($user and $user->role_id == 1){
-            return $next($request);
-        }
-
-        return response()->json(['error' => 'Unauthorized'], 403);
+class StudentMiddleware
+{
+  public function handle(Request $request, Closure $next)
+  {
+    $user = auth()->user();
+    if ($user and $user->role_id == 1) {
+      return $next($request);
     }
+
+    return response()->json(['error' => 'Unauthorized'], 403);
+  }
 }
