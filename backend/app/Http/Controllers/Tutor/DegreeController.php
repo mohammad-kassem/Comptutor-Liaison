0fcf85c129c2d608c4f72b35261e960c750e361a
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:38:28 +0300

    backend: refactor: clean the code degree controller

--- backend/app/Http/Controllers/Tutor/DegreeController.php
+++ backend/app/Http/Controllers/Tutor/DegreeController.php
@@ -9,80 +9,85 @@ use App\Models\Degree;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class DegreeController extends Controller{
-    public function get(){
-        $user= auth()->user();
+class DegreeController extends Controller
+{
+  public function get()
+  {
+    $user = auth()->user();
 
-        $degrees = Degree::where('tutor_id', $user->id)->get();
+    $degrees = Degree::where('tutor_id', $user->id)->get();
 
-        return response()->json([
-            'degrees' => $degrees,
-        ], 200);
+    return response()->json([
+      'degrees' => $degrees,
+    ], 200);
+  }
+
+  public function add(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'university' => 'required|string|min:2|max:255',
+      'degree' => 'required|string|min:2|max:255',
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json($validator->errors(), 400);
     }
 
-    public function add(Request $request){
-        $validator = Validator::make($request->all(), [
-            'university' => 'required|string|min:2|max:255',
-            'degree' => 'required|string|min:2|max:255',
-        ]);
+    $user = auth()->user();
 
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
+    $degree = Degree::create([
+      'university' => $request->university,
+      'degree' => $request->degree,
+      'tutor_id' => $user->id
+    ]);
 
-        $user = auth()->user();
+    return response()->json([
+      'message' => 'Degree successfully added',
+      'degree' => $degree,
+    ], 201);
+  }
 
-        $degree = Degree::create([
-            'university' => $request->university,
-            'degree' => $request->degree,
-            'tutor_id' => $user->id
-        ]);
-
-        return response()->json([
-            'message' => 'Degree successfully added',
-            'degree' => $degree,
-        ], 201);
+  public function delete($id)
+  {
+    $degree = $degree = Degree::find($id);
+    if ($degree === null) {
+      return response()->json(['message' => 'User degree does not exist'], 204);
     }
 
-    public function delete($id){
-        $degree = $degree = Degree::find($id);
-        if ($degree === null){
-            return response()->json(['message' => 'User degree does not exist'], 204);
-        }
+    $degree->delete();
 
-        $degree->delete();
+    return response()->json([
+      'message' => 'Degree successfully deleted'
+    ], 200);
+  }
 
-        return response()->json([
-            'message' => 'Degree successfully deleted'
-        ], 200);
+  public function update(Request $request, $id)
+  {
+    $user = auth()->user();
+
+    $validator = Validator::make($request->all(), [
+      'university' => 'required|string|min:2|max:255',
+      'degree' => 'required|string|min:2|max:255',
+    ]);
+
+    if ($validator->fails()) {
+      return response()->json($validator->errors(), 400);
     }
-    
-    public function update(Request $request, $id){
-        $user = auth()->user();
 
-        $validator = Validator::make($request->all(), [
-            'university' => 'required|string|min:2|max:255',
-            'degree' => 'required|string|min:2|max:255',
-        ]);
-
-        if ($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        $degree = Degree::find($id);
-        if ($degree === null){ 
-            return response()->json(['error' => ['Not found']], 404);
-        }
-        
-        $degree->update([
-            'university' => $request->university,
-            'degree' => $request->degree,
-            'tutor_id' => $user->id
-        ]);
-
-        return response()->json([
-            'message' => 'Degree successfully updated',
-            'degree' => $degree
-        ], 200);
+    $degree = Degree::find($id);
+    if ($degree === null) {
+      return response()->json(['error' => ['Not found']], 404);
     }
+
+    $degree->update([
+      'university' => $request->university,
+      'degree' => $request->degree,
+      'tutor_id' => $user->id
+    ]);
+
+    return response()->json([
+      'message' => 'Degree successfully updated',
+      'degree' => $degree
+    ], 200);
+  }
 }
