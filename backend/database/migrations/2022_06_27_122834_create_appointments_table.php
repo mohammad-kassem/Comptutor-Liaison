6358dafbe914b5a43b518df42a5794d06a85a7c0
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:33:55 +0300

    backend: refactor: clean the code appointments migrations

--- backend/database/migrations/2022_06_27_122834_create_appointments_table.php
+++ backend/database/migrations/2022_06_27_122834_create_appointments_table.php
@@ -4,16 +4,19 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateAppointmentsTable extends Migration{
-    public function up(){
-        Schema::create('appointments', function (Blueprint $table) {
-            $table->integer('schedule_id')->primary();
-            $table->integer('student_id');
-            $table->timestamps();
-        });
-    }
+class CreateAppointmentsTable extends Migration
+{
+  public function up()
+  {
+    Schema::create('appointments', function (Blueprint $table) {
+      $table->integer('schedule_id')->primary();
+      $table->integer('student_id');
+      $table->timestamps();
+    });
+  }
 
-    public function down(){
-        Schema::dropIfExists('appointments');
-    }
+  public function down()
+  {
+    Schema::dropIfExists('appointments');
+  }
 }
--- backend/database/migrations/2022_07_13_181527_update_appointments_table.php
+++ backend/database/migrations/2022_07_13_181527_update_appointments_table.php
@@ -4,12 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class UpdateAppointmentsTable extends Migration{
-    public function up(){
-        Schema::table('appointments', function (Blueprint $table) {
-            $table->integer('tutor_id')->after("student_id");
-            $table->string('url')->after('tutor_id')->nullable();
-            $table->tinyInteger('status')->after('url')->default(0);
-        });
-    }
+class UpdateAppointmentsTable extends Migration
+{
+  public function up()
+  {
+    Schema::table('appointments', function (Blueprint $table) {
+      $table->integer('tutor_id')->after("student_id");
+      $table->string('url')->after('tutor_id')->nullable();
+      $table->tinyInteger('status')->after('url')->default(0);
+    });
+  }
 }
