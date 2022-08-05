73296e1db2f9884eb08f85c880da96086152f268
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:38:15 +0300

    backend: refactor: clean the code schedule controller

--- backend/app/Http/Controllers/Tutor/ScheduleController.php
+++ backend/app/Http/Controllers/Tutor/ScheduleController.php
@@ -9,75 +9,80 @@ use App\Models\Schedule;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class ScheduleController extends Controller{
-    public function get(){
-        $user= auth()->user();
-        
-        $schedules = Schedule::where('tutor_id', $user->id)->get();
+class ScheduleController extends Controller
+{
+  public function get()
+  {
+    $user = auth()->user();
 
-        return response()->json([
-            'schedules' => $schedules,
-        ], 200);
+    $schedules = Schedule::where('tutor_id', $user->id)->get();
+
+    return response()->json([
+      'schedules' => $schedules,
+    ], 200);
+  }
+
+  public function getAvailableTimes($id)
+  {
+    $schedules = Schedule::where('tutor_id', $id)->get();
+
+    $available_times = [];
+    foreach ($schedules as $schedule) {
+      if (!$schedule->appointment) {
+        array_push($available_times, $schedule);
+      }
     }
 
-    public function getAvailableTimes($id){
-        $schedules = Schedule::where('tutor_id', $id)->get();
+    return response()->json([
+      'available_times' => $available_times,
+    ], 200);
+  }
 
-        $available_times = [];
-        foreach ($schedules as $schedule){
-            if (!$schedule->appointment){ 
-              array_push($available_times, $schedule);
-            } 
-        }
+  public function add(Request $request)
+  {
+    $hours = $request->hours;
+    $user = auth()->user();
+    $schedule = [];
+    foreach ($hours as $hour) {
+      $hour = json_decode(json_encode($hour));
 
-        return response()->json([
-            'available_times' => $available_times,
-        ], 200);
+      $validator = Validator::make(get_object_vars($hour), [
+        'date' => 'required|date_format:Y-m-d',
+        'start_time' => 'required|date_format:H:i',
+        'end_time' => 'required|date_format:H:i|after:start_time'
+      ]);
+
+      if ($validator->fails()) {
+        return response()->json($validator->errors(), 400);
+      }
+
+      $available_hour = Schedule::create([
+        'tutor_id' => $user->id,
+        'date' => $hour->date,
+        'start_time' => $hour->start_time,
+        'end_time' => $hour->end_time,
+      ]);
+
+      array_push($schedule, $available_hour);
     }
 
-    public function add(Request $request){
-        $hours = $request->hours;
-        $user = auth()->user();
-        $schedule = [];
-        foreach($hours as $hour){
-            $hour = json_decode(json_encode($hour));
+    return response()->json([
+      'message' => 'Schedule successfully added',
+      'schedule' => $schedule
+    ], 201);
+  }
 
-            $validator = Validator::make(get_object_vars($hour), [
-                'date' => 'required|date_format:Y-m-d',
-                'start_time' => 'required|date_format:H:i',
-                'end_time' => 'required|date_format:H:i|after:start_time'
-            ]);
-
-            if($validator->fails()) {
-                return response()->json($validator->errors(), 400);
-            }
-
-            $available_hour = Schedule::create([
-                'tutor_id' => $user->id,
-                'date' => $hour->date,
-                'start_time' => $hour->start_time,
-                'end_time' => $hour->end_time,
-            ]);
-
-            array_push($schedule, $available_hour);
-        }
-
-        return response()->json([
-            'message' => 'Schedule successfully added',
-            'schedule' => $schedule
-        ], 201);
+  public function delete($id)
+  {
+    $schedule = $schedule = Schedule::find($id);
+    if ($schedule === null) {
+      return response()->json(['message' => 'Schedule does not exist'], 204);
     }
 
-    public function delete($id){
-        $schedule = $schedule = Schedule::find($id);
-        if ($schedule === null){
-            return response()->json(['message' => 'Schedule does not exist'], 204);
-        }
-        
-        $schedule->delete();
+    $schedule->delete();
 
-        return response()->json([
-            'mesasage' => 'Schedule successfully deleted'
-        ], 200);
-    }
+    return response()->json([
+      'mesasage' => 'Schedule successfully deleted'
+    ], 200);
+  }
 }
