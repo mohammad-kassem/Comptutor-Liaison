6f4b6d41500683fd5c3c14454d71a6d65488e1de
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:35:54 +0300

    backend: refactor: clean the code Schedule model

--- backend/app/Models/Schedule.php
+++ backend/app/Models/Schedule.php
@@ -5,21 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Schedule extends Model{
-    use HasFactory;
+class Schedule extends Model
+{
+  use HasFactory;
 
-    protected $fillable = [
-        'date',
-        'start_time',
-        'end_time',
-        'tutor_id'
-    ];
+  protected $fillable = [
+    'date',
+    'start_time',
+    'end_time',
+    'tutor_id'
+  ];
 
-    public function appointment(){
-        return $this->hasOne(Appointment::class);
-    }
+  public function appointment()
+  {
+    return $this->hasOne(Appointment::class);
+  }
 
-    public function user(){
-        return $this->belongsTo(User::class, 'tutor_id');
-    }
+  public function user()
+  {
+    return $this->belongsTo(User::class, 'tutor_id');
+  }
 }
