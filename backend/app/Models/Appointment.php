dca6738b4281c4ae27312d56092dbdffb5ac6b99
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:36:36 +0300

    backend: refactor: clean the code Appointment model

--- backend/app/Models/Appointment.php
+++ backend/app/Models/Appointment.php
@@ -5,29 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Appointment extends Model{
-    use HasFactory;
+class Appointment extends Model
+{
+  use HasFactory;
 
-    protected $fillable = [
-        'schedule_id',
-        'student_id',
-        'tutor_id',
-        'url',
-        'status'
+  protected $fillable = [
+    'schedule_id',
+    'student_id',
+    'tutor_id',
+    'url',
+    'status'
 
-    ];
+  ];
 
-    protected $primaryKey = 'schedule_id';
+  protected $primaryKey = 'schedule_id';
 
-    public function schedule(){
-        return $this->belongsTo(Schedule::class);
-    }
+  public function schedule()
+  {
+    return $this->belongsTo(Schedule::class);
+  }
 
-    public function student(){
-        return $this->belongsTo(User::class, 'student_id');
-    }
+  public function student()
+  {
+    return $this->belongsTo(User::class, 'student_id');
+  }
 
-    public function tutor(){
-        return $this->belongsTo(User::class, 'tutor_id');
-    }
+  public function tutor()
+  {
+    return $this->belongsTo(User::class, 'tutor_id');
+  }
 }
