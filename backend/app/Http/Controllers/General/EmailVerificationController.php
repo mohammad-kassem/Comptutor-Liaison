c4f4aa73079e3f39443aa7a2804d58632f7ea659
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:38:03 +0300

    backend: refactor: clean the code email verification controller

--- backend/app/Http/Controllers/General/EmailVerificationController.php
+++ backend/app/Http/Controllers/General/EmailVerificationController.php
@@ -9,55 +9,55 @@ use App\Mail\VerificationMail;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
 
-class EmailVerificationController extends Controller{
-    public function verifyEmail(Request $request){
-        $user = auth()->user();
+class EmailVerificationController extends Controller
+{
+  public function verifyEmail(Request $request)
+  {
+    $user = auth()->user();
 
-        if ($request->code === $user->activation_code){
-            $user->update([
-                'is_verified' => 1
-            ]);
-        }
+    if ($request->code === $user->activation_code) {
+      $user->update([
+        'is_verified' => 1
+      ]);
+    } else return response()->json(['message' => ['Wrong activation code']], 401);
 
-        else return response()->json(['message'=>['Wrong activation code']], 401);
+    return response()->json([
+      'message' => 'Email successfully verified',
+      'user' => $user
+    ], 200);
+  }
 
-        return response()->json([
-            'message' => 'Email successfully verified',
-            'user' => $user
-        ], 200); 
-        
+  public function sendEmail()
+  {
+    $user = auth()->user();
+
+    $this->setCode();
+
+    Mail::to($user->email)->send(new VerificationMail($user));
+
+    return response()->json([
+      'message' => 'Email successfully sent',
+    ], 200);
+  }
+
+  public function setCode()
+  {
+    $user = auth()->user();
+
+    $length = 4;
+    $characters = '0123456789';
+    $charactersLength = strlen($characters);
+    $code = '';
+    for ($i = 0; $i < $length; $i++) {
+      $code .= $characters[rand(0, $charactersLength - 1)];
     }
 
-    public function sendEmail(){    
-        $user = auth()->user();
+    $user->update([
+      'activation_code' => $code
+    ]);
 
-        $this->setCode();
-
-        Mail::to($user->email)->send(new VerificationMail($user));
-
-        return response()->json([
-            'message' => 'Email successfully sent',
-        ], 200); 
-
-    }
-
-    public function setCode(){    
-        $user = auth()->user();
-
-        $length = 4;
-        $characters = '0123456789';
-        $charactersLength = strlen($characters);
-        $code = '';
-        for ($i = 0; $i < $length; $i++) {
-            $code .= $characters[rand(0, $charactersLength - 1)];
-        }
-        
-        $user->update([
-            'activation_code' => $code
-        ]);
-
-        return response()->json([
-            'message' => 'Activation code successfully set',
-        ], 200); 
-    }
+    return response()->json([
+      'message' => 'Activation code successfully set',
+    ], 200);
+  }
 }
