ed4d2c12811d79ca916d433fe3daf5151197671f
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:38:43 +0300

    backend: refactor: clean the code student controller

--- backend/app/Http/Controllers/Student/StudentController.php
+++ backend/app/Http/Controllers/Student/StudentController.php
@@ -8,27 +8,29 @@ use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class StudentController extends Controller{
-    public function update(Request $request){   
-        $validator = Validator::make($request->all(), [
-            'fname' => 'required|string|min:2|max:255',
-            'lname' => 'required|string|min:2|max:255',
-        ]);
+class StudentController extends Controller
+{
+  public function update(Request $request)
+  {
+    $validator = Validator::make($request->all(), [
+      'fname' => 'required|string|min:2|max:255',
+      'lname' => 'required|string|min:2|max:255',
+    ]);
 
-        if($validator->fails()) {
-            return response()->json($validator->errors(), 400);
-        }
-
-        $student = auth()->user();
-
-        $student->update([
-            'fname' => $request->fname,
-            'lname' => $request->lname
-        ]);
-
-        return response()->json([
-            'message' => 'Account info successfully updated',
-            'student' => $student,
-        ], 200);
+    if ($validator->fails()) {
+      return response()->json($validator->errors(), 400);
     }
+
+    $student = auth()->user();
+
+    $student->update([
+      'fname' => $request->fname,
+      'lname' => $request->lname
+    ]);
+
+    return response()->json([
+      'message' => 'Account info successfully updated',
+      'student' => $student,
+    ], 200);
+  }
 }
