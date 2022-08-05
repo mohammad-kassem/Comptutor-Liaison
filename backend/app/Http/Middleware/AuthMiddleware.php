246efe92b24d1f11a80e58ae531c974cb43e2be1
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-05 05:37:27 +0300

    backend: refactor: clean the code auth middleware

--- backend/app/Http/Middleware/AuthMiddleware.php
+++ backend/app/Http/Middleware/AuthMiddleware.php
@@ -6,13 +6,15 @@ use Auth;
 use Closure;
 use Illuminate\Http\Request;
 
-class TutorMiddleware{
-    public function handle(Request $request, Closure $next){
-        $user = auth()->user();
-        if($user){
-            return $next($request);
-        }
-
-        return response()->json(['error' => 'Unauthenticated'], 401);
+class AuthMiddleware
+{
+  public function handle(Request $request, Closure $next)
+  {
+    $user = auth()->user();
+    if ($user) {
+      return $next($request);
     }
+
+    return response()->json(['error' => 'Unauthenticated'], 401);
+  }
 }
