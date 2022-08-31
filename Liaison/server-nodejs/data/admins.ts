fd0d8192e81ba0477be5928950397604111950e5
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-31 08:28:48 +0300

    liaison: backend: feat: set up the data to seed into the db

---
+++ Liaison/server-nodejs/data/admins.ts
@@ -0,0 +1,7 @@
+export const adminsData = [
+  {
+    fname: "Mhmd",
+    lname: "Kassem",
+    email: "mhmd.kassem.eng@gmail.com",
+  },
+];
---
+++ Liaison/server-nodejs/data/roles.ts
@@ -0,0 +1,1 @@
+export const rolesData = [{ role: "user" }, { role: "admin" }];
