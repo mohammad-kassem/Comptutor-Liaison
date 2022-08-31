cf8a90220037513040eafeb9ea57ff6f2e10c733
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-31 06:41:55 +0300

    liaison: backend: feat: implement the db config

---
+++ Liaison/server-nodejs/config/db.ts
@@ -0,0 +1,17 @@
+require("dotenv").config();
+import mongoose from "mongoose";
+const DB_NAME = process.env.DB_NAME || "";
+
+export const connectDB = async () => {
+  console.log(DB_NAME);
+  try {
+    await mongoose.connect(`mongodb://localhost:27017/${DB_NAME}`);
+    console.log("Connected to Database");
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const discconectDB = async () => {
+  await mongoose.disconnect();
+};
