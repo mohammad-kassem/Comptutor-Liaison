aa33db37ae2f039c03de76e623eedc204560d4f2
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-31 20:39:34 +0300

    liaison: backend: feat: implement the add contact function

---
+++ Liaison/server-nodejs/src/contact/service.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from "express";
+import { handleValidationError } from "../../error-handeling/dataValidation";
+import { handleUnexpectedError } from "../../error-handeling/unexcpectedError";
+import Contact from "../../model/contact";
+import User from "../../model/user";
+
+export const addContact = async (body: any, userID: string | undefined) => {
+  const { name, email, phone, relationship, location, country } = body;
+
+  const contact = new Contact({
+    name,
+    email,
+    phone,
+    relationship,
+    location,
+    country,
+    user: userID,
+  });
+
+  return await contact.save();
+};
