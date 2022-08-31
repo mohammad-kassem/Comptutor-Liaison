ba281915bcacda3a760d66d37254fe843a6c77cb
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-08-31 08:15:50 +0300

    liaison: backend: feat: implement the role model

---
+++ Liaison/server-nodejs/model/role.ts
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+export interface IRole extends mongoose.Document {
+  role: string;
+  users: any[];
+}
+
+const roleSchema = new mongoose.Schema({
+  role: {
+    type: String,
+    required: true,
+    enum: ["user", "admin"],
+  },
+
+  users: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+});
+
+const Role = mongoose.model<IRole>("Role", roleSchema);
+export default Role;
