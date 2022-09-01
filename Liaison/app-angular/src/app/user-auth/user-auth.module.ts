3f7d4d58d57f9a6d9b3025b9b63e56eec7e0de14
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-01 17:32:23 +0300

    liaison: frontend: feat: generate the auth-form component

--- Liaison/app-angular/src/app/user-auth/user-auth.module.ts
+++ Liaison/app-angular/src/app/user-auth/user-auth.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserAuthRoutingModule } from './user-auth-routing.module';
+import { AuthFormComponent } from './components/auth-form/auth-form.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AuthFormComponent
+  ],
   imports: [
     CommonModule,
     UserAuthRoutingModule
