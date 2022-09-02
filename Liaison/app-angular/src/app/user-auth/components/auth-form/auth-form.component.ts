6fb9803e88f4539a5a6a8a5ac1abc327320d39ac
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-02 03:50:24 +0300

    liaison: frontend: feat: declare the auth-form fields and methods

--- Liaison/app-angular/src/app/user-auth/components/auth-form/auth-form.component.ts
+++ Liaison/app-angular/src/app/user-auth/components/auth-form/auth-form.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./auth-form.component.scss']
 })
 export class AuthFormComponent implements OnInit {
+  fname: string;
+  lname: string;
+  email: string;
+  password: string;
+  type: string = "register";
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(): void {
+    const cridential = {
+      fname: this.fname,
+      lname: this.lname,
+      email: this.email,
+      password: this.password,
+    }
+
+    console.log(this.email);
+
+    this.fname = '';
+    this.lname = '';
+    this.email = '';
+    this.password = '';
   }
 
 }
