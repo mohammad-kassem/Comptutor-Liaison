8eb541dbcc2e637306e99b0865ca849b943c406d
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-02 23:17:31 +0300

    liaison: frontend: feat: implement using the auth services register method

--- Liaison/app-angular/src/app/user-auth/pages/register/register.component.ts
+++ Liaison/app-angular/src/app/user-auth/pages/register/register.component.ts
@@ -1,4 +1,7 @@
+import { ICridentials } from './../../models/cridentials';
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service';
+import { handleErrors } from 'src/app/utils/error-handling';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  type: string = 'register';
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
   }
 
+  handleSuccess(): void {
+    console.log("successfully")
+  }
+
+
+  register(cridential: ICridentials): void {
+    this.authService.register(cridential).subscribe({
+      complete: this.handleSuccess, 
+      error: (error) => handleErrors(error)
+    })
+  }
 }
