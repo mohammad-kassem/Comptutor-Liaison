ca5950cb574d76ff3961d6981484f028b2f8f5a0
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-02 05:02:40 +0300

    liaison: frontend: feat: set up the register page

--- Liaison/app-angular/src/app/user-auth/user-auth.module.ts
+++ Liaison/app-angular/src/app/user-auth/user-auth.module.ts
@@ -5,12 +5,14 @@ import { UserAuthRoutingModule } from './user-auth-routing.module';
 import { AuthFormComponent } from './components/auth-form/auth-form.component';
 import { FormsModule } from '@angular/forms';
 import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 @NgModule({
   declarations: [
     AuthFormComponent,
     LoginComponent,
+    RegisterComponent
   ],
   imports: [
     CommonModule,
