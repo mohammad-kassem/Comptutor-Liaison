bc5195738d7cd149113c6e6b9f8135840d702b80
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-04 22:55:49 +0300

    liaison: frontend: feat: implement the contact model

---
+++ Liaison/app-angular/src/app/contacts/models/contact.ts
@@ -0,0 +1,11 @@
+export interface IContact {
+  name: string;
+  email: string;
+  phone: string;
+  relationship: string;
+  location: any;
+  country: string;
+  user: any;
+  liker?: any;
+  messages?: any[];
+}
