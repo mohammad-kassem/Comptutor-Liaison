6b6beccc0145c6c419b6a951d184ca31c6029898
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-05 00:58:37 +0300

    liaison: frontend: feat: set up the add contact page

--- Liaison/app-angular/src/app/contacts/contacts-routing.module.ts
+++ Liaison/app-angular/src/app/contacts/contacts-routing.module.ts
@@ -1,9 +1,11 @@
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddContactComponent } from './pages/add-contact/add-contact.component';
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent}
+  {path: '', component: HomePageComponent},
+  {path: 'contact/add', component: AddContactComponent}
 ];
 
 @NgModule({
--- Liaison/app-angular/src/app/contacts/contacts.module.ts
+++ Liaison/app-angular/src/app/contacts/contacts.module.ts
@@ -1,3 +1,4 @@
+import { ContactFormComponent } from './components/contact-form/contact-form.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -5,12 +6,15 @@ import { CommonModule } from '@angular/common';
 import { ContactsRoutingModule } from './contacts-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { DisplayContactsComponent } from './componenets/display-contacts/display-contacts.component';
+import { AddContactComponent } from './pages/add-contact/add-contact.component';
 
 
 @NgModule({
   declarations: [
     HomePageComponent,
-    DisplayContactsComponent
+    DisplayContactsComponent,
+    AddContactComponent,
+    ContactFormComponent,
   ],
   imports: [
     CommonModule,
