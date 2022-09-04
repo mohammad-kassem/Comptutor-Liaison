e17c590fc315b7076aa20d251b8812339b97123e
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-05 00:39:22 +0300

    liaison: frontend: feat: use the remove contact functionality

--- Liaison/app-angular/src/app/contacts/componenets/display-contacts/display-contacts.component.ts
+++ Liaison/app-angular/src/app/contacts/componenets/display-contacts/display-contacts.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ContactsService } from '../../contacts.service';
 import { faTimes, faStar, faPen } from '@fortawesome/free-solid-svg-icons';
 import { faStar as faStarRegular} from '@fortawesome/free-regular-svg-icons';
+import { IContact } from '../../models/contact';
+import { ErrorHandelingService } from 'src/app/utils/error-handling/error-handeling.service';
 
 @Component({
   selector: 'app-display-contacts',
@@ -9,17 +11,25 @@ import { faStar as faStarRegular} from '@fortawesome/free-regular-svg-icons';
   styleUrls: ['./display-contacts.component.scss']
 })
 export class DisplayContactsComponent implements OnInit {
-  contacts: any[];
+  contacts: IContact[];
   faTimes = faTimes;
   faStar = faStar;
   faStarRegular = faStarRegular;
   faPen = faPen;
 
-  constructor(private contactsService: ContactsService) { }
+  constructor(private contactsService: ContactsService, private errorService: ErrorHandelingService) { }
 
   ngOnInit(): void {
-    this.contactsService.getContacts().subscribe(
-      {next: (contacts) => this.contacts = contacts.contacts}
-    );
+    this.contactsService.getContacts().subscribe({
+      next: (contacts) => this.contacts = contacts.contacts,
+      error: (error) => this.errorService.handleErrors(error)
+    })
+  }
+
+  onRemove(removedContact: IContact): void {
+    this.contactsService.removeContact(removedContact).subscribe({
+      next: (response) => this.contacts = this.contactsService.handleRemove(response, this.contacts),
+      error: (error) => this.errorService.handleErrors(error)
+    })
   }
 }
