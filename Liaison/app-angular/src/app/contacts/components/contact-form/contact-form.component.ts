368440a2fbf3edfd9d6fdf34b86131aa17cb5cf9
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-06 02:02:49 +0300

    liaison: frontend: feat: implement the contact-form and integrate it with the add-contact

--- Liaison/app-angular/src/app/contacts/components/contact-form/contact-form.component.ts
+++ Liaison/app-angular/src/app/contacts/components/contact-form/contact-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IContact } from '../../models/contact';
 
 @Component({
   selector: 'app-contact-form',
@@ -6,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent implements OnInit {
-
-  constructor() { }
+  @Input() name: string;
+  @Input() email: string;
+  @Input() phone: string;
+  @Input() relationship: string;
+  @Input() location: IContact["location"];
+  
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  contactForm = new FormGroup({
+    name: new FormControl(
+      '',
+      Validators.compose([Validators.required, Validators.minLength(6)])
+    ),
+    email: new FormControl(
+      '',
+      Validators.compose([Validators.required, Validators.email, Validators.minLength(6)])
+    ),
+    phone: new FormControl(
+      '',
+      Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(15), Validators.pattern(/^\d+$/)])
+    ),
+    relationship: new FormControl(
+      '',
+      Validators.compose([Validators.required])
+    )
+  })
+
+  onCancel (): void {
+    this.router.navigate(['/']);
   }
 
 }
