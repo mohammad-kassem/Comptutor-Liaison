23341aa314e1ea1fb4609ad327bac052a17ffc37
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-06 01:57:51 +0300

    liaison: frontend: feat: integrate the contact info with add-contact

--- Liaison/app-angular/src/app/contacts/pages/add-contact/add-contact.component.ts
+++ Liaison/app-angular/src/app/contacts/pages/add-contact/add-contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IContact } from '../../models/contact';
 
 @Component({
   selector: 'app-add-contact',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-contact.component.scss']
 })
 export class AddContactComponent implements OnInit {
+  name: string = '';
+  email: string = '';
+  phone: string = '';
+  relationship: string = 'Single';
+  location: IContact["location"] = {lat: 33.89539, long: 35.481902} //beirut default coordinates
 
   constructor() { }
 
