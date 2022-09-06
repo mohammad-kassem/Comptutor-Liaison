997e773f835eaefea65740a004c1039f01c555d0
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-06 15:07:34 +0300

    liaison: frontend: feat: implement the contacts service hadleAdd method

--- Liaison/app-angular/src/app/contacts/contacts.service.ts
+++ Liaison/app-angular/src/app/contacts/contacts.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { generateApiUrl, generateHttpOptions } from '../utils/api-functions/api-functions';
 import { IContact } from './models/contact';
+import { Router } from '@angular/router';
 
 const authToken: string = localStorage.getItem('auth_token') || ''; 
 const httpOptions = generateHttpOptions(authToken);
@@ -13,7 +14,7 @@ const httpOptions = generateHttpOptions(authToken);
 })
 export class ContactsService {
   
-  constructor(private http:HttpClient, private toastr: ToastrService) { }
+  constructor(private http:HttpClient, private toastr: ToastrService, private router: Router) { }
 
   getContacts(): Observable<{message: string, contacts: IContact[]}> {
     const getContactsApiUrl: string = generateApiUrl('contacts');
@@ -38,5 +39,10 @@ export class ContactsService {
   addContact(contact: IContact): Observable<{message: string, new: IContact}> {
     const removeContactApiUrl: string = generateApiUrl('contacts', 'add');
     return this.http.post<{message: string, new: IContact}>(removeContactApiUrl, contact, httpOptions);
+  }
+
+  handleAdd(response: {message: string, new: IContact}){
+    this.toastr.success(response.message, 'Contact Added');
+    this.router.navigate(['/']);
   }
 }
