83af7eeaa365a5ea11719d803c73455109f33787
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-11 17:08:22 +0300

    liaison: frontend: feat: implement the inbox

---
+++ Liaison/app-angular/src/app/messages/pages/inbox/inbox.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ErrorHandelingService } from 'src/app/utils/error-handling/error-handeling.service';
+import { MessagesService } from '../../messages.service';
+import { IMessage } from '../../models/messages';
+
+@Component({
+  selector: 'app-inbox',
+  templateUrl: './inbox.component.html',
+  styleUrls: ['./inbox.component.scss']
+})
+export class InboxComponent implements OnInit {
+  messages: IMessage[];
+
+  constructor(private messagesService: MessagesService, private errorService: ErrorHandelingService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.messagesService.getMessages().subscribe({
+      next: (response) => this.messages = response.messages,
+      error: (error) => this.errorService.handleErrors(error),
+    })
+  }
+
+  refactorText(text: string): string {
+    const re = new RegExp('<br />', 'g');
+    const output = text.replace(re, '\n');
+    return output.length >= 80 ? output.slice(0, 80) + '...' : output;
+  }
+
+  searchMessages(searchText: string): void {
+    this.messagesService.filterMessages(searchText).subscribe({
+      next: (response) => this.messages = response.messages,
+      error: (error) => this.errorService.handleErrors(error),
+    })
+  }
+}
