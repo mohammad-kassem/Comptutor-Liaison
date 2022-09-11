4e0ea859edd4db2b6883a9346594a154a234a7ed
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-11 16:49:01 +0300

    liaison: frontend: feat: implement the messages service sendMessage method

---
+++ Liaison/app-angular/src/app/messages/messages.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { generateApiUrl, generateHttpOptions } from '../utils/api-functions/api-functions';
+import { IMessage, IMessageBody } from './models/messages';
+
+const authToken: string = localStorage.getItem('auth_token') || ''; 
+const httpOptions = generateHttpOptions(authToken);
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessagesService {
+
+  constructor(private http: HttpClient, private toastr: ToastrService) { }
+  
+  sendMessage(messageBody: IMessageBody): Observable<{message: string, new: IMessage}> {
+    const sendMessageApiUrl: string = generateApiUrl('messages', 'send');
+    return this.http.post<{message: string, new: IMessage}>(sendMessageApiUrl, messageBody, httpOptions);
+  }
+
+  handleSend(response: {message: string, new: IMessage}): void {
+    this.toastr.success(response.message, 'Message Sent')
+  }
+
+}
