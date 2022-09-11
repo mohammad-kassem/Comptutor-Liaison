5f7e51930ba0de64c145d8327d817f4762f23334
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-11 17:10:49 +0300

    liaison: frontend: refactor: refactor the contacts service

--- Liaison/app-angular/src/app/contacts/contacts.service.ts
+++ Liaison/app-angular/src/app/contacts/contacts.service.ts
@@ -1,68 +1,111 @@
 import { ToastrService } from 'ngx-toastr';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { generateApiUrl, generateHttpOptions } from '../utils/api-functions/api-functions';
+import { BehaviorSubject, Observable } from 'rxjs';
+import {
+  generateApiUrl,
+  generateHttpOptions,
+} from '../utils/api-functions/api-functions';
 import { IContact } from './models/contact';
 import { Router } from '@angular/router';
 
-const authToken: string = localStorage.getItem('auth_token') || ''; 
+const authToken: string = localStorage.getItem('auth_token') || '';
 const httpOptions = generateHttpOptions(authToken);
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ContactsService {
-  filteredContactsSubject = new Subject<any>();
+  filteredContactsSubject = new BehaviorSubject<any>([]);
   filteredContacts = this.filteredContactsSubject.asObservable();
 
+  constructor(
+    private http: HttpClient,
+    private toastr: ToastrService,
+    private router: Router
+  ) {}
 
-  constructor(private http:HttpClient, private toastr: ToastrService, private router: Router) { }
-
-  getContacts(): Observable<{message: string, contacts: IContact[]}> {
+  getContacts(): Observable<{ message: string; contacts: IContact[] }> {
     const getContactsApiUrl: string = generateApiUrl('contacts');
-    return this.http.get<{message: string, contacts: IContact[]}>(getContactsApiUrl,  httpOptions);
+    return this.http.get<{ message: string; contacts: IContact[] }>(
+      getContactsApiUrl,
+      httpOptions
+    );
   }
 
-  getContact(id: string): Observable<{contact: IContact}> {
+  getContact(id: string): Observable<{ contact: IContact }> {
     const getContactApiUrl: string = generateApiUrl('contacts', '', id);
-    return this.http.get<{contact: IContact}>(getContactApiUrl,  httpOptions);
+    return this.http.get<{ contact: IContact }>(getContactApiUrl, httpOptions);
   }
 
-  removeContact(contact: IContact): Observable<{message: string, deleted: IContact}> {
-    const removeContactApiUrl: string = generateApiUrl('contacts', 'remove', contact._id);
-    return this.http.delete<{message: string, deleted: IContact}>(removeContactApiUrl, httpOptions);
+  removeContact(
+    contact: IContact
+  ): Observable<{ message: string; deleted: IContact }> {
+    const removeContactApiUrl: string = generateApiUrl(
+      'contacts',
+      'remove',
+      contact._id
+    );
+    return this.http.delete<{ message: string; deleted: IContact }>(
+      removeContactApiUrl,
+      httpOptions
+    );
   }
 
-  handleRemove(response: {message: string, deleted: IContact}, DBContacts: IContact[], filteredContacts: IContact[]): [IContact[], IContact[]]  {
-    this.toastr.success(response.message, 'Contact Deleted')
-    filteredContacts = filteredContacts.filter((contact: IContact) => contact._id !== response.deleted._id)
-    DBContacts = DBContacts.filter((contact: IContact) => contact._id !== response.deleted._id)
-    return [DBContacts, filteredContacts]
+  handleRemove(
+    response: { message: string; deleted: IContact },
+    DBContacts: IContact[],
+    filteredContacts: IContact[]
+  ): [IContact[], IContact[]] {
+    this.toastr.success(response.message, 'Contact Deleted');
+    filteredContacts = filteredContacts.filter(
+      (contact: IContact) => contact._id !== response.deleted._id
+    );
+    DBContacts = DBContacts.filter(
+      (contact: IContact) => contact._id !== response.deleted._id
+    );
+    return [DBContacts, filteredContacts];
   }
 
-  getCountry(location: IContact["location"]): Observable<any> {
-    const geoCoderApiURl: string =`https://nominatim.openstreetmap.org/reverse?format=json&lat=${location.lat}&lon=${location.long}&accept-language=en`;
+  getCountry(location: IContact['location']): Observable<any> {
+    const geoCoderApiURl: string = `https://nominatim.openstreetmap.org/reverse?format=json&lat=${location.lat}&lon=${location.long}&accept-language=en`;
     return this.http.get<any>(geoCoderApiURl, httpOptions);
   }
 
-  addContact(contact: IContact): Observable<{message: string, new: IContact}> {
+  addContact(
+    contact: IContact
+  ): Observable<{ message: string; new: IContact }> {
     const addContactApiUrl: string = generateApiUrl('contacts', 'add');
-    return this.http.post<{message: string, new: IContact}>(addContactApiUrl, contact, httpOptions);
+    return this.http.post<{ message: string; new: IContact }>(
+      addContactApiUrl,
+      contact,
+      httpOptions
+    );
   }
 
-  handleAdd(response: {message: string, new: IContact}){
+  handleAdd(response: { message: string; new: IContact }) {
     this.toastr.success(response.message, 'Contact Added');
     this.router.navigate(['/']);
   }
 
-  updateContact(updatedDetails: IContact, contactId: string): Observable<{message: string, updated: IContact}> {
-    const updateContactApiUrl: string = generateApiUrl('contacts', 'update', contactId);
-    return this.http.put<{message: string, updated: IContact}>(updateContactApiUrl, updatedDetails, httpOptions);
+  updateContact(
+    updatedDetails: IContact,
+    contactId: string
+  ): Observable<{ message: string; updated: IContact }> {
+    const updateContactApiUrl: string = generateApiUrl(
+      'contacts',
+      'update',
+      contactId
+    );
+    return this.http.put<{ message: string; updated: IContact }>(
+      updateContactApiUrl,
+      updatedDetails,
+      httpOptions
+    );
   }
 
-  handleUpdate(response: {message: string, updated: IContact})  {
-    this.toastr.success(response.message, 'Contact Deleted')
+  handleUpdate(response: { message: string, updated: IContact }) {
+    this.toastr.success(response.message, 'Contact Deleted');
     this.router.navigate(['/']);
   }
 
@@ -70,20 +113,42 @@ export class ContactsService {
     this.filteredContactsSubject.next(contacts);
   }
 
-  toggleLikeContact(contact: IContact): Observable<{message: string, liked?: IContact, unliked?: IContact}> {
-    const likeContactApiUrl: string = generateApiUrl('contacts', contact.liker ? 'unlike' : 'like', contact._id);
-    return this.http.put<{message: string, liked?: IContact, unliked?: IContact}>(likeContactApiUrl,'', httpOptions);
+  toggleLikeContact(
+    contact: IContact
+  ): Observable<{ message: string; liked?: IContact; unliked?: IContact }> {
+    const likeContactApiUrl: string = generateApiUrl(
+      'contacts',
+      contact.liker ? 'unlike' : 'like',
+      contact._id
+    );
+    return this.http.put<{
+      message: string;
+      liked?: IContact;
+      unliked?: IContact;
+    }>(likeContactApiUrl, '', httpOptions);
   }
 
-  handleToggleLike(response: {message: string, liked?: IContact, unliked?: IContact}, DBContacts: IContact[], filteredContacts: IContact[]): [IContact[], IContact[]] {
+  handleToggleLike(
+    response: { message: string; liked?: IContact; unliked?: IContact },
+    DBContacts: IContact[],
+    filteredContacts: IContact[]
+  ): [IContact[], IContact[]] {
     DBContacts = DBContacts.map((contact: IContact) => {
-      if (response.liked) return contact._id === response.liked?._id ? response.liked : contact
-      else return contact._id === response.unliked?._id ? response.unliked : contact
-    })
+      if (response.liked)
+        return contact._id === response.liked?._id ? response.liked : contact;
+      else
+        return contact._id === response.unliked?._id
+          ? response.unliked
+          : contact;
+    });
     filteredContacts = filteredContacts.map((contact: IContact) => {
-      if (response.liked) return contact._id === response.liked?._id ? response.liked : contact
-      else return contact._id === response.unliked?._id ? response.unliked : contact
-    })
-    return [DBContacts, filteredContacts]
+      if (response.liked)
+        return contact._id === response.liked?._id ? response.liked : contact;
+      else
+        return contact._id === response.unliked?._id
+          ? response.unliked
+          : contact;
+    });
+    return [DBContacts, filteredContacts];
   }
 }
