43877e74d01073d81af692d473e9a1c3d2b97b31
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-11 17:06:16 +0300

    liaison: frontend: feat: implement the messagfes search-bar

---
+++ Liaison/app-angular/src/app/messages/components/search-bar/search-bar.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-search-bar',
+  templateUrl: './search-bar.component.html',
+  styleUrls: ['./search-bar.component.scss']
+})
+export class SearchBarComponent implements OnInit {
+  faSearch = faSearch;
+  searchText: string;
+  @Output() handleSearch: EventEmitter<any> = new EventEmitter;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSearch(): void {
+    this.handleSearch.emit(this.searchText);
+  }
+}
