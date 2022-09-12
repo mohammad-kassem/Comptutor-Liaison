95903965d0557668d976108e1f7a721a5dcecfd2
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-12 18:34:01 +0300

    liaison: frontend: feat: implement the user-stats

--- Liaison/app-angular/src/app/admin/admin-routing.module.ts
+++ Liaison/app-angular/src/app/admin/admin-routing.module.ts
@@ -1,9 +1,11 @@
 import { DisplayUsersComponent } from './pages/display-users/display-users.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserStatsComponent } from './pages/user-stats/user-stats.component';
 
 const routes: Routes = [
   {path: 'admin', component: DisplayUsersComponent},
+  {path: 'admin/user/:id', component: UserStatsComponent}
 ];
 
 @NgModule({
--- Liaison/app-angular/src/app/admin/admin.module.ts
+++ Liaison/app-angular/src/app/admin/admin.module.ts
@@ -5,16 +5,22 @@ import { AdminRoutingModule } from './admin-routing.module';
 import { DisplayUsersComponent } from './pages/display-users/display-users.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from '../shared/shared.module';
+import { UserStatsComponent } from './pages/user-stats/user-stats.component';
+import { NgxChartsModule } from '@swimlane/ngx-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     DisplayUsersComponent,
+    UserStatsComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
     FontAwesomeModule,
     SharedModule,
+    NgxChartsModule,
+    BrowserAnimationsModule 
   ]
 })
 export class AdminModule { }
