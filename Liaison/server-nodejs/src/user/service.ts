46dbccd1c842fde70ba4cceef3a8a33c048f78ad
Mohammad Kassem <mhmd.kassem.eng@gmail.com>
2022-09-13 17:05:36 +0300

    laiison: backend: fix: fix filtering the admins in the getAll users method

--- Liaison/server-nodejs/src/user/service.ts
+++ Liaison/server-nodejs/src/user/service.ts
@@ -3,15 +3,15 @@ import { handleUnexpectedError } from "../../error-handeling/unexcpectedError";
 import User from "../../model/user";
 
 export const getAll = async () => {
-  return await User.find().populate({
+  return (await User.find().populate({
     path: "role",
     match: { role: "user" },
-  });
+  })).filter(user => user.role);
 };
 
 export const search = async (searchString: string) => {
   const re = new RegExp(searchString, "i");
-  return await User.find({
+  return (await User.find({
     $or: [
       { $text: { $search: searchString } },
       { fname: { $regex: re } },
@@ -21,7 +21,7 @@ export const search = async (searchString: string) => {
   }).populate({
     path: "role",
     match: { role: "user" },
-  });
+  })).filter(user => user.role);;
 };
 
 export const handleErrors = async (res: Response, err: any) => {
